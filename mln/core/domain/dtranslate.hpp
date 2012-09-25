167fa12173d1b4bde38f9404dbedc3b28f068782
Edwin Carlinet <carlinet@lrde.epita.fr>
2012-09-25 18:38:50 +0200

    Add converter between STL and MLN range/iterators.
    * mln/core/domain/dtranslate.hpp,
    * mln/core/iterator/stditerator.hpp,
    * mln/core/range/stdrange.hpp: New

--- mln/core/domain/dtranslate.hpp
+++ mln/core/domain/dtranslate.hpp
@@ -1,11 +1,7 @@
 #ifndef MLN_CORE_DOMAINS_DTRANSLATE_HPP
 # define MLN_CORE_DOMAINS_DTRANSLATE_HPP
 
-//# include <mln/core/domain.hpp>
-
-# include <boost/range/value_type.hpp>
-# include <boost/range/adaptor/transformed.hpp>
-
+# include <mln/core/range/adaptor/transformed.hpp>
 
 namespace mln {
 
@@ -14,17 +10,15 @@ namespace mln {
     template <typename Domain>
     struct translate_fun
     {
-      typedef typename boost::range_value<Domain>::type P;
+      typedef typename Domain::value_type P;
       typedef std::binder1st< std::plus<P> > type;
-      //typedef decltype( std::bind(std::plus<P> (), std::placeholders::_1, std::declval<P> ()) ) type;
     };
 
   };
 
 
   template <typename Domain>
-  using translated_domain = boost::transformed_range<typename internal::translate_fun<Domain>::type, const Domain >;
-
+  using translated_domain = adaptors::transformed_range<typename internal::translate_fun<Domain>::type, const Domain >;
 
 
   template <typename Domain>
@@ -32,8 +26,8 @@ namespace mln {
   translated_domain<Domain>
   dtranslate(const Domain& domain, typename boost::range_value<Domain>::type p)
   {
-    typedef typename boost::range_value<Domain>::type P;
-    return boost::adaptors::transform(domain, std::bind1st(std::plus<P> (), p));
+    typedef typename Domain::value_type P;
+    return adaptors::transform(domain, std::bind1st(std::plus<P> (), p));
   }
 
 

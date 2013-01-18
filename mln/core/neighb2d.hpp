cde9e7502c9ccdc804858ea6f409cea32b9bb6b6
Edwin Carlinet <carlinet@lrde.epita.fr>
2013-01-18 20:09:07 +0100

    Add c2 horizontal and vertical neighborhood.
    	* mln/core/neighb2d.hpp: c2_h + c2_v.

--- mln/core/neighb2d.hpp
+++ mln/core/neighb2d.hpp
@@ -15,7 +15,6 @@ namespace mln {
     static constexpr mln::array<point2d, 4> dpoints = {{{ {-1,0}, {0,-1}, {0,1}, {1,0} }}};
   };
 
-  //mln::array<point2d, 4> c4_t::dpoints;
   constexpr mln::array<point2d, 4> c4_t::dpoints;
 
   static const c4_t c4;
@@ -28,10 +27,31 @@ namespace mln {
                                                           {1,-1},  {1,1},  {1,0} }}};
   };
 
-  //constexpr mln::array<point2d, 8> c8_t::dpoints = 
   constexpr mln::array<point2d, 8> c8_t::dpoints;
 
   static const c8_t c8;
+
+  struct c2_v_t : neighborhood_base< c2_v_t, mln::array<point2d, 2> >
+  {
+    static const int static_size = 2;
+    static constexpr mln::array<point2d, 2> dpoints = {{{ {-1,0}, {1,0} }}};
+  };
+
+  constexpr mln::array<point2d, 2> c2_v_t::dpoints;
+
+  static const c2_v_t c2_v;
+
+
+
+  struct c2_h_t : neighborhood_base< c2_h_t, mln::array<point2d, 2> >
+  {
+    static const int static_size = 2;
+    static constexpr mln::array<point2d, 2> dpoints = {{{ {0,-1}, {0,1} }}};
+  };
+
+  constexpr mln::array<point2d, 2> c2_h_t::dpoints;
+
+  static const c2_h_t c2_h;
 
 
 } // end of namespace mln

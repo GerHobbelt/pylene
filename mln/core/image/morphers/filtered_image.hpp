1b4abb59a364bb1a4a9ecdd39de17cf05417d639
Edwin Carlinet <carlinet@lrde.epita.fr>
2013-04-30 19:11:09 +0200

    Make filtered image morpher works with byvalue functions.
    	* mln/core/image/morphers/filtered_image.hpp: Fix.

--- mln/core/image/morphers/filtered_image.hpp
+++ mln/core/image/morphers/filtered_image.hpp
@@ -180,7 +180,27 @@ namespace mln
       return m_ima.at(p);
     }
 
+    const_pixel_type	pixel(const point_type& p) const
+    {
+      mln_precondition(m_domain.has(p));
+      return const_pixel_type(static_cast<const self_t&>(*this), m_ima.pixel(p));
+    }
 
+    pixel_type		pixel(const point_type& p)
+    {
+      mln_precondition(m_domain.has(p));
+      return pixel_type(static_cast<self_t&>(*this), m_ima.pixel(p));
+    }
+
+    const_pixel_type	pixel_at(const point_type& p) const
+    {
+      return const_pixel_type(static_cast<const self_t&>(*this), m_ima.pixel_at(p));
+    }
+
+    pixel_type		pixel_at(const point_type& p)
+    {
+      return pixel_type(static_cast<self_t&>(*this), m_ima.pixel_at(p));
+    }
 
   private:
     I           m_ima;
@@ -188,6 +208,166 @@ namespace mln
     domain_type m_domain;
   };
 
+  template <typename I, class Predicate>
+  struct filtered_image_base<I, Predicate, false>
+    : morpher_base< filtered_image<I, Predicate>, I, mln_point(I), mln_value(I) >
+  {
+  private:
+    typedef typename std::remove_reference<I>::type image_t;
+
+    static_assert(image_traits<image_t>::accessible::value, "Image must be accessible.");
+    typedef filtered_image<I, Predicate> self_t;
+
+    typedef filter_iterator<
+      typename image_value_iterator<image_t>::type, Predicate> value_iterator;
+
+    typedef filter_iterator<
+      typename image_const_value_iterator<image_t>::type, Predicate> const_value_iterator;
+
+    struct pixel_predicate_t
+    {
+      typedef typename image_const_pixel_iterator<image_t>::type::reference	pixel_t;
+      typedef typename image_pixel_iterator<image_t>::type::reference		const_pixel_t;
+
+      pixel_predicate_t(const Predicate& pred)
+        : m_pred(pred)
+      {
+      }
+
+      bool
+      operator() (const pixel_t& pix)
+      {
+	return m_pred(pix.val());
+      }
+
+      bool
+      operator() (const const_pixel_t& pix)
+      {
+	return m_pred(pix.val());
+      }
+
+    private:
+      Predicate m_pred;
+    };
+
+    typedef rebind_pixel_iterator<
+      self_t,
+      filter_iterator<typename image_pixel_iterator<image_t>::type, pixel_predicate_t>
+      > pixel_iterator;
+
+    typedef rebind_pixel_iterator<
+      const self_t,
+      filter_iterator<typename image_const_pixel_iterator<image_t>::type, pixel_predicate_t>
+      > const_pixel_iterator;
+
+
+
+  public:
+    typedef internal::where_t<const image_t&, Predicate>        domain_type;
+
+    typedef typename image_t::point_type                        point_type;
+    typedef typename image_reference<image_t>::type             reference;
+    typedef typename image_const_reference<image_t>::type       const_reference;
+    typedef rebinded_pixel<self_t, typename image_t::pixel_type>                pixel_type;
+    typedef rebinded_pixel<const self_t, typename image_t::const_pixel_type>    const_pixel_type;
+    typedef iterator_range<value_iterator>                      value_range;
+    typedef iterator_range<const_value_iterator>                const_value_range;
+    typedef iterator_range<pixel_iterator>                      pixel_range;
+    typedef iterator_range<const_pixel_iterator>                const_pixel_range;
+
+
+    filtered_image_base(I&& ima, const Predicate& pred)
+      : m_ima(std::forward<I>(ima)),
+      m_pred(pred),
+      m_domain(m_ima, m_pred)
+        {
+        }
+
+    const domain_type& domain() const
+    {
+      return m_domain;
+    }
+
+
+    const_value_range values() const
+    {
+      return const_value_range
+	(make_filter_iterator(m_ima.values().iter(), m_pred));
+    }
+
+    value_range values()
+    {
+      return value_range
+	(make_filter_iterator(m_ima.values().iter(), m_pred));
+    }
+
+
+    const_pixel_range pixels() const
+    {
+      return const_pixel_range
+        (const_pixel_iterator
+         (static_cast<const self_t&>(*this),
+          make_filter_iterator(m_ima.pixels().iter(), pixel_predicate_t(m_pred))));
+    }
+
+    pixel_range pixels()
+    {
+      return pixel_range
+        (pixel_iterator
+         (static_cast<self_t&>(*this),
+          make_filter_iterator(m_ima.pixels().iter(), pixel_predicate_t(m_pred))));
+    }
+
+
+    const_reference     operator() (const point_type& p) const
+    {
+      mln_precondition(m_domain.has(p));
+      return m_ima(p);
+    }
+
+    reference           operator() (const point_type& p)
+    {
+      mln_precondition(m_domain.has(p));
+      return m_ima(p);
+    }
+
+    const_reference     at (const point_type& p) const
+    {
+      return m_ima.at(p);
+    }
+
+    reference           at (const point_type& p)
+    {
+      return m_ima.at(p);
+    }
+
+    const_pixel_type	pixel(const point_type& p) const
+    {
+      mln_precondition(m_domain.has(p));
+      return const_pixel_type(static_cast<const self_t&>(*this), m_ima.pixel(p));
+    }
+
+    pixel_type		pixel(const point_type& p)
+    {
+      mln_precondition(m_domain.has(p));
+      return pixel_type(static_cast<self_t&>(*this), m_ima.pixel(p));
+    }
+
+    const_pixel_type	pixel_at(const point_type& p) const
+    {
+      return const_pixel_type(static_cast<const self_t&>(*this), m_ima.pixel_at(p));
+    }
+
+    pixel_type		pixel_at(const point_type& p)
+    {
+      return pixel_type(static_cast<self_t&>(*this), m_ima.pixel_at(p));
+    }
+
+  private:
+    I           m_ima;
+    Predicate   m_pred;
+    domain_type m_domain;
+  };
 
 
   template <typename I, class Predicate>
@@ -205,6 +385,7 @@ namespace mln
   filtered_image<I, Predicate>
   imfilter(Image<I>&& ima, const Predicate& pred)
   {
+    static_assert(image_traits<I>::accessible::value, "Image must be accessible.");
     return filtered_image<I, Predicate>(move_exact(ima), pred);
   }
 
@@ -212,6 +393,7 @@ namespace mln
   filtered_image<const I&, Predicate>
   imfilter(const Image<I>& ima, const Predicate& pred)
   {
+    static_assert(image_traits<I>::accessible::value, "Image must be accessible.");
     return filtered_image<const I&, Predicate>(exact(ima), pred);
   }
 
@@ -219,6 +401,7 @@ namespace mln
   filtered_image<I&, Predicate>
   imfilter(Image<I>& ima, const Predicate& pred)
   {
+    static_assert(image_traits<I>::accessible::value, "Image must be accessible.");
     return filtered_image<I&, Predicate>(exact(ima), pred);
   }
 
--- tests/core/image/morphers/filtered_image.cpp
+++ tests/core/image/morphers/filtered_image.cpp
@@ -48,8 +48,43 @@ BOOST_AUTO_TEST_CASE(filtered_bypix)
 
 }
 
+BOOST_AUTO_TEST_CASE(filtered_byval)
+{
+  using namespace mln;
 
-BOOST_AUTO_TEST_CASE(filtered_image_writing)
+  typedef image2d<int> I;
+
+  box2d dom{{-1,-2}, {3,3}};
+  image2d<int> ima(dom);
+
+  typedef I::const_pixel_type Pix;
+
+  iota(ima, 0);
+  auto x = imfilter(ima, [](int v) { return v > 10; });
+
+  BOOST_CHECK(all(x > 10));
+
+  {
+    mln_foreach(const Pix& pix, ima.pixels())
+      if (pix.val() > 10)
+        BOOST_CHECK_EQUAL(pix.val(), x(pix.point()));
+      else {
+        BOOST_CHECK(!x.domain().has(pix.point()));
+        BOOST_CHECK_EQUAL(pix.val(), x.at(pix.point()));
+      }
+  }
+
+  {
+    mln_foreach(const auto& pix, x.pixels()) {
+      BOOST_CHECK_EQUAL(pix.val(), ima(pix.point()));
+    }
+  }
+
+}
+
+
+
+BOOST_AUTO_TEST_CASE(filtered_bypix_writing)
 {
   using namespace mln;
 
@@ -67,6 +102,63 @@ BOOST_AUTO_TEST_CASE(filtered_image_writing)
 
   io::imprint(ima);
 
+}
+
+BOOST_AUTO_TEST_CASE(filtered_byval_writing)
+{
+  using namespace mln;
+
+  typedef image2d<int> I;
+
+  box2d dom{{-1,-2}, {3,3}};
+  image2d<int> ima(dom);
+
+  typedef I::const_pixel_type Pix;
+
+  iota(ima, 0);
+  auto x = imfilter(ima, [](float x) { return x > 10; });
+
+  fill(x, 10);
+
+  io::imprint(ima);
+
+}
+
+
+
+BOOST_AUTO_TEST_CASE(filtered_chaining)
+{
+  using namespace mln;
+
+  typedef image2d<int> I;
+
+  box2d dom{{-1,-2}, {3,3}};
+  image2d<int> ima(dom);
+
+  iota(ima, 0);
+  auto x = imfilter(ima, [](int v) { return v > 10; });
+  auto u = imfilter(x, [](int v) { return v < 15; });
+
+  BOOST_CHECK(all(land(u > 10, u < 15)));
+
+  {
+    mln_foreach(const auto& pix, ima.pixels())
+      if (pix.val() > 10 and pix.val() < 15)
+        BOOST_CHECK_EQUAL(pix.val(), u(pix.point()));
+      else {
+        BOOST_CHECK(!u.domain().has(pix.point()));
+        BOOST_CHECK_EQUAL(pix.val(), u.at(pix.point()));
+      }
+  }
+
+  {
+    mln_foreach(const auto& pix, u.pixels()) {
+      BOOST_CHECK_EQUAL(pix.val(), ima(pix.point()));
+    }
+  }
+
+  fill(u, 1);
+  io::imprint(ima);
 }
 
 BOOST_AUTO_TEST_SUITE_END()

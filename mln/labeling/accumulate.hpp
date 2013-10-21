30c8dcb3c1b245c8a71b06fca208835cb1327112
Edwin Carlinet <carlinet@lrde.epita.fr>
2013-10-21 10:08:52 +0200

    Fix issue in accumulate when concept is passed by value. Add label accumulation on values.
    	*  mln/labeling/accumulate.hpp: Accumulation on values.

--- mln/labeling/accumulate.hpp
+++ mln/labeling/accumulate.hpp
@@ -13,7 +13,7 @@ namespace mln
 
     template <typename I, typename Label, typename AccuLike>
     std::vector< typename accu::result_of<AccuLike, mln_point(I)>::type >
-    p_accumulate(const Image<I>& lbl, Label nlabels, AccumulatorLike<AccuLike> accu_)
+    p_accumulate(const Image<I>& lbl, Label nlabels, const AccumulatorLike<AccuLike>& accu_)
     {
       static_assert( std::is_same<mln_value(I), Label>::value,
 		     "Image value type and Label type must match." );
@@ -42,9 +42,47 @@ namespace mln
 	  results[i] = accumulators[i].to_result();
       }
 
+      return results;
+    }
+
+    template <typename I, typename J, typename Label, typename AccuLike>
+    std::vector< typename accu::result_of<AccuLike, mln_value(J)>::type >
+    v_accumulate(const Image<I>& lbl_,
+		 const Image<J>& f_,
+		 Label nlabels,
+		 const AccumulatorLike<AccuLike>& accu_)
+    {
+      static_assert( std::is_same<mln_value(I), Label>::value,
+		     "Image value type and Label type must match." );
+
+
+      const I& lbl = exact(lbl_);
+      const J& f = exact(f_);
+      auto acc = accu::make_accumulator(exact(accu_), mln_value(J) ());
+      acc.init();
+
+      typedef decltype(acc) Accu;
+      std::vector<Accu> accumulators(nlabels+1, acc);
+
+      // accumulate
+      {
+	mln_pixter(px1, px2, lbl, f);
+	mln_forall(px1, px2)
+	  accumulators[px1->val()].take(px2->val());
+      }
+
+      // extract results
+      typedef typename accu::result_of<AccuLike, mln_value(J)>::type R;
+      std::vector<R> results(nlabels+1);
+      {
+	for (unsigned i = 0; i <= nlabels; ++i)
+	  results[i] = accumulators[i].to_result();
+      }
 
       return results;
     }
+
+
 
   }
 

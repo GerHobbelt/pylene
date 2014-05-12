b4993bef013d3a43348647e927a69d98dc26bd6e
Edwin Carlinet <edwin.carlinet@gmail.com>
2014-05-12 14:34:47 +0200

    New tests facilities and add support for custom stream in io::imprint. Fix issue with sub_image test.
    	* mln/io/imprint.hpp: New ostream parameter.
    	* tests/core/image/sub_image.cpp: Fixed.
    	* tests/test.hpp: New tests facilities.

--- mln/io/imprint.hpp
+++ mln/io/imprint.hpp
@@ -15,109 +15,120 @@
 
 namespace mln
 {
+
   namespace io
   {
+
+    template <typename I>
+    void imprint(const Image<I>& ima, std::ostream& os = std::cout);
+
+    template <typename I>
+    void imprint_with_border(const Image<I>& ima, std::ostream& os = std::cout);
+
+
+
+    /******************************************/
+    /****          Implementation          ****/
+    /******************************************/
 
     namespace internal
     {
 
       template <typename Image, typename Domain>
       typename std::enable_if< image_traits<Image>::accessible, void>::type
-      imprint(const Image& ima, Domain domain)
+      imprint(const Image& ima, Domain domain, std::ostream& os)
       {
         typedef typename Image::value_type V;
-	//typedef typename std::conditional<std::is_same<V, uint8>::value, int, typename Image::const_reference>::type T;
 
         for (auto p : domain) {
-          std::cout << '{' << p << ",";
-	  format(std::cout, ima(p)) << "}," << std::endl;
-	}
+          os << '{' << p << ",";
+          format(os, ima(p)) << "}," << std::endl;
+        }
       }
 
 
       template <typename Image, typename Domain>
       typename std::enable_if< std::is_convertible<typename image_traits<Image>::category,
                                                    forward_image_tag>::value, void>::type
-      imprint(const Image& ima, Domain domain)
+      imprint(const Image& ima, Domain domain, std::ostream& os)
       {
-        typedef typename Image::value_type V;
-	//typedef typename std::conditional<std::is_same<V, uint8>::value, int, typename Image::const_reference>::type T;
         (void) domain;
-	typedef typename Image::const_pixel_type pixel_t;
+        typedef typename Image::const_pixel_type pixel_t;
         mln_foreach (const pixel_t& pix, ima.pixels()) {
-          std::cout << '{' << pix.point() << ",";
-	  format(std::cout, pix.val()) << "}," << std::endl;
-	}
+          os << '{' << pix.point() << ",";
+          format(os, pix.val()) << "}," << std::endl;
+        }
       }
 
 
       template <typename Image, typename P>
       typename std::enable_if< image_traits<Image>::accessible::value, void>::type
-      imprint(const Image& ima, box<P, 2> domain)
+      imprint(const Image& ima, box<P, 2> domain, std::ostream& os)
       {
         typedef typename Image::value_type V;
-	//typedef typename std::conditional<std::is_same<V, uint8>::value, int, typename Image::const_reference>::type T;
-	mln_point(Image) p;
+
         std::ios state(NULL);
-        state.copyfmt(std::cout);
+        state.copyfmt(os);
 
-        std::cout << domain << "(" << typeid(V).name() << ")" << std::endl;
+        int wtext = 0;
+        frmt_max_width<V> frmter;
+        mln_foreach(V v, ima.values())
+          wtext = std::max(wtext, frmter(v));
 
-        //const int wtext = frmt_max_width<V>::value;
+        os << domain << "(" << typeid(V).name() << ")" << std::endl;
+        os.width(4);
 
-	int wtext = 0;
-	frmt_max_width<V> frmter;
-	mln_foreach(V v, ima.values())
-	  wtext = std::max(wtext, frmter(v));
-
-        //if (std::is_same<V, uint8>::value)
-        std::cout.width(4);
-	for (p[0] = domain.pmin[0]; p[0] < domain.pmax[0]; ++p[0]) {
-	  for (p[1] = domain.pmin[1]; p[1] < domain.pmax[1]; ++p[1]) {
-	    std::cout << std::setw(wtext);
-	    format(std::cout, ima(p)) << " ";
-	  }
-	  std::cout << std::endl;
-	}
-        std::cout.copyfmt(state);
+        mln_point(Image) p;
+        for (p[0] = domain.pmin[0]; p[0] < domain.pmax[0]; ++p[0]) {
+          for (p[1] = domain.pmin[1]; p[1] < domain.pmax[1]; ++p[1]) {
+            os << std::setw(wtext);
+            format(os, ima(p)) << " ";
+          }
+          os << std::endl;
+        }
+        os.copyfmt(state);
       }
 
       template <typename Image, typename P>
       typename std::enable_if< image_traits<Image>::accessible::value, void>::type
-      imprint_with_border(const Image& ima, box<P, 2> domain)
+      imprint_with_border(const Image& ima, box<P, 2> domain, std::ostream& os)
       {
         typedef typename Image::value_type V;
-	//typedef typename std::conditional<std::is_same<V, uint8>::value, int, typename Image::const_reference>::type T;
-	mln_point(Image) p;
+
+        std::ios state(NULL);
+        state.copyfmt(os);
+
+        int wtext = 0;
+        frmt_max_width<V> frmter;
+        mln_foreach(V v, ima.values())
+          wtext = std::max(wtext, frmter(v));
+
+        os << domain << "(" << typeid(V).name() << ")" << std::endl;
+        os.width(4);
+
+        mln_point(Image) p;
         int border = ima.border();
-
-	const int wtext =
-          std::is_same<V, uint8>::value ? 3 :
-          std::is_same<V, unsigned>::value ? 10 :
-          std::is_same<V, std::size_t>::value ? 10 : 0;
-
-        std::cout << domain << "(" << typeid(V).name() << ")" << std::endl;
-	std::cout.width(4);
-	for (p[0] = domain.pmin[0] - border; p[0] < domain.pmax[0] + border; ++p[0]) {
-	  for (p[1] = domain.pmin[1] - border; p[1] < domain.pmax[1] + border; ++p[1]) {
-	    std::cout << std::setw(wtext);
-	    format(std::cout, ima.at(p)) << " ";
-	  }
-	  std::cout << std::endl;
-	}
+        for (p[0] = domain.pmin[0]-border; p[0] < domain.pmax[0]+border; ++p[0]) {
+          for (p[1] = domain.pmin[1]-border; p[1] < domain.pmax[1]+border; ++p[1]) {
+            os << std::setw(wtext);
+            format(os, ima(p)) << " ";
+          }
+          os << std::endl;
+        }
+        os.copyfmt(state);
       }
     }
 
     template <typename I>
-    void imprint(const Image<I>& ima)
+    void imprint(const Image<I>& ima, std::ostream& os)
     {
-      internal::imprint(exact(ima), exact(ima).domain());
+      internal::imprint(exact(ima), exact(ima).domain(), os);
     }
 
     template <typename I>
-    void imprint_with_border(const Image<I>& ima)
+    void imprint_with_border(const Image<I>& ima, std::ostream& os)
     {
-      internal::imprint_with_border(exact(ima), exact(ima).domain());
+      internal::imprint_with_border(exact(ima), exact(ima).domain(), os);
     }
 
   }
--- tests/core/image/sub_image.cpp
+++ tests/core/image/sub_image.cpp
@@ -1,13 +1,13 @@
-# include <mln/core/image/image2d.hpp>
-# include <mln/core/image/sub_image.hpp>
-# include <mln/core/algorithm/iota.hpp>
-# include <mln/core/algorithm/fill.hpp>
-
-# include <mln/io/imprint.hpp>
-
 #define BOOST_TEST_MODULE Core
-#include <boost/test/unit_test.hpp>
-#include <boost/test/execution_monitor.hpp>
+#include <tests/test.hpp>
+
+#include <mln/core/image/image2d.hpp>
+#include <mln/core/image/sub_image.hpp>
+#include <mln/core/algorithm/iota.hpp>
+#include <mln/core/algorithm/fill.hpp>
+
+
+
 
 BOOST_AUTO_TEST_SUITE(SubImage)
 
@@ -16,13 +16,32 @@ BOOST_AUTO_TEST_CASE(sub_domain_with_box)
   using namespace mln;
 
   image2d<int> ima(5,5);
-  iota(ima, 0);
-  fill(ima | box2d{{1,1}, {4,4}}, 42);
-  fill((ima | box2d{{1,1}, {4,4}}) | box2d{{1,1}, {2,3}}, 44);
 
+  {
+    image2d<int> ref = {{0,1,2,3,4},
+                        {5,42,42,42,9},
+                        {10,42,42,42,14},
+                        {15,42,42,42,19},
+                        {20,21,22,23,24}};
+
+    iota(ima, 0);
+    fill(ima | box2d{{1,1}, {4,4}}, 42);
+    MLN_CHECK_IMEQUAL(ima, ref);
+  }
+
+  {
+    image2d<int> ref = {{0,1,2,3,4},
+                        {5,42,42,8,9},
+                        {10,11,12,13,14},
+                        {15,16,17,18,19},
+                        {20,21,22,23,24}};
+
+    iota(ima, 0);
+    fill(ima | box2d{{1,1}, {4,4}} | box2d{{1,1}, {2,3}}, 42);
+    MLN_CHECK_IMEQUAL(ima, ref);
+  }
 
   static_assert( std::is_same< decltype(ima | box2d()), image2d<int> >::value, "");
-  io::imprint(ima);
 }
 
 
@@ -34,31 +53,58 @@ BOOST_AUTO_TEST_CASE(sub_domain)
   using namespace mln;
 
   image2d<int> ima(5,5);
+
+  {
+    image2d<int> ref = {{0,1,2,3,4},
+                        {5,6,7,8,9},
+                        {10,42,42,42,42},
+                        {42,42,42,42,42},
+                        {42,42,42,42,42}};
+    iota(ima, 0);
+    fill(ima | where(ima > 10), 42);
+    MLN_CHECK_IMEQUAL(ima, ref);
+  }
+
+
+  {
+    image2d<int> ref = {{0,1,2,3,4},
+                        {5,6,7,8,9},
+                        {10,11,12,13,14},
+                        {15,16,17,18,19},
+                        {20,42,42,42,42}};
+
+    iota(ima, 0);
+    fill((ima | where(ima > 10)) | where(ima > 20), 42);
+    MLN_CHECK_IMEQUAL(ima, ref);
+  }
+
+  {
+    image2d<int> ref = {{0,1,2,3,4},
+                        {5,6,7,8,9},
+                        {10,42,42,42,42},
+                        {42,42,42,42,42},
+                        {20,21,22,23,24}};
+    iota(ima, 0);
+    fill(ima | where(land(ima > 10, ima < 20)), 42);
+    MLN_CHECK_IMEQUAL(ima, ref);
+  }
+
+}
+
+BOOST_AUTO_TEST_CASE_EXPECTED_FAILURES(sub_domain_failure, 1)
+
+BOOST_AUTO_TEST_CASE(sub_domain_failure)
+{
+  using namespace mln;
+
+  image2d<int> ima(5,5);
   iota(ima, 0);
   fill(ima | where(ima > 10), 42);
-  io::imprint(ima);
-
-
-  {
-    iota(ima, 0);
-    fill((ima | where(ima > 10)) | where(ima > 20), 44);
-    io::imprint(ima);
-  }
-
-  {
-    iota(ima, 0);
-    fill(ima | where(land(ima > 10, ima < 20)), 44);
-    io::imprint(ima);
-  }
-
-
   // This should throw because
   // where(ima < 20) is not included in where(ima > 10)
   {
     iota(ima, 0);
-    BOOST_REQUIRE_THROW( (fill((ima | where(ima > 10)) | where(ima < 20), 44)),
-                         boost::execution_exception);
-    io::imprint(ima);
+    fill((ima | where(ima > 10)) | where(ima < 20), 44);
   }
 }
 

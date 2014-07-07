f23a70b0a400db11656ac5b463297ba7419baada
Edwin Carlinet <edwin.carlinet@lrde.epita.fr>
2014-07-07 11:32:17 +0200

    Use stable sort in sort sites routine and add trace information.
    	*  mln/core/algorithm/sort_sites.hpp: Fix.

--- mln/core/algorithm/sort_sites.hpp
+++ mln/core/algorithm/sort_sites.hpp
@@ -3,6 +3,7 @@
 
 # include <mln/core/value/value_traits.hpp>
 # include <mln/core/image/image.hpp>
+# include <mln/core/trace.hpp>
 # include <mln/core/value/indexer.hpp>
 # include <vector>
 # include <algorithm>
@@ -12,6 +13,16 @@
 namespace mln
 {
 
+
+  /// \brief Sort the points of an image.
+  ///
+  /// \p sort_sites sorts the points of an image. An optional
+  /// comparison function can be provided through the parameter \p
+  /// cmp.  When two sites \p p and \p q are equivalent (neither
+  /// `input(p) < input(q)` nor `input(q) < input(p)` they are ranked
+  /// according to the natural point ordering relation (e.g. the scan
+  /// order for \p point2d)
+  ///
   template <typename I, typename BinaryFunction = std::less<mln_value(I)> >
   std::vector<typename I::site_type>
   sort_sites(const Image<I>& input,  BinaryFunction cmp = BinaryFunction ());
@@ -23,27 +34,28 @@ namespace mln
 
   namespace impl
   {
-    template <typename I, typename StrictWeakOrdering,
-	      typename Indexer = indexer<mln_value(I), StrictWeakOrdering> >
+    template <typename I, typename Compare>
     std::vector<typename I::site_type>
-    sort_sites(const I& input, StrictWeakOrdering, std::true_type _is_low_quant_)
+    sort_sites(const I& input, Compare, std::true_type _use_indexer_)
     {
-      (void) _is_low_quant_;
+      typedef indexer<mln_value(I), Compare> Indexer;
       typedef typename Indexer::index_type index_t;
-      Indexer f;
 
+      mln_entering("mln::sort_sites (couting sort)");
+      (void) _use_indexer_;
+      Indexer f;
 
       typedef mln_value(I) V;
       static constexpr std::size_t nvalues = 1 << value_traits<index_t>::quant;
       unsigned h[nvalues] = {0,};
       {
-	mln_viter(v, input);
-	mln_forall(v)
-	  ++h[f(*v)];
+        mln_viter(v, input);
+        mln_forall(v)
+          ++h[f(*v)];
 
-	unsigned count = 0;
+        unsigned count = 0;
         index_t i = value_traits<index_t>::min();
-	do {
+        do {
           unsigned tmp = h[i];
           h[i] = count;
           count += tmp;
@@ -54,40 +66,47 @@ namespace mln
       std::vector<typename I::site_type> v;
       v.resize(input.domain().size());
       {
-	mln_pixter(px, input);
-	mln_forall(px)
-	  v[ h[f(px->val())]++ ] = px->point();
+        mln_pixter(px, input);
+        mln_forall(px)
+          v[ h[f(px->val())]++ ] = px->point();
       }
+      mln_exiting();
       return v;
     }
 
-    template <typename I, typename StrictWeakOrdering,
-	      typename Indexer = indexer<mln_value(I), StrictWeakOrdering> >
+    template <typename I, typename StrictWeakOrdering>
     std::vector<typename I::site_type>
-    sort_sites(const I& input, StrictWeakOrdering cmp, std::false_type _is_low_quant_)
+    sort_sites(const I& input, StrictWeakOrdering cmp, std::false_type _use_indexer_)
     {
-      typedef typename I::site_type P;
-      std::vector<P> v;
+      mln_entering("mln::sort_sites (std sort)");
+
+      (void) _use_indexer_;
+
+      std::vector<mln_point(I)> v;
       v.reserve(input.domain().size());
       mln_piter(p, input);
       mln_forall(p)
-	v.push_back(*p);
+        v.push_back(*p);
 
-      std::sort(v.begin(), v.end(), [&input, cmp](const P& x, const P& y) { return cmp(input(x), input(y)); });
+      std::stable_sort(v.begin(), v.end(), [&input, cmp](const mln_point(I)& x, const mln_point(I)& y) {
+          return cmp(input(x), input(y)); });
+
+      mln_exiting();
       return v;
     }
 
   } // end of namespace mln::impl
 
 
-  template <typename I, typename BinaryFunction>
+  template <typename I, typename Compare>
   std::vector<typename I::site_type>
-  sort_sites(const Image<I>& input, BinaryFunction cmp)
+  sort_sites(const Image<I>& input, Compare cmp)
   {
-    static_assert(std::is_same<typename image_category<I>::type, raw_image_tag>::value,
-		  "Image must model the Raw Image Concept");
-    typedef std::integral_constant<bool, (value_traits<mln_value(I)>::quant <= 16)> is_low_quant;
-    return impl::sort_sites(exact(input), cmp, is_low_quant ());
+    constexpr bool _is_low_quant = value_traits<mln_value(I)>::quant <= 16;
+    constexpr bool _has_indexer  = has_indexer<mln_value(I), Compare>::value;
+
+    std::integral_constant<bool, _is_low_quant and _has_indexer> _use_indexer;
+    return impl::sort_sites(exact(input), cmp, _use_indexer);
   }
 
 } // end of namespace mln

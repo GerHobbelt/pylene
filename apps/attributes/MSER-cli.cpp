7315a3d2e187f98bf8d8e7754b335943d1efa0b5
Edwin Carlinet <edwin.carlinet@lrde.epita.fr>
2015-04-30 11:20:53 +0200

    Work on morphological attributes. Add new 'circularity' attribute and add 'ethreshold' parameter for thresholding extinction maps.
        *  apps/attributes/CMakeLists.txt: Fixed.
        *  apps/attributes/MSER-cli.cpp: Add reconstruction parameter.
        *  apps/attributes/attributes.cpp: Add ethreshold parameter.
        *  apps/attributes/circularity-cli.cpp,
        *  apps/attributes/circularity.cpp,
        *  apps/attributes/circularity.hpp: New.

--- apps/attributes/MSER-cli.cpp
+++ apps/attributes/MSER-cli.cpp
@@ -1,9 +1,11 @@
 #include <mln/io/imread.hpp>
 #include <mln/morpho/component_tree/accumulate.hpp>
+#include <mln/morpho/component_tree/reconstruction.hpp>
 #include <mln/accu/accumulators/accu_if.hpp>
 #include <mln/accu/accumulators/count.hpp>
 #include <apps/tos/Kinterpolate.hpp>
 #include <apps/tos/topology.hpp>
+#include <mln/io/imsave.hpp>
 #include "attributes.hpp"
 #include "cMSER.hpp"
 
@@ -15,7 +17,8 @@ int main(int argc, char** argv)
 
   po::options_description desc("MSER Options");
   desc.add_options()
-    ("eps,h", po::value<float>()->required(), "The height for neighbor lookup (e.g. 10).");
+    ("eps,h", po::value<float>()->required(), "The height for neighbor lookup (e.g. 10).")
+    ("export-rec", po::value<std::string>(), "For reconstruction.");
 
   po::variables_map vm = process_cmdline(argc, argv, desc);
   tree_t tree = preprocess(vm);
@@ -41,4 +44,11 @@ int main(int argc, char** argv)
   std::function<float(tree_t::node_type)> attrs[] = { _as_fun(amser), _as_fun(smap) };
 
   export_(vm, tree, smap, names, attrs, 2);
+
+  if (vm.count("export-rec")) {
+    auto vmap = morpho::make_attribute_map_from_image(tree, F);
+    morpho::reconstruction(tree, vmap, F);
+    io::imsave(F, vm["export-rec"].as<std::string>());
+  }
+
 }

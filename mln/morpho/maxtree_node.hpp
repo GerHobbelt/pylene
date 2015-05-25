c277a88d034e3dd30a27398b7a985def35e574aa
Edwin Carlinet <edwin.carlinet@lrde.epita.fr>
2015-05-25 11:37:08 +0200

    Add missing file for maxtree comarison apps.
            *  mln/morpho/maxtree_node.hpp,
            *  mln/morpho/maxtree_pqueue_2.hpp,
            *  apps/maxtree_comparison/bench_parallel_pqueue_2.cpp: New.

---
+++ mln/morpho/maxtree_node.hpp
@@ -0,0 +1,24 @@
+#ifndef MLN_MORPHO_MAXTREE_NODE_HPP
+# define MLN_MORPHO_MAXTREE_NODE_HPP
+
+namespace mln
+{
+
+  namespace morpho
+  {
+    struct maxtree_node
+    {
+      typedef unsigned size_type;
+      static const unsigned Nil = -1;
+
+      size_type m_parent;
+      size_type m_prev;
+      size_type m_next;
+      double	m_pos;
+    };
+
+  }
+
+}
+
+#endif // ! MLN_MORPHO_MAXTREE_NODE_HPP

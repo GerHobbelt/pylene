f1431359100e1e49d61bc83085f163a1342f3dd6
ecarlinet <ecarlinet@dxo.com>
2016-02-03 16:40:58 +0100

    Fix for alternative operators (and, or, not) on MSVC
     *  mln/core/config.hpp: Fixed.

--- mln/core/config.hpp
+++ mln/core/config.hpp
@@ -1,1 +1,9 @@
-#define BOOST_PP_VARIADICS 1 
+#include <ciso646> // FOR MSVC alternative operator representations
+
+#define BOOST_PP_VARIADICS 1
+
+#ifndef MLN_USE_TBB
+# define MLN_HAS_TBB 1
+#else
+# define MLN_HAS_TBB 0
+#endif

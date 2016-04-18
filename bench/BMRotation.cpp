3e3c1cf18c77b1739105f17dd55448b3d8480d2d
Edwin Carlinet <edwin.carlinet@gmail.com>
2016-04-18 18:11:25 +0200

    Add BM for rotation.

---
+++ bench/BMRotation.cpp
@@ -0,0 +1,92 @@
+#include <mln/core/image/image2d.hpp>
+#include <mln/io/imread.hpp>
+#include <mln/io/imsave.hpp>
+#include <benchmark/benchmark.h>
+
+using namespace mln;
+
+struct BMRotation : public benchmark::Fixture
+{
+  const char* filename = MLN_IMG_PATH "/lena.ppm";
+
+  BMRotation()
+    {
+      io::imread(filename, m_input);
+
+      int nr = m_input.domain().shape()[0];
+      int nc = m_input.domain().shape()[1];
+      m_output = image2d<rgb8>(box2d{ {-nc+1,0}, {1,nr}});
+      m_bytes = nr * nc * sizeof(rgb8);
+    }
+
+  image2d<rgb8> m_input;
+  image2d<rgb8> m_output;
+  std::size_t   m_bytes;
+};
+
+
+template <class T>
+void rotate_ptr(const image2d<T>& f, image2d<T>& out)
+{
+
+  int nr = f.nrows();
+  int nc = f.ncols();
+  int istride = f.strides()[0];
+  int ostride = out.strides()[0];
+  const T* ptr_in = & f({0,0});
+
+  for (int i = 0; i < nr; ++i) {
+    T* ptr_out = &out({0,i});
+    for (int j = 0; j < nc; ++j) {
+      *ptr_out = ptr_in[j];
+      ptr_out = (T*)((char*)ptr_out - ostride);
+    }
+    ptr_in = (T*)((char*)ptr_in + istride);
+  }
+}
+
+template <class T>
+void rotate_p(const image2d<T>& f, image2d<T>& out)
+{
+  mln_foreach(point2d p, f.domain()) {
+    point2d q{-p[1], p[0]};
+    out(q) = f(p);
+  }
+}
+
+
+template <class T>
+void rotate_pix(const image2d<T>& f, image2d<T>& out)
+{
+  mln_foreach(auto px, f.pixels()) {
+    point2d q{-px.point()[1], px.point()[0]};
+    out(q) = px.val();
+  }
+}
+
+BENCHMARK_F(BMRotation, WithPtr)(benchmark::State& st) {
+   while (st.KeepRunning()) {
+     rotate_ptr(m_input, m_output);
+  }
+   st.SetBytesProcessed(st.iterations() * m_bytes);
+}
+
+BENCHMARK_F(BMRotation, WithPoint)(benchmark::State& st) {
+   while (st.KeepRunning()) {
+     rotate_p(m_input, m_output);
+  }
+   st.SetBytesProcessed(st.iterations() * m_bytes);
+}
+
+BENCHMARK_F(BMRotation, WithPixel)(benchmark::State& st) {
+   while (st.KeepRunning()) {
+     rotate_pix(m_input, m_output);
+  }
+   st.SetBytesProcessed(st.iterations() * m_bytes);
+}
+
+BENCHMARK_MAIN();
+
+
+
+

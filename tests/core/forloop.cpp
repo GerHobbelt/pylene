b74ddfdfbd8846ddb5d0d0f879027126f2c764c1
Edwin Carlinet <edwin.carlinet@gmail.com>
2017-10-03 12:35:15 +0200

    Clean and add test for loop.

--- tests/core/forloop.cpp
+++ tests/core/forloop.cpp
@@ -15,6 +15,23 @@ BOOST_AUTO_TEST_CASE(foreach_break)
   BOOST_CHECK((p == mln::point2d{2,-1}));
 }
 
+BOOST_AUTO_TEST_CASE(foreach_0)
+{
+  mln::box2d box = {{-1, -1}, {5, 5}};
+  mln::point2d p;
+
+  int y = -1;
+  int x = -1;
+  mln_foreach (p, box)
+  {
+    BOOST_CHECK_EQUAL(p[0], y);
+    BOOST_CHECK_EQUAL(p[1], x);
+    if (++x == 5) { y++; x = -1; }
+  }
+
+  BOOST_CHECK_EQUAL(p, (mln::point2d{4,4}));
+}
+
 BOOST_AUTO_TEST_CASE(forall_break)
 {
   mln::box2d box = {{-1, -1}, {5, 5}};
@@ -25,3 +42,14 @@ BOOST_AUTO_TEST_CASE(forall_break)
 
   BOOST_CHECK((*p == mln::point2d{2,-1}));
 }
+
+BOOST_AUTO_TEST_CASE(forall_0)
+{
+  mln::box2d box = {{-1, -1}, {5, 5}};
+  auto p = mln::rng::iter(box);
+  mln_forall (p)
+    ;
+
+  BOOST_CHECK((*p > mln::point2d{4,4}));
+}
+

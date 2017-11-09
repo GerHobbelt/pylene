4834940d0de6fcce01623e14b4ab450a17142cd8
Michaël Roynard <michael.roynard@lrde.epita.fr>
2017-11-09 15:01:10 +0100

    Indent

--- tests/accu/count.cpp
+++ tests/accu/count.cpp
@@ -4,23 +4,23 @@
 
 TEST(Accu, Sum)
 {
-  using namespace mln::accu;
+    using namespace mln::accu;
 
-  accumulators::count<unsigned> acc;
-  accumulators::count<unsigned> acc2;
+    accumulators::count<unsigned> acc;
+    accumulators::count<unsigned> acc2;
 
-  acc.take(12);
-  acc.take("blabla");
+    acc.take(12);
+    acc.take("blabla");
 
-  ASSERT_EQ(extractor::count(acc), 2);
+    ASSERT_EQ(extractor::count(acc), 2);
 
-  acc.untake(13);
+    acc.untake(13);
 
-  ASSERT_EQ(extractor::count(acc), 1);
+    ASSERT_EQ(extractor::count(acc), 1);
 
-  acc2.take(69);
-  acc.take(69);
-  acc2.take(acc);
+    acc2.take(69);
+    acc.take(69);
+    acc2.take(acc);
 
-  ASSERT_EQ(extractor::count(acc2), 3);
+    ASSERT_EQ(extractor::count(acc2), 3);
 }
--- tests/accu/moment_of_inertia.cpp
+++ tests/accu/moment_of_inertia.cpp
@@ -6,38 +6,38 @@
 // Moment of inertia of a square
 TEST(Accu, Moment_of_inertia)
 {
-  using namespace mln;
+    using namespace mln;
 
-  auto sum_of_square = [](float x) { return x * (x + 1) * (2 * x + 1) / 6; };
+    auto sum_of_square = [](float x) { return x * (x + 1) * (2 * x + 1) / 6; };
 
-  double res1;
-  {
-    accu::accumulators::moment_of_inertia<point2d, vec2u> acc;
-    acc.init();
-    int n = 10;
-    for (short i = 0; i <= n; ++i)
-      for (short j = 0; j <= n; ++j)
-        acc.take(point2d{i, j});
-    res1 = acc.to_result();
+    double res1;
+    {
+        accu::accumulators::moment_of_inertia<point2d, vec2u> acc;
+        acc.init();
+        int n = 10;
+        for (short i = 0; i <= n; ++i)
+            for (short j = 0; j <= n; ++j)
+                acc.take(point2d{i, j});
+        res1 = acc.to_result();
 
-    double tmp = 2 * (n + 1) * sum_of_square(n / 2.f);
-    double ref = (tmp + tmp) / sqr(sqr(n + 1));
-    ASSERT_EQ(res1, ref);
-  }
+        double tmp = 2 * (n + 1) * sum_of_square(n / 2.f);
+        double ref = (tmp + tmp) / sqr(sqr(n + 1));
+        ASSERT_EQ(res1, ref);
+    }
 
-  // We are supposed to be scale invariant.
-  {
-    accu::accumulators::moment_of_inertia<point2d, vec<double, 2>> acc;
-    acc.init();
-    int n = 10000;
-    for (short i = 0; i <= n; ++i)
-      for (short j = 0; j <= n; ++j)
-        acc.take(point2d{i, j});
+    // We are supposed to be scale invariant.
+    {
+        accu::accumulators::moment_of_inertia<point2d, vec<double, 2>> acc;
+        acc.init();
+        int n = 10000;
+        for (short i = 0; i <= n; ++i)
+            for (short j = 0; j <= n; ++j)
+                acc.take(point2d{i, j});
 
-    double res2 = acc.to_result();
-    double tmp = 2 * (n + 1) * sum_of_square(n / 2.f);
-    double ref = (tmp + tmp) / sqr(sqr(double(n + 1)));
-    ASSERT_TRUE(std::abs(res2 - ref) <= 1);
-    ASSERT_TRUE(std::abs(res1 - res2) <= 1);
-  }
+        double res2 = acc.to_result();
+        double tmp = 2 * (n + 1) * sum_of_square(n / 2.f);
+        double ref = (tmp + tmp) / sqr(sqr(double(n + 1)));
+        ASSERT_TRUE(std::abs(res2 - ref) <= 1);
+        ASSERT_TRUE(std::abs(res1 - res2) <= 1);
+    }
 }

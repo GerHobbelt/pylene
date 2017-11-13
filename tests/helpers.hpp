40b7cb88c01b23d3d39b27ddc89bc71fd5ffcc4b
Michaël Roynard <michael.roynard@lrde.epita.fr>
2017-11-13 11:43:01 +0100

    gtest migration draw io colors graph graphcut transform

---
+++ tests/helpers.hpp
@@ -0,0 +1,37 @@
+#ifndef MILENA_HELPERS_H
+#define MILENA_HELPERS_H
+
+#include <mln/core/image/image2d.hpp>
+#include <mln/io/imprint.hpp>
+
+#include <gtest/gtest.h>
+
+namespace mln
+{
+
+    template <class I, class J>
+    void imcheck_equal(const Image<I>& f, const Image<J>& g)
+    {
+        mln_pixter(px1, exact(f));
+        mln_pixter(px2, exact(g));
+
+        mln_forall (px1, px2)
+        {
+            if (px1->point() != px2->point() or px1->val() != px2->val())
+            {
+                std::stringstream msg;
+                msg << "The following images differs:\n";
+                io::imprint(f, msg);
+                msg << " and\n:";
+                io::imprint(g, msg);
+
+                ASSERT_TRUE(msg.str() == ""); // fail
+                return;
+            }
+        }
+    }
+}
+
+#define MLN_CHECK_IMEQUAL(f, g) mln::imcheck_equal(f, g);
+
+#endif //MILENA_HELPERS_H
--- tests/io/registration.cpp
+++ tests/io/registration.cpp
@@ -1,24 +1,23 @@
-#include <mln/io/registration.hpp>
 #include <iostream>
+#include <mln/io/registration.hpp>
 
-int main()
+int
+main()
 {
-  {
-    std::map<int, const mln::io::type_info_base*>::const_iterator x =
-      mln::io::registration_map_.begin();
+    {
+        std::map<int, const mln::io::type_info_base*>::const_iterator x = mln::io::registration_map_.begin();
 
-    for (x; x !=  mln::io::registration_map_.end(); ++x)
-      std::cout << x->first << ":" << x->second->name() << std::endl;
-  }
+        for (x; x != mln::io::registration_map_.end(); ++x)
+            std::cout << x->first << ":" << x->second->name() << std::endl;
+    }
 
-  mln::io::type_info<mln::uint8> t;
-  std::cout << t.id() << ":" << t.name() << std::endl;
+    mln::io::type_info<mln::uint8> t;
+    std::cout << t.id() << ":" << t.name() << std::endl;
 
-  {
-    std::map<int, const mln::io::type_info_base*>::const_iterator x =
-      mln::io::registration_map_.begin();
+    {
+        std::map<int, const mln::io::type_info_base*>::const_iterator x = mln::io::registration_map_.begin();
 
-    for (x; x !=  mln::io::registration_map_.end(); ++x)
-      std::cout << x->first << ":" << x->second->name() << std::endl;
-  }
+        for (x; x != mln::io::registration_map_.end(); ++x)
+            std::cout << x->first << ":" << x->second->name() << std::endl;
+    }
 }

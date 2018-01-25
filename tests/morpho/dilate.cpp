4ebbf0239e2a82a0c5f06ef3d0fd8ec97fbac4f1
Edwin Carlinet <edwin.carlinet@gmail.com>
2018-01-25 17:52:27 +0100

    Moved rect2d impl in its own file and use periodic line decomp.

--- tests/morpho/dilate.cpp
+++ tests/morpho/dilate.cpp
@@ -48,7 +48,41 @@ TEST(Dilation, Disc_euclidean)
   ASSERT_IMAGES_EQ(ref, output);
 }
 
-TEST(Morpho, Generic_wide_enough_extension)
+TEST(Dilation, Rectangle2d)
+{
+  mln::box2d domain{ {0,0}, {21, 21}};
+  mln::image2d<mln::uint8> input(domain, 0);
+  input.at(10,10) = 1;
+
+  const mln::image2d<mln::uint8> ref = {
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, //
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, //
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, //
+      {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0}, //
+      {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0}, //
+      {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0}, //
+      {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0}, //
+      {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0}, //
+      {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0}, //
+      {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0}, //
+      {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0}, //
+      {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0}, //
+      {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0}, //
+      {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0}, //
+      {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0}, //
+      {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0}, //
+      {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0}, //
+      {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0}, //
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, //
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, //
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}  //
+  };
+
+  auto output = mln::morpho::structural::dilate(input, mln::se::rect2d(19, 15));
+  ASSERT_IMAGES_EQ(ref, output);
+}
+
+TEST(Dilation, Generic_with_wide_enough_extension)
 {
   image2d<uint8> ima;
   io::imread(MLN_IMG_PATH "small.pgm", ima);
@@ -72,23 +106,22 @@ TEST(Dilation, Generic_with_too_small_extension)
 }
 
 // Dilation on a with a vmorph / binary case
-TEST(Morpho, dilation_dilate_2)
+TEST(Dilation, Square_on_a_vmorph)
 {
   image2d<uint8> ima;
   io::imread(MLN_IMG_PATH "small.pgm", ima);
 
-  auto win = make_rectangle2d(3, 3);
+  mln::se::rect2d win(3, 3);
   auto out = morpho::structural::dilate(ima > 128, win);
   ASSERT_TRUE(all(out >= (ima > 128))); // extensive
 }
 
-// Dilation on a with a vmorph / binary case
-TEST(Morpho, dilation_dilate_view)
+TEST(Dilation, Unregular_domain)
 {
   image2d<uint8> ima;
   io::imread(MLN_IMG_PATH "small.pgm", ima);
 
-  auto win = make_rectangle2d(3, 3);
+  mln::se::rect2d win(3, 3);
   auto out = clone(ima);
   auto tmp = out | where(ima > 128);
   morpho::structural::dilate(ima | where(ima > 128), win, tmp, std::less<uint8>());
@@ -97,18 +130,18 @@ TEST(Morpho, dilation_dilate_view)
 }
 
 // Custom comparison function, erosion
-TEST(Morpho, dilation_dilate_with_custom_cmp_function)
+TEST(Dilation, Custom_cmp_function)
 {
   image2d<uint8> ima;
   io::imread(MLN_IMG_PATH "small.pgm", ima);
 
-  auto win = make_rectangle2d(5, 5);
+  mln::se::rect2d win(5, 5);
   auto out = morpho::structural::dilate(ima, win, std::greater<uint8>());
   ASSERT_TRUE(all(out <= ima)); // anti-extensive
 }
 
 // Dilation of a binary image
-TEST(Morpho, dilation_dilate_binary)
+TEST(Dilation, Binary)
 {
   image2d<bool> ima(11, 11);
   fill(ima, false);
@@ -116,31 +149,46 @@ TEST(Morpho, dilation_dilate_binary)
   ima.at(5, 5) = true;
   ima.at(10, 10) = true;
 
-  auto win = make_rectangle2d(3, 3);
+  image2d<bool> ref = {
+      {1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0}, //
+      {1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0}, //
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, //
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, //
+      {0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0}, //
+      {0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0}, //
+      {0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0}, //
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, //
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, //
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1}, //
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1}  //
+  };
+
+  mln::se::rect2d win(3, 3);
   auto out = morpho::structural::dilate(ima, win);
-  ASSERT_TRUE(all(ima <= out)); // anti-extensive
+  ASSERT_IMAGES_EQ(ref, out);
 }
 
 // Dilation of a bianry image
-TEST(Morpho, dilation_dilate_binary_2)
+TEST(Dilation, Binary_2)
 {
   image2d<bool> ima;
   io::imread(MLN_IMG_PATH "tiny.pbm", ima);
 
-  auto win = make_rectangle2d(3, 3);
+  mln::se::rect2d win(3, 3);
   auto out = morpho::structural::dilate(ima, win);
   ASSERT_TRUE(all(ima <= out)); // anti-extensive
 }
 
 // Dilation of a rgb image
-TEST(Morpho, dilation_dilate_rgb)
+TEST(Dilation, RGB)
 {
   image2d<rgb8> ima;
   io::imread(MLN_IMG_PATH "small.ppm", ima);
 
-  auto win = make_rectangle2d(5, 5);
+  mln::se::rect2d win(5, 5);
   auto out = morpho::structural::dilate(ima, win);
   ASSERT_TRUE(all(red(ima) <= red(out)));     // anti-extensive
   ASSERT_TRUE(all(green(ima) <= green(out))); // anti-extensive
   ASSERT_TRUE(all(blue(ima) <= blue(out)));   // anti-extensive
 }
+

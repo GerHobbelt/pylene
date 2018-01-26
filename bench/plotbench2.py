9dd47e5b26bde264bd00d9a57808e47d89c9496a
Edwin Carlinet <edwin.carlinet@gmail.com>
2018-01-26 18:00:16 +0100

    Add benchmark for dilation.

---
+++ bench/plotbench2.py
@@ -0,0 +1,33 @@
+#! /usr/bin/python3
+
+import json
+import sys
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+data = json.load(open(sys.argv[1]))
+entries = data["benchmarks"]
+
+for entry in entries:
+    _, algo, size = entry["name"].split("/")
+    entry["algo"] = algo
+    entry["size"] = int(size)
+    entry["bytes_per_second"] = entry["bytes_per_second"] / (2 ** 20)
+    del entry["name"]
+    del entry["cpu_time"]
+    del entry["iterations"]
+    del entry["real_time"]
+    del entry["time_unit"]
+
+df = pd.DataFrame(entries)
+
+
+df.set_index(["size", "algo"], inplace=True)
+df = df.unstack()["bytes_per_second"]
+df.plot(logy=True)
+
+
+plt.xlabel("SE Size")
+plt.ylabel("Performance (MB/s)")
+plt.show()

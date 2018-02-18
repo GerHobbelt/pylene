ef6117d369703325e7ce38e2554a9833e8708920
Virgile Hirtz <virgile.hirtz@epita.fr>
2018-02-18 22:27:38 +0100

    Add new benchmark neighborhood for reference
    Empty benchmark, no function implemented yet
    Changed the name of the previous reference benchmark

--- bench/BMReference_Neighborhood_main.cpp
+++ bench/BMReference_Neighborhood_main.cpp
@@ -2,12 +2,6 @@
 
 #include <mln/core/image/image2d.hpp>
 #include <mln/io/imread.hpp>
-#include <mln/core/extension/fill.hpp>
-
-void Sum_C(mln::image2d<mln::uint8>& img, mln::image2d<mln::uint8>& new_img);
-void Sum_Pylene(mln::image2d<mln::uint8>& img, mln::image2d<mln::uint8>& new_img);
-void Average_C(mln::image2d<mln::uint8>& img, mln::image2d<mln::uint8>& new_img);
-void Average_Pylene(mln::image2d<mln::uint8>& img, mln::image2d<mln::uint8>& new_img);
 
 class Bench_Ref_Neighborhood : public benchmark::Fixture
 {
@@ -17,45 +11,21 @@ class Bench_Ref_Neighborhood : public benchmark::Fixture
   {
     mln::image2d<mln::rgb8> tmp(0,0);
     mln::io::imread(filename, tmp);
-    mln::resize(ima, tmp);
+    resize(ima, tmp);
     mln::copy(mln::red(tmp), ima);
-
-    mln::resize(out_ima, ima);
-    mln::extension::fill(ima, 0);
   }
 
 protected:
-  void Do_0(benchmark::State& st, void (*func)(mln::image2d<mln::uint8>& , mln::image2d<mln::uint8>& ))
+  void Do_0(benchmark::State& st, void (*func)(mln::image2d<mln::uint8>& ))
   {
     while (st.KeepRunning())
-      func(ima, out_ima);
+      func(ima);
 
     st.SetBytesProcessed(st.iterations() * ima.domain().size() * sizeof(int));
   }
 
 private:
   mln::image2d<mln::uint8> ima;
-  mln::image2d<mln::uint8> out_ima;
 };
-
-BENCHMARK_F(Bench_Ref_Neighborhood, Sum_C)(benchmark::State& st)
-{
-  Do_0(st, Sum_C);
-}
-
-BENCHMARK_F(Bench_Ref_Neighborhood, Sum_Pylene)(benchmark::State& st)
-{
-  Do_0(st, Sum_Pylene);
-}
-
-BENCHMARK_F(Bench_Ref_Neighborhood, Average_C)(benchmark::State& st)
-{
-  Do_0(st, Average_C);
-}
-
-BENCHMARK_F(Bench_Ref_Neighborhood, Average_Pylene)(benchmark::State& st)
-{
-  Do_0(st, Average_Pylene);
-}
 
 BENCHMARK_MAIN();

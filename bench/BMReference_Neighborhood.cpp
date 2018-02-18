963034edb2a1d7e561152de6b9e61da861f7c57b
Virgile Hirtz <virgile.hirtz@epita.fr>
2018-02-19 00:08:15 +0100

    Add Sum and Average Benchmark for reference

--- bench/BMReference_Neighborhood.cpp
+++ bench/BMReference_Neighborhood.cpp
@@ -1,1 +1,94 @@
 #include <mln/core/image/image2d.hpp>
+#include <mln/core/extension/fill.hpp>
+
+void Sum_Pylene(mln::image2d<mln::uint8>& img)
+{
+  mln::image2d<mln::uint8> new_img(0,0);
+  mln::resize(new_img, img);
+  mln::extension::fill(img, 0);
+  mln_foreach(auto p, img.domain())
+  {
+    unsigned tmp = 0;
+    for (int i = -1; i <= 1; i++)
+      for (int j = -1; j <= 1; j++)
+      {
+        mln::point2d p2(i,j);
+        tmp += img(p + p2);
+      }
+    new_img(p) = tmp;
+  }
+}
+
+void Sum_C(mln::image2d<mln::uint8>& img)
+{
+  mln::image2d<mln::uint8> new_img(0,0);
+  mln::resize(new_img, img);
+  mln::extension::fill(img, 0);
+
+  int nrow = img.nrows();
+  int ncol = img.ncols();
+  std::ptrdiff_t stride = img.strides()[0];
+
+  mln::uint8* buffer = &img.at(0,0);
+  mln::uint8* buffer_new = &new_img.at(0,0);
+  for (int i = 0; i < nrow; i++)
+  {
+    for (int j = 0; j < ncol; j++)
+    {
+      for (int k = -1; k <= 1; k++)
+        for (int l = -1; l <= 1; l++)
+        {
+          buffer_new[j] += buffer[j + k * stride + l];
+        }
+    }
+    buffer += stride;
+    buffer_new += stride;
+  }
+}
+
+void Average_Pylene(mln::image2d<mln::uint8>& img)
+{
+  mln::image2d<mln::uint8> new_img(0,0);
+  mln::resize(new_img, img);
+  mln::extension::fill(img, 0);
+  mln_foreach(auto p, img.domain())
+  {
+    unsigned int tmp = 0;
+    for (int i = -1; i <= 1; i++)
+      for (int j = -1; j <= 1; j++)
+      {
+        mln::point2d p2(i,j);
+        tmp += img(p + p2);
+      }
+    new_img(p) = tmp / 9;
+  }
+}
+
+void Average_C(mln::image2d<mln::uint8>& img)
+{
+  mln::image2d<mln::uint8> new_img(0,0);
+  mln::resize(new_img, img);
+  mln::extension::fill(img, 0);
+
+  int nrow = img.nrows();
+  int ncol = img.ncols();
+  std::ptrdiff_t stride = img.strides()[0];
+
+  mln::uint8* buffer = &img.at(0,0);
+  mln::uint8* buffer_new = &new_img.at(0,0);
+  for (int i = 0; i < nrow; i++)
+  {
+    for (int j = 0; j < ncol; j++)
+    {
+      unsigned int tmp = 0;
+      for (int k = -1; k <= 1; k++)
+        for (int l = -1; l <= 1; l++)
+        {
+          tmp += buffer[j + k * stride + l];
+        }
+      buffer_new[j] = tmp / 9;
+    }
+    buffer += stride;
+    buffer_new += stride;
+  }
+}
--- bench/BMReference_Neighborhood_main.cpp
+++ bench/BMReference_Neighborhood_main.cpp
@@ -3,6 +3,11 @@
 #include <mln/core/image/image2d.hpp>
 #include <mln/io/imread.hpp>
 
+void Sum_C(mln::image2d<mln::uint8>& img);
+void Sum_Pylene(mln::image2d<mln::uint8>& img);
+void Average_C(mln::image2d<mln::uint8>& img);
+void Average_Pylene(mln::image2d<mln::uint8>& img);
+
 class Bench_Ref_Neighborhood : public benchmark::Fixture
 {
   const char* filename = MLN_IMG_PATH "/lena.ppm";
@@ -27,5 +32,25 @@ protected:
 private:
   mln::image2d<mln::uint8> ima;
 };
+
+BENCHMARK_F(Bench_Ref_Neighborhood, Sum_C)(benchmark::State& st)
+{
+  Do_0(st, Sum_C);
+}
+
+BENCHMARK_F(Bench_Ref_Neighborhood, Sum_Pylene)(benchmark::State& st)
+{
+  Do_0(st, Sum_Pylene);
+}
+
+BENCHMARK_F(Bench_Ref_Neighborhood, Average_C)(benchmark::State& st)
+{
+  Do_0(st, Average_C);
+}
+
+BENCHMARK_F(Bench_Ref_Neighborhood, Average_Pylene)(benchmark::State& st)
+{
+  Do_0(st, Average_Pylene);
+}
 
 BENCHMARK_MAIN();

943f5ae40313ca9ea8634226accb73cdb8e5ccec
Michaël Roynard <michael.roynard@lrde.epita.fr>
2018-10-12 15:00:12 +0200

    Rebase from ranges-transform

--- mln/core/rangev3/multi_span.hpp
+++ mln/core/rangev3/multi_span.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <mln/core/assert.hpp>
 #include <mln/core/rangev3/private/multi_view_facade.hpp>
 #include <range/v3/span.hpp>
 #include <range/v3/view/reverse.hpp>
@@ -32,97 +31,85 @@ namespace mln::ranges
     }
   };
 
-  namespace details
+
+
+  /// Implement a multidimentional span with rows beings span (contiguous elements)
+  template <class T, std::size_t Rank>
+  class multi_span : public details::multi_view_facade<Rank, multi_span<T, Rank>>
   {
-    /// Implement a multidimentional span with rows beings span (contiguous elements)
-    template <class T, std::size_t Rank>
-    struct multi_span : ::ranges::view_base
+  public:
+    struct cursor
     {
-      struct cursor
+      T& __read() const { return *(m_ptr.back()); };
+      T& __rread() const { return *(m_ptr.back()); };
+      ::ranges::span<T> __read_row() const { return {m_ptr[Rank - 2], static_cast<std::ptrdiff_t>(m_count.back())}; }
+
+      ::ranges::reverse_view<::ranges::span<T>> __read_rrow() const
       {
-        T& __read() const { return *(m_ptr.back()); };
-        T& __rread() const { return *(m_ptr.back()); };
-        ::ranges::span<T> __read_row() const { return {m_ptr[Rank - 2], static_cast<std::ptrdiff_t>(m_count.back())}; }
+        auto line = ::ranges::make_span(m_ptr[Rank - 2] - m_count.back() + 1, m_ptr[Rank - 2] + 1);
+        return ::ranges::view::reverse(line);
+      }
 
-        ::ranges::reverse_view<::ranges::span<T>> __read_rrow() const
-        {
-          auto line = ::ranges::make_span(m_ptr[Rank - 2] - m_count.back() + 1, m_ptr[Rank - 2] + 1);
-          return ::ranges::view::reverse(line);
-        }
+      bool __is_at_end(std::size_t k) const { return m_i[k] == m_count[k]; }
+      bool __is_at_rend(std::size_t k) const { return m_i[k] == m_count[k]; }
 
-        bool __is_at_end(std::size_t k) const { return m_i[k] == m_count[k]; }
-        bool __is_at_rend(std::size_t k) const { return m_i[k] == m_count[k]; }
-
-        void __next(std::size_t k)
-        {
-          m_i[k]++;
-          m_ptr[k] += m_stride[k];
-        }
-
-        void __rnext(std::size_t k)
-        {
-          m_i[k]++;
-          m_ptr[k] -= m_stride[k];
-        }
-
-        void __reset_to_begin(std::size_t k)
-        {
-          assert(k >= 1);
-          m_ptr[k] = m_ptr[k - 1];
-          m_i[k] = 0;
-        }
-
-        void __reset_to_rbegin(std::size_t k)
-        {
-          assert(k >= 1);
-          m_ptr[k] = m_ptr[k - 1];
-          m_i[k] = 0;
-        }
-
-        cursor() = default;
-        cursor(const multi_span& sp, bool forward = true) : m_count(sp.m_count), m_stride(sp.m_stride)
-        {
-          T* pointer = sp.m_ptr;
-          if (!forward) // Go to the last element
-          {
-            for (std::size_t k = 0; k < Rank; ++k)
-              pointer += (m_count[k] - 1) * m_stride[k];
-          }
-          m_ptr.fill(pointer);
-          m_i.fill(0);
-        }
-
-      private:
-        std::array<std::size_t, Rank> m_count;
-        std::array<std::ptrdiff_t, Rank> m_stride;
-        std::array<T*, Rank> m_ptr;
-        std::array<std::size_t, Rank> m_i;
-      };
-
-      multi_span() = default;
-      multi_span(T* pointer, std::array<std::size_t, Rank> count, std::array<std::ptrdiff_t, Rank> strides)
-          : m_ptr(pointer), m_count(count), m_stride(strides)
+      void __next(std::size_t k)
       {
+        m_i[k]++;
+        m_ptr[k] += m_stride[k];
+      }
+
+      void __rnext(std::size_t k)
+      {
+        m_i[k]++;
+        m_ptr[k] -= m_stride[k];
+      }
+
+      void __reset_to_begin(std::size_t k)
+      {
+        assert(k >= 1);
+        m_ptr[k] = m_ptr[k - 1];
+        m_i[k] = 0;
+      }
+
+      void __reset_to_rbegin(std::size_t k)
+      {
+        assert(k >= 1);
+        m_ptr[k] = m_ptr[k - 1];
+        m_i[k] = 0;
+      }
+
+      cursor() = default;
+      cursor(const multi_span& sp, bool forward = true) : m_count(sp.m_count), m_stride(sp.m_stride)
+      {
+        T* pointer = sp.m_ptr;
+        if (!forward) // Go to the last element
+        {
+          for (std::size_t k = 0; k < Rank; ++k)
+            pointer += (m_count[k] - 1) * m_stride[k];
+        }
+        m_ptr.fill(pointer);
+        m_i.fill(0);
       }
 
     private:
-      T* m_ptr;
       std::array<std::size_t, Rank> m_count;
       std::array<std::ptrdiff_t, Rank> m_stride;
+      std::array<T*, Rank> m_ptr;
+      std::array<std::size_t, Rank> m_i;
     };
-  } // namespace details
-
-  template <class T, std::size_t Rank>
-  class multi_span : public details::multi_view_decorator<Rank, details::multi_span<T, Rank>>
-  {
-    using base = details::multi_view_decorator<Rank, details::multi_span<T, Rank>>;
 
   public:
     multi_span() = default;
     multi_span(T* pointer, std::array<std::size_t, Rank> count, std::array<std::ptrdiff_t, Rank> strides)
-        : base(details::multi_span<T, Rank>(pointer, count, strides))
+        : m_ptr(pointer), m_count(count), m_stride(strides)
     {
     }
+
+  private:
+    T* m_ptr;
+    std::array<std::size_t, Rank> m_count;
+    std::array<std::ptrdiff_t, Rank> m_stride;
   };
 
 } // namespace mln::ranges
---
+++ mln/core/rangev3/range_traits.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <type_traits>
+
+#pragma once
+
+#include <mln/core/rangev3/range_traits.hpp>
+
+namespace mln::ranges
+{
+
+  template <class R, class = void>
+  struct has_reverse_method;
+
+  template <class R>
+  constexpr bool has_reverse_method_v = has_reverse_method<R>::value;
+
+/******************************************/
+/****          Implementation          ****/
+/******************************************/
+
+  template <class R, class>
+  struct has_reverse_method : std::false_type
+  {
+  };
+
+
+  template <class R>
+  struct has_reverse_method<R, std::void_t<decltype(std::declval<R>().reversed())>>
+    : std::true_type
+  {
+  };
+
+}

29de039406c117c8e67f734e3aa0eb8d1e540241
Michaël Roynard <michael.roynard@lrde.epita.fr>
2018-10-15 15:48:30 +0200

    Fix conanfile + gtest linkage

--- conanfile.py
+++ conanfile.py
@@ -8,8 +8,9 @@ class Pylene(ConanFile):
     url = "https://gitlab.lrde.epita.fr/olena/pylene"
     description = "Image processing library targeting genericity and efficiency."
     settings = "os", "compiler", "arch", "build_type", "cppstd"
-    options = {"benchmark": [True, False]}
-    default_options = "benchmark=True"
+    options = {"gtest": [True, False], "benchmark": [True, False], "freeimage": [
+        True, False], "boost_program_options": [True, False]}
+    default_options = "gtest=True", "benchmark=True", "freeimage=False", "boost_program_options=False"
     generators = "cmake_paths"
 
     def source(self):
@@ -17,13 +18,16 @@ class Pylene(ConanFile):
         git.clone("{}.git".format(self.url))
 
     def requirements(self):
-        if str(self.settings.compiler) == "Visual Studio":
-            tag = "stable-default"
-        else:
-            tag = "stable-{}-{}".format(self.settings.compiler,
-                                        str(self.settings.build_type).lower())
-
         self.requires("range-v3/0.3.7@ericniebler/stable")
-        self.requires("gtest/1.8.1@bincrafters/stable")
+
+        if self.options.gtest:
+            self.requires("gtest/1.8.1@bincrafters/stable")
+
         if self.options.benchmark:
-            self.requires("benchmark/1.4.1@dutiona/testing")
+            self.requires("benchmark/1.4.1@dutiona/stable")
+
+        if self.options.freeimage:
+            self.requires("freeimage/3.18.0@dutiona/stable")
+
+        if self.options.boost_program_options:
+            self.requires("boost_program_options/1.66.0@bincrafters/stable")

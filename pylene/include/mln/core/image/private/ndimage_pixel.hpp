8069c128588061c9bbb281eb244b9f78d17a342f
Michaël Roynard <michael.roynard@lrde.epita.fr>
2018-11-07 11:36:27 +0100

    Cherry-picked from development/neighborhood: Fix regression in linear benchmarks with gcc 8

--- pylene/include/mln/core/image/private/ndimage_pixel.hpp
+++ pylene/include/mln/core/image/private/ndimage_pixel.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <mln/core/rangev3/private/multi_view_facade.hpp>
+#include <mln/core/concept/pixel.hpp>
 #include <array>
 
 // Note: because of the CRTP, we do not want this to be inner class of ndimage<T, N, E>
@@ -21,26 +22,66 @@ namespace mln::details
   };
 
   template <class T, std::size_t N>
-  struct ndpix
+  struct ndpix_base : Pixel<ndpix_base<T, N>>
   {
     using point_type = typename ndimage_info<T, N>::point_type;
-
-    ndpix() = default;
+    using site_type = point_type;
+    using value_type = std::remove_const_t<T>;
+    using reference = T&;
 
     T& val()            const { return m_lineptr[m_point[N-1]]; }
     point_type point()  const { return m_point; }
-    void advance(point_type dp)
-    {
-      for (std::size_t k = 0; k < N - 1; ++k)
-        m_lineptr += m_info->stride[k] * dp[k];
-      m_point += dp;
-    }
 
-  protected:
-    const ndimage_info<T, N>*        m_info;
+
     T*                               m_lineptr;
     point_type                       m_point;
   };
+
+  template <class T, std::size_t N>
+  struct ndpix : ndpix_base<T, N>
+  {
+    using typename ndpix_base<T, N>::point_type;
+
+    // Overwrite value_type/reference for cursor
+    using value_type = ndpix;
+    using reference = ndpix;
+
+    void advance(const point_type& dp)
+    {
+      for (std::size_t k = 0; k < N - 1; ++k)
+        this->m_lineptr += m_info->stride[k] * dp[k];
+      this->m_point += dp;
+    }
+
+    const ndimage_info<T, N>*        m_info;
+
+  private:
+    friend ::ranges::range_access;
+    // Do not return by const& because reverse iterator does not support stashing
+    // (It creates a copy and return *it)
+    ndpix read() const { return *this; }
+
+    void next() { this->m_point[N-1]++; }
+    void prev() { this->m_point[N-1]--; }
+    bool equal(const ndpix& other) const { return this->m_point[N-1] == other.m_point[N-1]; }
+  };
+
+  template <class T, std::size_t N>
+  struct ndpixel : ndpix_base<T, N>
+  {
+    using typename ndpix_base<T, N>::point_type;
+
+    void advance(const point_type& dp)
+    {
+      for (std::size_t k = 0; k < N - 1; ++k)
+        this->m_lineptr += m_info.stride[k] * dp[k];
+      this->m_point += dp;
+    }
+
+  public:
+    ndimage_info<T, N>        m_info;
+  };
+
 
 
   template <class T, std::size_t N>
@@ -48,29 +89,24 @@ namespace mln::details
   {
     using point_type = mln::point<std::ptrdiff_t, N>;
 
+  public:
     struct row_t : public ndpix<T, N>, ::ranges::view_facade<row_t>
     {
       friend ::ranges::range_access;
     private:
-      struct cursor : public ndpix<T, N>
-      {
-        ndpix<T, N> read() const { return *this; } // Do not return by const& (reverse iterator does not support stashing)
-        void next() { this->m_point[N - 1]++; }
-        void prev() { this->m_point[N - 1]--; }
-        bool equal(const cursor& other) const { return this->m_point[N - 1] == other.m_point[N - 1]; }
-      };
+      using cursor_t = ndpix<T, N>;
 
-      cursor begin_cursor() const
+      cursor_t begin_cursor() const
       {
-        row_t tmp = *this;
+        cursor_t tmp = *this;
         tmp.m_point[N-1] = this->m_info->from[N-1];
-        return cursor{tmp};
+        return tmp;
       }
-      cursor end_cursor() const
+      cursor_t end_cursor() const
       {
-        row_t tmp = *this;
+        cursor_t tmp = *this;
         tmp.m_point[N-1] = this->m_info->to[N-1];
-        return cursor{tmp};
+        return tmp;
       }
 
     public:
@@ -81,6 +117,9 @@ namespace mln::details
   public:
     struct cursor : ndpix<T, N>
     {
+      using value_type = ndpix<T, N>;
+      using reference = const ndpix<T, N>&;
+
       const ndpix<T,N>& __read() const { return *this; }
       const ndpix<T,N>& __rread() const { return *this; }
       row_t __read_row() const { return row_t(*this); }

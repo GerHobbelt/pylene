124e5c396c42e0ce425a1e1b8eb6d9ad1b2fd87c
Michaël Roynard <michael.roynard@lrde.epita.fr>
2018-11-28 15:33:00 +0100

    Add concept check via require macro

--- pylene/include/mln/core/concept/new/concepts.hpp
+++ pylene/include/mln/core/concept/new/concepts.hpp
@@ -2,9 +2,10 @@
 
 #include <mln/core/concept/new/cmcstl2.hpp>
 
-#include <mln/core/concept/new/domains.hpp>
-#include <mln/core/concept/new/indexes.hpp>
-#include <mln/core/concept/new/pixels.hpp>
-#include <mln/core/concept/new/points.hpp>
 #include <mln/core/concept/new/ranges.hpp>
-#include <mln/core/concept/new/values.hpp>
+
+#ifdef PYLENE_CONCEPT_TS_ENABLED
+#define PYLENE_CONCEPT_REQUIRE_CHECK(Concept, ...) requires Concept<__VA_ARGS__>
+#else
+#define PYLENE_CONCEPT_REQUIRE_CHECK(Concept, ...)
+#endif // PYLENE_CONCEPT_TS_ENABLED

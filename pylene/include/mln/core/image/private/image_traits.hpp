fb009e7f8171625eb7f9bb83fd98242a97305bd8
Michaël Roynard <michael.roynard@lrde.epita.fr>
2018-12-01 14:23:03 +0100

    Add missing image traits

--- pylene/include/mln/core/image/private/image_traits.hpp
+++ pylene/include/mln/core/image/private/image_traits.hpp
@@ -12,4 +12,16 @@ namespace mln
   template <class I>
   using image_value_t = typename I::value_type;
 
+  template <class I>
+  using image_point_t = typename I::point_type;
+
+  template <class I>
+  using image_domain_t = typename I::domain_type;
+
+  template <class I>
+  using image_difference_t = typename I::difference_type;
+
+  template <class I>
+  using image_index_t = typename I::index_type;
+
 } // namespace mln

d673a9c8bf3906e114bf834504f8716587f69ea4
Michaël Roynard <michael.roynard@lrde.epita.fr>
2018-12-07 16:36:51 +0100

    Add OutputImage concept

--- pylene/include/mln/core/concept/new/images.hpp
+++ pylene/include/mln/core/concept/new/images.hpp
@@ -58,16 +58,26 @@ namespace mln::concepts
     stl::CommonReference<image_reference_t<Ima>&&, image_value_t<Ima>&> &&
 		stl::CommonReference<image_reference_t<Ima>&&, image_value_t<Ima>&&> &&
 		stl::CommonReference<image_value_t<Ima>&&, const image_value_t<Ima>&> &&
-    requires(Ima ima, image_domain_t<Ima> d, image_point_t<Ima> p) {
-      { ima.template ch_value<archetype::value_archetype>() }
+    requires(Ima ima, const Ima cima, image_domain_t<Ima> d, image_point_t<Ima> p) {
+      { cima.template ch_value<archetype::value_archetype>() }
                             -> stl::ConvertibleTo<image_ch_value_t<Ima, archetype::value_archetype>>&&; // Image builder (FIXME: improve builder design)
-      { ima.concretize() }  -> stl::ConvertibleTo<image_concrete_t<Ima>>&&;                             // Image builder (FIXME: improve builder design)
-      { ima.domain() }      -> image_domain_t<Ima>;
+      { cima.concretize() } -> stl::ConvertibleTo<image_concrete_t<Ima>>&&;                             // Image builder (FIXME: improve builder design)
+      { cima.domain() }     -> image_domain_t<Ima>;
       { ima.new_pixels() }  -> stl::ForwardRange&&;
       { ima.new_values() }  -> stl::ForwardRange&&;
 
       requires RangeValueTypeConvertibleTo<decltype(ima.new_pixels()), image_pixel_t<Ima>>;
       requires RangeValueTypeConvertibleTo<decltype(ima.new_values()), image_value_t<Ima>>;
+    };
+
+
+  // WritableImage
+  template<typename WIma>
+  concept WritableImage =
+    Image<WIma> &&
+    requires(WIma ima) {
+      { ima.new_pixels() }  -> stl::OutputRange<image_value_t<WIma>>&&;
+      { ima.new_values() }  -> stl::OutputRange<image_value_t<WIma>>&&;
     };
 
 
@@ -89,11 +99,21 @@ namespace mln::concepts
       typename image_index_t<Ima>;
     } &&
     image_indexable_v<Ima> &&
-    requires (Ima ima, image_index_t<Ima> k, image_point_t<Ima> p) {
-      { ima[k] }                -> image_reference_t<Ima>;
-      { ima.point_of_index(k) } -> image_point_t<Ima>;
-      { ima.index_at_point(p) } -> image_index_t<Ima>;
-      { ima.delta_index(p) }    -> image_index_t<Ima>;
+    requires (const Ima cima, image_index_t<Ima> k, image_point_t<Ima> p) {
+      { cima[k] }                -> image_reference_t<Ima>;
+      { cima.point_of_index(k) } -> image_point_t<Ima>;
+      { cima.index_at_point(p) } -> image_index_t<Ima>;
+      { cima.delta_index(p) }    -> image_index_t<Ima>;
+    };
+
+
+  // WritableIndexableImage
+  template<typename WIma>
+  concept WritableIndexableImage =
+    WritableImage<WIma> &&
+    IndexableImage<WIma> &&
+    requires(WIma ima, image_index_t<WIma> k, image_value_t<WIma> v) {
+      { ima[k] = v };
     };
 
 
@@ -102,23 +122,41 @@ namespace mln::concepts
   concept AccessibleImage =
     Image<Ima> &&
     image_accessible_v<Ima> &&
-    requires (Ima ima, image_point_t<Ima> p) {
-      { ima(p) }              -> image_reference_t<Ima>;
-      { ima.at(p) }           -> image_reference_t<Ima>;
-      { ima.new_pixel(p) }    -> image_pixel_t<Ima>;
-      { ima.new_pixel_at(p) } -> image_pixel_t<Ima>;
+    requires (const Ima cima, image_point_t<Ima> p) {
+      { cima(p) }              -> image_reference_t<Ima>;
+      { cima.at(p) }           -> image_reference_t<Ima>;
+      { cima.new_pixel(p) }    -> image_pixel_t<Ima>;
+      { cima.new_pixel_at(p) } -> image_pixel_t<Ima>;
+    };
+
+
+  // WritableAccessibleImage
+  template<typename WIma>
+  concept WritableAccessibleImage =
+    WritableImage<WIma> &&
+    AccessibleImage<WIma> &&
+    requires(WIma ima, image_point_t<WIma> p, image_value_t<WIma> v) {
+      { ima(p) = v };
+      { ima.at(p) = v };
     };
 
 
   // BidirectionalImage (not in STL term)
   template <typename Ima>
-    concept BidirectionalImage =
-      Image<Ima> &&
-      stl::DerivedFrom<image_category_t<Ima>, bidirectional_image_tag> &&
-      requires (Ima ima) {
-        { ima.new_pixels() }  -> ReversibleRange&&;
-        { ima.new_values() }  -> ReversibleRange&&;
-      };
+  concept BidirectionalImage =
+    Image<Ima> &&
+    stl::DerivedFrom<image_category_t<Ima>, bidirectional_image_tag> &&
+    requires (Ima ima) {
+      { ima.new_pixels() }  -> ReversibleRange&&;
+      { ima.new_values() }  -> ReversibleRange&&;
+    };
+
+
+  // WritableBidirectionalImage
+  template<typename WIma>
+  concept WritableBidirectionalImage =
+    WritableImage<WIma> &&
+    BidirectionalImage<WIma>;
 
 
   // RawImage (not contiguous, stride = padding)
@@ -134,6 +172,16 @@ namespace mln::concepts
     };
 
 
+  // WritableRawImage
+  template<typename WIma>
+  concept WritableRawImage =
+    WritableImage<WIma> &&
+    RawImage<WIma> &&
+    requires(WIma ima, image_value_t<WIma> v) {
+      { *(ima.data()) = v };
+    };
+
+
   // ExtendedImage
   template <typename Ima>
   concept ExtendedImage =
@@ -142,26 +190,20 @@ namespace mln::concepts
       typename image_extension_t<Ima>;
     } &&
     not stl::Same<mln::extension::none_extension_tag, image_extension_category_t<Ima>> &&
-    requires (Ima ima, image_point_t<Ima> p) {
-      { ima.extension() } -> image_extension_t<Ima>;
+    requires (const Ima cima, image_point_t<Ima> p) {
+      { cima.extension() } -> image_extension_t<Ima>;
     };
 
 
+  // OutputImage
+  template <typename Ima>
+  concept OutputImage =
+    (InputImage<Ima> && WritableImage<Ima>) || // alias for Image and ForwardImage
+    (IndexableImage<Ima> && WritableIndexableImage<Ima>) ||
+    (AccessibleImage<Ima> && WritableAccessibleImage<Ima>) ||
+    (BidirectionalImage<Ima> && WritableBidirectionalImage<Ima>) ||
+    (RawImage<Ima> && WritableRawImage<Ima>);
 
-
-/*
-template <RawImage I>
-OutputImage = WritableRawImage<I>;
-
-
-(WritableIndexableImage) ||
-(WritableRawImage)
- || ...
-   
-  RawImage && OutputImage
-   */
-
-   // TODO: writable
 // TODO: think about ViewImage vs. ConcreteImage
 
 #endif // PYLENE_CONCEPT_TS_ENABLED

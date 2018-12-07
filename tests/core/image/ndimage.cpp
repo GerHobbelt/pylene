700670edc39e3dc3153b2c41078e52e463af4fdf
Michaël Roynard <michael.roynard@lrde.epita.fr>
2018-12-07 16:43:10 +0100

    Fix clang compilation

--- tests/core/image/ndimage.cpp
+++ tests/core/image/ndimage.cpp
@@ -11,20 +11,12 @@
 namespace concepts = mln::concepts;
 
 
-#ifdef PYLENE_CONCEPT_TS_ENABLED
-template <concepts::Image Ima>
-void foo(Ima)
-{
-}
-#endif // PYLENE_CONCEPT_TS_ENABLED
-
 TEST(Core, Image_NdImage)
 {
 
   using tested_image_type = mln::ndimage_base<int, 3, mln::image2d<int>>;
 
   [[maybe_unused]] auto img = tested_image_type{};
-  foo(img);
 
 #ifdef PYLENE_CONCEPT_TS_ENABLED
   // static_assert(concepts::Image<decltype(img)>);

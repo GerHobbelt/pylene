21002d40848654118d7d0f4d41f8d556ee673ac1
Michaël Roynard <michael.roynard@lrde.epita.fr>
2018-12-11 16:26:09 +0100

    ndimage models outputimage

--- tests/core/image/ndimage.cpp
+++ tests/core/image/ndimage.cpp
@@ -35,23 +35,17 @@ TEST(Core, Image_Image3D)
 }
 
 
-#ifdef PYLENE_CONCEPT_TS_ENABLED
-template <concepts::detail::WritableImage Ima>
-void foo(Ima)
-{
-}
-template <concepts::Pixel Pix>
-void bar(Pix)
-{
-}
-#endif // PYLENE_CONCEPT_TS_ENABLED
-
 TEST(Core, Image_OutputImage2D)
 {
 #ifdef PYLENE_CONCEPT_TS_ENABLED
+  static_assert(concepts::OutputImage<mln::image2d<int>>);
+#endif // PYLENE_CONCEPT_TS_ENABLED
+}
 
-  foo(mln::image2d<int>{});
 
-  // static_assert(concepts::OutputImage<mln::image2d<int>>);
+TEST(Core, Image_OutputImage3D)
+{
+#ifdef PYLENE_CONCEPT_TS_ENABLED
+  static_assert(concepts::OutputImage<mln::image3d<int>>);
 #endif // PYLENE_CONCEPT_TS_ENABLED
 }

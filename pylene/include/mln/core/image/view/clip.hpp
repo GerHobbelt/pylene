70f26a5251ea3521356a56897c68c32a0def4d83
Michaël Roynard <michael.roynard@lrde.epita.fr>
2019-01-24 16:40:48 +0100

    Update clip (impl)

--- pylene/include/mln/core/image/view/clip.hpp
+++ pylene/include/mln/core/image/view/clip.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <mln/core/domain/private/domain_traits.hpp>
 #include <mln/core/image/image.hpp>
 #include <mln/core/image/view/adaptor.hpp>
 #include <mln/core/rangev3/view/transform.hpp>
@@ -22,28 +23,26 @@ namespace mln
     using typename clip_view::image_adaptor::point_type;
     using typename clip_view::image_adaptor::reference;
     using typename clip_view::image_adaptor::value_type;
-    using domain_type   = D;
-    using concrete_type = clip_view<image_concrete_t<I>, D>;
+    using domain_type = D;
 
-    template <class V>
-    using ch_value_type = clip_view<image_ch_value_t<I, V>, D>;
+    static_assert(std::is_convertible_v<domain_value_t<D>, point_type>,
+                  "Domain value type must be convertible to image point type.");
     /// \}
 
 
     /// Traits & Image Properties
     /// \{
     using accessible         = std::true_type;
-    using indexable          = typename I::indexable;
+    using indexable          = image_indexable_t<I>;
+    using view               = std::true_type;
     using extension_category = mln::extension::none_extension_tag; // FIXME: should be improved
+    using concrete_type      = clip_view<image_concrete_t<I>, D>;
+
+    template <class V>
+    using ch_value_type = clip_view<image_ch_value_t<I, V>, D>;
+
+    static_assert(image_accessible_v<I>, "Image must be pw-accessible.");
     /// \}
-
-
-    // Concepts check
-    static_assert(std::is_convertible_v<typename D::value_type, point_type>,
-                  "Domain value type must be convertible to image point type.");
-
-    static_assert(I::accessible::value, "Image must be pw-accessible.");
-
 
   public:
     clip_view(I ima, D domain)
@@ -55,14 +54,14 @@ namespace mln
 
     template <class I2, class D2>
     clip_view(const clip_view<I2, D2>& other, mln::init)
-      : clip_view::image_adaptor{static_cast<I>(imchvalue<value_type>(other.base()))}
+      : clip_view::image_adaptor{static_cast<I>(other.base().template ch_value<value_type>())}
       , m_domain{other.m_domain}
     {
     }
 
     template <class I2, class D2>
     clip_view(const clip_view<I2, D2>& other, const value_type& v)
-      : clip_view::image_adaptor{static_cast<I>(imchvalue<value_type>(other.base()).init(v))}
+      : clip_view::image_adaptor{static_cast<I>((other.base().template ch_value<value_type>()).init(v))}
       , m_domain{other.m_domain}
     {
     }
@@ -89,24 +88,78 @@ namespace mln
       return mln::ranges::view::transform(this->m_domain, g);
     }
 
-    reference operator()(point_type p)
+
+    /// Indexable-image related methods
+    /// \{
+    template <typename dummy = I>
+    reference operator[](image_index_t<dummy> i)
+    {
+      mln_precondition(i < m_domain.size());
+      auto p = domain()[i];
+      return this->base()(p);
+    }
+    /// \}
+
+
+    /// Accessible-image related methods
+    /// \{
+    template <typename Ret = reference>
+    std::enable_if_t<accessible::value, Ret> operator()(point_type p)
     {
       mln_precondition(m_domain.has(p));
       mln_precondition(this->base().domain().has(p));
+      return this->base()(p);
+    }
+    template <typename Ret = reference>
+    std::enable_if_t<accessible::value, Ret> at(point_type p)
+    {
       return this->base().at(p);
     }
-
-    reference at(point_type p) { return this->base().at(p); }
-
-
-    new_pixel_type new_pixel(point_type p)
+    template <typename Ret = new_pixel_type>
+    std::enable_if_t<accessible::value, Ret> new_pixel(point_type p)
     {
       mln_precondition(this->m_domain.has(p));
       mln_precondition(this->base().domain().has(p));
-      return this->clip_view::image_adaptor::new_pixel_at(p);
+      return this->base().new_pixel(p);
+    }
+    template <typename Ret = new_pixel_type>
+    std::enable_if_t<accessible::value, Ret> new_pixel_at(point_type p)
+    {
+      return this->base().new_pixel_at(p);
+    }
+    /// \}
+
+
+    /// IndexableAndAccessible-image related methods
+    /// \{
+    template <typename dummy = I>
+    std::enable_if_t<(indexable::value && accessible::value), image_index_t<dummy>> index_of_point(point_type p) const
+    {
+      // FIXME: how to implement this here ?
+      return this->base().index_of_point(p);
     }
 
-    new_pixel_type new_pixel_at(point_type p) { return this->clip_view::image_adaptor::new_pixel_at(p); }
+    template <typename dummy = I>
+    point_type point_at_index(std::enable_if_t<(indexable::value && accessible::value), image_index_t<dummy>> i) const
+    {
+      return domain()[i];
+    }
+
+    template <typename dummy = I>
+    std::enable_if_t<(indexable::value && accessible::value), image_index_t<dummy>> delta_index(point_type p) const
+    {
+      // FIXME: how to implement this here ?
+      return this->base().delta_index(p);
+    }
+    /// \}
+
+
+    /// Raw-image related methods
+    /// \{
+    auto data() const           = delete;
+    auto data()                 = delete;
+    auto strides(int dim) const = delete;
+    /// \}
   };
 
 

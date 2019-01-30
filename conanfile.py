10c879682b141468ccd4479fa833b30b3acd0c8b
Michaël Roynard <michael.roynard@lrde.epita.fr>
2019-01-30 14:42:37 +0100

    Update range-v3 dependency

--- conanfile.py
+++ conanfile.py
@@ -35,7 +35,7 @@ class Pylene(ConanFile):
 
     # Requirements part of the INTERFACE
     def requirements(self):
-        self.requires("range-v3/0.4.0@ericniebler/stable")
+        self.requires("range-v3/head@dutiona/testing")
         self.requires("cmcstl2/0.1@dutiona/testing")
 
         if self.options.freeimage:

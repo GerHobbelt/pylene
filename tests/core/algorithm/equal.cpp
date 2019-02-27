8b8d1eaaeb79dc5407822bf42ab669b20d9eea9d
Michaël Roynard <michael.roynard@lrde.epita.fr>
2019-02-27 11:59:28 +0100

    Add experimental equal

--- tests/core/algorithm/equal.cpp
+++ tests/core/algorithm/equal.cpp
@@ -19,3 +19,20 @@ TEST(Core, Algorithm_Equal)
   out(p)         = 12;
   ASSERT_TRUE(not mln::equal(ima, out));
 }
+
+TEST(Core, Algorithm_ExperimentalEqual)
+{
+  using namespace mln;
+
+  image2d<uint8> ima(10, 10);
+  image2d<uint8> out(10, 10);
+
+  iota(ima, 0);
+  iota(out, 0);
+
+  ASSERT_TRUE(experimental::equal(ima, out));
+
+  point2d p = {2, 3};
+  out(p)    = 12;
+  ASSERT_TRUE(not experimental::equal(ima, out));
+}

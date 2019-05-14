4d828a35a0abe339c8effd489d50ef0594905483
Celian Gossec <celian.gossec@epita.fr>
2019-05-14 13:40:13 +0200

    value_set: Adding divide function to replace normalize

--- pylene/include/py/core/value_set.hpp
+++ pylene/include/py/core/value_set.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <any>
 #include <limits>
+#include <iostream>
 
 namespace mln::py
 {
@@ -11,7 +13,10 @@ namespace mln::py
   template <>
   struct value_set<void>
   {
-    virtual float normalize(void* val) const = 0;
+    virtual float normalize(void* val) const = 0; //abstract normalize function for vs<void>
+    virtual std::any max() const = 0;
+    virtual std::any divide(void* val, std::any any) const = 0;
+    virtual std::any divide(void* val, void* any) const = 0;
   };
 
   template <typename T>
@@ -23,5 +28,67 @@ namespace mln::py
     }
 
     float normalize(T val) const { return static_cast<float>(val) / std::numeric_limits<T>::max(); }
+
+    std::any max() const override final
+    {
+        return std::any(std::numeric_limits<T>::max());
+    }
+
+    std::any divide(void* val, std::any denominator) const override final
+    {
+        std::any res;
+        try
+        {
+            res = static_cast<float>(*static_cast<T*>(val)) / std::any_cast<T>(denominator);
+        }
+        catch (const std::bad_any_cast& e)
+        {
+            std::cerr << "Error in 'divide(void*, std::any)': unhandled type.\n";
+        }
+        return res;
+    }
+
+    std::any divide(void* val, void *denominator) const override final
+    {
+        std::any res;
+        try
+        {
+            res = static_cast<float>(*static_cast<T*>(val)) / *static_cast<T*>(denominator);
+        }
+        catch (const std::bad_any_cast& e)
+        {
+            std::cerr << "Error in 'divide(void*, void*)': unhandled type.\n";
+        }
+        return res;
+    }
+
+    std::any divide(T val, std::any denominator) const
+    {
+        std::any res;
+        try
+        {
+            res = static_cast<float>(val) / std::any_cast<T>(denominator);
+        }
+        catch (const std::bad_any_cast& e)
+        {
+            std::cerr << "Error in 'divide(T, std::any)': unhandled type.\n";
+        }
+        return res;
+    }
+
+
+    std::any divide(T val, T denominator) const
+    {
+        std::any res;
+        try
+        {
+            res = static_cast<float>(val) / denominator;
+        }
+        catch (const std::bad_any_cast& e)
+        {
+            std::cerr << "Error in 'divide(T, T)': unhandled type.\n";
+        }
+        return res;
+    }
   };
 } // namespace mln::py

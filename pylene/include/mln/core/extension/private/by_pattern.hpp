eeecae90be029d7379c1e224b91e4e9f7fc7c02b
Michaël Roynard <michael.roynard@lrde.epita.fr>
2019-05-16 16:17:46 +0200

    WIP by_pattern extension

---
+++ pylene/include/mln/core/extension/private/by_pattern.hpp
@@ -0,0 +1,97 @@
+#pragma once
+
+#include <mln/core/concept/new/structuring_elements.hpp>
+
+#include <type_traits>
+#include <utility>
+
+
+namespace mln::extension
+{
+
+  template <typename V>
+  struct by_pattern
+  {
+    enum class Pattern
+    {
+      Mirror,
+      Periodize,
+      Clamp
+    };
+
+    using value_type        = V;
+    using support_fill      = std::false_type;
+    using support_mirror    = std::true_type;
+    using support_periodize = std::true_type;
+    using support_clamp     = std::true_type;
+    using is_finite         = std::false_type;
+
+    by_pattern(Pattern p, std::size_t padding = 0)
+      : m_pattern(p)
+      , m_padding(padding)
+    {
+    }
+
+    template <typename SE>
+    bool fit(const SE&) const
+    {
+      PYLENE_CONCEPT_TS_ASSERT(concepts::StructuringElement<SE>, "SE is not a valid Structuring Element!");
+
+      return true;
+    }
+
+    void mirror(std::size_t padding)
+    {
+      m_pattern = Pattern::Mirror;
+      m_padding = padding;
+    }
+
+    void periodize() { m_pattern = Pattern::Periodize; }
+
+    void clamp() { return m_pattern = Pattern::Clamp; }
+
+    Pattern pattern() const { return m_pattern; }
+
+    std::size_t padding() const { return m_padding; }
+
+    template <typename Pnt, typename Ima>
+    const V& yield(Pnt pnt, const Ima& ima) const
+    {
+      switch (m_pattern)
+      {
+      case Pattern::Mirror:
+        return yield_mirror(std::move(pnt), ima);
+      case Pattern::Periodize:
+        return yield_periodize(std::move(pnt), ima);
+      case Pattern::Clamp:
+        return yield_clamp(std::move(pnt), ima);
+      }
+    }
+
+  private:
+    template <typename Pnt, typename Ima>
+    const V& yield_mirror(Pnt pnt, const Ima& ima) const
+    {
+      (void)pnt;
+      (void)ima;
+    }
+
+    template <typename Pnt, typename Ima>
+    const V& yield_periodize(Pnt pnt, const Ima& ima) const
+    {
+      (void)pnt;
+      (void)ima;
+    }
+
+    template <typename Pnt, typename Ima>
+    const V& yield_clamp(Pnt pnt, const Ima& ima) const
+    {
+      (void)pnt;
+      (void)ima;
+    }
+
+    Pattern     m_pattern;
+    std::size_t m_padding;
+  };
+
+} // namespace mln::extension
---
+++ pylene/include/mln/core/image/view/pattern_extended.hpp
@@ -0,0 +1,130 @@
+#pragma once
+
+#include <mln/core/extension/extension_traits.hpp>
+#include <mln/core/extension/private/by_pattern.hpp>
+#include <mln/core/image/view/adaptor.hpp>
+#include <mln/core/rangev3/view/transform.hpp>
+
+#include <range/v3/utility/common_type.hpp> // common_reference
+
+
+namespace mln
+{
+  template <class I>
+  class pattern_extended_view;
+
+  namespace view
+  {
+    template <class I>
+    pattern_extended_view<I> pattern_extended(I image, image_value_t<I> value);
+  };
+
+
+  /******************************************/
+  /****          Implementation          ****/
+  /******************************************/
+
+
+  template <class I>
+  class pattern_extended_view : public image_adaptor<I>, public experimental::Image<pattern_extended_view<I>>
+  {
+    using base_t = image_adaptor<I>;
+
+  public:
+    using extension_category = extension::experimental::value_tag;
+    using extension_type     = extension::by_pattern<image_value_t<I>>;
+    using reference          = const image_value_t<I>&; // Restrict the image to be read-only
+    using category_type      = std::common_type_t<image_category_t<I>, bidirectional_image_tag>;
+    using point_type         = image_point_t<I>;
+    using typename image_adaptor<I>::domain_type;
+
+    struct new_pixel_type : pixel_adaptor<image_pixel_t<I>>, experimental::Pixel<new_pixel_type>
+    {
+      using reference = pattern_extended_view::reference;
+
+      reference val() const
+      {
+        if (m_dom.has(this->base().point()))
+        {
+          return this->base().val();
+        }
+        else
+        {
+          return m_extptr->yield(this->base().point(), *m_imaptr);
+        }
+      }
+
+      new_pixel_type(image_pixel_t<I> px, extension_type* ext, domain_type dom, const I* ima)
+        : new_pixel_type::pixel_adaptor{std::move(px)}
+        , m_extptr{ext}
+        , m_imaptr(ima)
+        , m_dom{std::move(dom)}
+
+      {
+      }
+
+    private:
+      extension_type* m_extptr;
+      I*              m_imaptr;
+      domain_type     m_dom;
+    };
+
+  private:
+    extension_type m_ext;
+
+  public:
+    pattern_extended_view(I ima, image_value_t<I> value)
+      : base_t{std::move(ima)}
+      , m_ext{std::move(value)}
+    {
+    }
+
+    /// Accessible-image related methods
+    /// \{
+    template <class J = I>
+    std::enable_if_t<image_accessible_v<J>, reference> operator()(point_type p)
+    {
+      return this->at(p);
+    }
+
+
+    template <class J = I>
+    std::enable_if_t<image_accessible_v<J>, reference> at(point_type p)
+    {
+      return this->domain().has(p) ? this->base().at(p) : m_ext.value();
+    }
+
+    template <class J = I>
+    std::enable_if_t<image_accessible_v<J>, new_pixel_type> new_pixel(point_type p)
+    {
+      return this->new_pixel_at(p);
+    }
+
+    template <class J = I>
+    std::enable_if_t<image_accessible_v<J>, new_pixel_type> new_pixel_at(point_type p)
+    {
+      return {this->base().new_pixel(p), &m_ext, this->domain(), &this->base()};
+    }
+    /// \}
+
+    auto new_pixels()
+    {
+      return ranges::view::transform(this->base().new_pixels(), [this](image_pixel_t<I> px) -> new_pixel_type {
+        return {std::move(px), &this->m_ext, this->domain(), &this->base()};
+      });
+    }
+
+    const extension_type& extension() { return m_ext; }
+  };
+
+
+  namespace view
+  {
+    template <class I>
+    pattern_extended_view<I> pattern_extended(I image, image_value_t<I> value)
+    {
+      return {std::move(image), std::move(value)};
+    }
+  }; // namespace view
+
+} // namespace mln

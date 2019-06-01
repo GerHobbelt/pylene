aa575d04423b71e2bbcd4b7d05e3a150014ab8ef
Celian Gossec <celian.gossec@epita.fr>
2019-06-01 14:59:01 +0200

    tests: any_ref: adding tests that need tweaking before being accurate

--- tests/py/any_ref.cpp
+++ tests/py/any_ref.cpp
@@ -5,10 +5,18 @@
 #include <gtest/gtest.h>
 
 
-TEST(Py, any_ref_ctor)
+TEST(Py, any_ref_ctor_std_any)
 {
   using namespace mln::py;
   std::any a = std::any(true);
   any_ref obj = any_ref(a);
-  ASSERT_EQ(obj.data(), a);
+  ASSERT_EQ(obj.as<bool>(), true);
+}
+
+TEST(Py, any_ref_ctor_void_ptr)
+{
+  using namespace mln::py;
+  int a = 7;
+  any_ref obj = any_ref(a);
+  ASSERT_EQ(obj.as<int>(), a);
 }

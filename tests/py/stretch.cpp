e06f40d4d5f0a1403f5cfc8c671210151e7c213a
Celian Gossec <celian.gossec@epita.fr>
2019-06-10 23:30:31 +0200

    Update: tests: adding test for stretch on templated images (should not go through any_ref conversion. benchmarkes have yet to be done)

--- tests/py/stretch.cpp
+++ tests/py/stretch.cpp
@@ -5,7 +5,7 @@
 #include <gtest/gtest.h>
 
 
-TEST(Py, stretch)
+TEST(Py, stretch_void)
 {
   using namespace mln::py;
 
@@ -15,6 +15,26 @@ TEST(Py, stretch)
     buffer[i] = i;
   }
   auto img       = image2d<>::from_buffer(reinterpret_cast<std::byte*>(buffer), 3, 3, Info::INT8_V);
+  auto stretched = stretch(img);
+
+  int j = 0;
+  for (auto i : stretched.values())
+  {
+      EXPECT_EQ(i, static_cast<float>(j++) / std::numeric_limits<int8_t>::max());
+  }
+  delete[] buffer;
+}
+
+TEST(Py, stretch_T)
+{
+  using namespace mln::py;
+
+  int8_t* buffer = new int8_t[9];
+  for (int i = 0; i < 9; ++i)
+  {
+    buffer[i] = i;
+  }
+  auto img       = image2d<int8_t>::from_buffer(buffer, 3, 3);
   auto stretched = stretch(img);
 
   int j = 0;

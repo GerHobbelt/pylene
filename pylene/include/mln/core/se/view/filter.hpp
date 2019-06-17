16375a73a3bad657eb9d0f6a7edeb5037732aa5f
Michaël Roynard <michael.roynard@lrde.epita.fr>
2019-06-17 11:24:28 +0200

    Rename func -> pred

--- pylene/include/mln/core/se/view/filter.hpp
+++ pylene/include/mln/core/se/view/filter.hpp
@@ -10,15 +10,15 @@
 
 namespace mln
 {
-  template <class Func, class SE>
+  template <class Pred, class SE>
   class se_filter_view : public se_adaptor<SE>
   {
     using base_t = se_adaptor<SE>;
 
   public:
-    se_filter_view(Func func, SE se)
+    se_filter_view(Pred pred, SE se)
       : base_t{std::move(se)}
-      , m_func{std::move(func)}
+      , m_pred{std::move(pred)}
     {
     }
 
@@ -26,7 +26,7 @@ namespace mln
     auto operator()(const P& pixel) const
     {
       return ::ranges::view::filter(details::sliding_pixel_range{pixel, static_cast<const SE*>(this)->offsets()},
-                                    [this](auto pix) { return m_func(pix.point()); });
+                                    [this](auto pix) { return m_pred(pix.point()); });
     }
 
     template <class P, std::enable_if_t<!is_a<P, mln::experimental::Pixel>::value, int> = 0>
@@ -34,19 +34,19 @@ namespace mln
     {
       return ::ranges::view::filter(::ranges::view::transform(static_cast<const SE*>(this)->offsets(),
                                                               [point](P offset) -> P { return point + offset; }),
-                                    m_func);
+                                    m_pred);
     }
 
   private:
-    Func m_func;
+    Pred m_pred;
   };
 
   namespace view
   {
-    template <class Func, class SE>
-    se_filter_view<Func, SE> se_filter(Func&& func, SE&& se)
+    template <class Pred, class SE>
+    se_filter_view<Pred, SE> se_filter(Pred&& pred, SE&& se)
     {
-      return {std::forward<Func>(func), std::forward<SE>(se)};
+      return {std::forward<Pred>(pred), std::forward<SE>(se)};
     }
   } // namespace view
 } // namespace mln

8e9f9f81fbec4ed381f1576191aae2fa62187a50
Michaël Roynard <michael.roynard@lrde.epita.fr>
2019-10-30 16:22:43 +0100

    Formating

--- apps/tip.2019-2020/gen_figs.cpp
+++ apps/tip.2019-2020/gen_figs.cpp
@@ -1,24 +1,24 @@
+#include <mln/core/colors.hpp>
+#include <mln/core/grays.hpp>
+#include <mln/core/image/experimental/ndimage.hpp>
 #include <mln/core/image/image2d.hpp>
 #include <mln/core/image/views.hpp>
-#include <mln/core/image/experimental/ndimage.hpp>
-#include <mln/core/grays.hpp>
-#include <mln/core/colors.hpp>
 #include <mln/io/experimental/imread.hpp>
 #include <mln/io/experimental/imsave.hpp>
 
 
 int main()
 {
-  auto in = mln::io::experimental::imread("images/lena_color.png");
+  auto  in     = mln::io::experimental::imread("images/lena_color.png");
   auto* in_ptr = in.cast_to<mln::rgb8, 2>();
 
   mln::io::experimental::imsave(mln::view::red(*in_ptr), "images/lena_red_mono.png");
   mln::io::experimental::imsave(mln::view::green(*in_ptr), "images/lena_green_mono.png");
   mln::io::experimental::imsave(mln::view::blue(*in_ptr), "images/lena_blue_mono.png");
 
-  auto filter_red = [](auto v) -> mln::rgb8 { return {v[0], 0, 0}; };
+  auto filter_red   = [](auto v) -> mln::rgb8 { return {v[0], 0, 0}; };
   auto filter_green = [](auto v) -> mln::rgb8 { return {0, v[1], 0}; };
-  auto filter_blue = [](auto v) -> mln::rgb8 { return {0, 0, v[2]}; };
+  auto filter_blue  = [](auto v) -> mln::rgb8 { return {0, 0, v[2]}; };
 
   mln::io::experimental::imsave(mln::view::transform(*in_ptr, filter_red), "images/lena_red.png");
   mln::io::experimental::imsave(mln::view::transform(*in_ptr, filter_green), "images/lena_green.png");

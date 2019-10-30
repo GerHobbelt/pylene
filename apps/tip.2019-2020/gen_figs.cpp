369be23dfdd972813c6a17fb09428dc277df1c79
Michaël Roynard <michael.roynard@lrde.epita.fr>
2019-10-30 16:21:19 +0100

    Generate rgb views figures

--- apps/tip.2019-2020/gen_figs.cpp
+++ apps/tip.2019-2020/gen_figs.cpp
@@ -12,7 +12,15 @@ int main()
   auto in = mln::io::experimental::imread("images/lena_color.png");
   auto* in_ptr = in.cast_to<mln::rgb8, 2>();
 
-  mln::io::experimental::imsave(mln::view::green(*in_ptr), "images/lena_green.png");
-  mln::io::experimental::imsave(mln::view::red(*in_ptr), "images/lena_red.png");
-  mln::io::experimental::imsave(mln::view::blue(*in_ptr), "images/lena_blue.png");
+  mln::io::experimental::imsave(mln::view::red(*in_ptr), "images/lena_red_mono.png");
+  mln::io::experimental::imsave(mln::view::green(*in_ptr), "images/lena_green_mono.png");
+  mln::io::experimental::imsave(mln::view::blue(*in_ptr), "images/lena_blue_mono.png");
+
+  auto filter_red = [](auto v) -> mln::rgb8 { return {v[0], 0, 0}; };
+  auto filter_green = [](auto v) -> mln::rgb8 { return {0, v[1], 0}; };
+  auto filter_blue = [](auto v) -> mln::rgb8 { return {0, 0, v[2]}; };
+
+  mln::io::experimental::imsave(mln::view::transform(*in_ptr, filter_red), "images/lena_red.png");
+  mln::io::experimental::imsave(mln::view::transform(*in_ptr, filter_green), "images/lena_green.png");
+  mln::io::experimental::imsave(mln::view::transform(*in_ptr, filter_blue), "images/lena_blue.png");
 }

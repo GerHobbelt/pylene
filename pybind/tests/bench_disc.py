7d43e7b6488a63f5e43782f3971b5fa8071ac927
Michaël Roynard <michael.roynard@lrde.epita.fr>
2020-01-29 10:28:23 +0100

    Update benchmark scripts

--- pybind/tests/bench_disc.py
+++ pybind/tests/bench_disc.py
@@ -16,6 +16,7 @@ sizes = {"width": 3138, "height": 3138}
 number = 100
 percent = 20
 
+
 def setup_test_img():
     global sizes
     ref = np.zeros((sizes["width"], sizes["height"]), dtype="uint8")
@@ -28,20 +29,20 @@ def setup_test_img():
         ref[x, y] = 255
     return ref
 
-ref = setup_test_img()
 
-
-def test_pylena():
-    disc = pln.se.disc(float(radius))
-    pln.morpho.dilate(ref, disc)
+# def test_pylena():
+#     disc = pln.se.disc(float(radius))
+#     pln.morpho.dilate(ref, disc)
 
 def test_pylena_decomp():
     disc = pln.se.disc_decomposable(float(radius))
     pln.morpho.dilate(ref, disc)
 
+
 def test_sckimage():
     disc = skimorph.disk(radius, dtype="uint8")
     skimorph.binary_dilation(ref, disc)
+
 
 def test_cv2():
     disc = cv2.getStructuringElement(
@@ -50,12 +51,13 @@ def test_cv2():
 
 
 sizes_list = [sizes]
-radius_list = [1+2**r for r in range(1, 6)]
+radius_list = [1+2**r for r in range(1, 8)]
 
 
 def get_sizes():
     for s in sizes_list:
         yield s
+
 
 def get_radius():
     for r in radius_list:
@@ -64,52 +66,57 @@ def get_radius():
 
 def print_results(times):
     for t in times:
-        print("Img {}x{} SE rad={} (nb={}): Pln={}, PlnD={}, Skimg={}, CV={}".format(
-            t["sizes"]["width"], t["sizes"]["height"], t["radius"], t["number"], t["Pylena"], t["PylenaD"], t["Skimage"], t["OpenCV"]
+        print("Img {}x{} SE rad={} (nb={}): PlnD={}, Skimg={}, CV={}".format(
+            t["sizes"]["width"], t["sizes"]["height"], t["radius"], t["number"],
+            # t["Pylena"],
+            t["PylenaD"], t["Skimage"], t["OpenCV"]
         ))
+
 
 def plot_results_by_SE(times, size):
     x = radius_list
-    y_pln = []
+    # y_pln = []
     y_plnD = []
     y_skimage = []
     y_cv = []
     for t in times:
         if t["sizes"]["width"] == size["width"] and t["sizes"]["height"] == size["height"]:
-            y_pln.append(t["Pylena"])
+            # y_pln.append(t["Pylena"])
             y_plnD.append(t["PylenaD"])
             y_skimage.append(t["Skimage"])
             y_cv.append(t["OpenCV"])
 
     plt.figure("Benchmarks: Time vs. SE size")
 
-    plt.plot(x, y_pln, label="Pylena")
+    # plt.plot(x, y_pln, label="Pylena")
     plt.plot(x, y_plnD, label="Pylena Decomp")
     plt.plot(x, y_skimage, label="Skimage")
     plt.plot(x, y_cv, label="OpenCV")
 
     plt.xlabel("SE radius size")
     plt.ylabel("Time (s)")
+    plt.yscale("log")
     plt.title(
         "dilation ({}x{}) image, {} iterations".format(size["width"], size["height"], number))
     plt.legend()
 
+
 def plot_results_by_pixels(times, radius):
     x = [x["width"] * x["height"] for x in sizes_list]
-    y_pln = []
+    # y_pln = []
     y_plnD = []
     y_skimage = []
     y_cv = []
     for t in times:
         if t["radius"] == radius:
-            y_pln.append(t["Pylena"])
+            # y_pln.append(t["Pylena"])
             y_plnD.append(t["PylenaD"])
             y_skimage.append(t["Skimage"])
             y_cv.append(t["OpenCV"])
 
     plt.figure("Benchmarks: Time vs. Image size")
 
-    plt.plot(x, y_pln, label="Pylena")
+    # plt.plot(x, y_pln, label="Pylena")
     plt.plot(x, y_plnD, label="Pylena Decomp")
     plt.plot(x, y_skimage, label="Skimage")
     plt.plot(x, y_cv, label="OpenCV")
@@ -135,32 +142,47 @@ def main_bench():
                 number, s["width"], s["height"], r
             ))
 
+            # print("Starting Pylena...")
+            # time_pln = timeit.timeit(
+            #     "test_pylena()",
+            #     setup="from __main__ import test_pylena",
+            #     number=number
+            # )
+            # print("End Pylena")
+
+            print("Starting Pylena decomposable...")
+            time_pln_decomp = timeit.timeit(
+                "test_pylena_decomp()",
+                setup="from __main__ import test_pylena_decomp",
+                number=number
+            )
+            print("End Pylena decomposable")
+
+            print("Starting scikit-image...")
+            time_skimage = timeit.timeit(
+                "test_sckimage()",
+                setup="from __main__ import test_sckimage",
+                number=number
+            )
+            print("End scikit-image")
+
+            print("Starting OpenCV...")
+            time_cv2 = timeit.timeit(
+                "test_cv2()",
+                setup="from __main__ import test_cv2",
+                number=number
+            )
+            print("End OpenCV")
+
             times.append(
                 {
                     "number": number,
                     "sizes": s,
                     "radius": r,
-                    "Pylena": timeit.timeit(
-                        "test_pylena()",
-                        setup="from __main__ import test_pylena",
-                        number=number
-                    ),
-                    "PylenaD": timeit.timeit(
-                        "test_pylena_decomp()",
-                        setup="from __main__ import test_pylena_decomp",
-                        number=number
-                    ),
-                    "Skimage":
-                    timeit.timeit(
-                        "test_sckimage()",
-                        setup="from __main__ import test_sckimage",
-                        number=number
-                    ),
-                    "OpenCV": timeit.timeit(
-                        "test_cv2()",
-                        setup="from __main__ import test_cv2",
-                        number=number
-                    )
+                    # "Pylena": time_pln,
+                    "PylenaD": time_pln_decomp,
+                    "Skimage": time_skimage,
+                    "OpenCV": time_cv2
                 }
             )
 
@@ -174,7 +196,7 @@ if __name__ == "__main__":
     plt.figure("Original image")
     plt.imshow(ref)
 
-    plot_results_by_SE(times, {"width": 1000, "height": 1000})
-    plot_results_by_pixels(times, radius=15)
+    plot_results_by_SE(times, {"width": 3138, "height": 3138})
+    # plot_results_by_pixels(times, radius=15)
 
     plt.show()
--- pybind/tests/bench_rect.py
+++ pybind/tests/bench_rect.py
@@ -13,9 +13,10 @@ import cv2
 
 rect_width = 3
 rect_height = 3
-sizes = {"width": 3138, "height": 3138} # 10Mo
+sizes = {"width": 3138, "height": 3138}  # 10Mo
 number = 100
 percent = 20
+
 
 def setup_test_img():
     global sizes
@@ -29,20 +30,20 @@ def setup_test_img():
         ref[x, y] = 255
     return ref
 
-ref = setup_test_img()
 
-
-def test_pylena():
-    rect = pln.se.rect2d(rect_width, rect_height)
-    pln.morpho.dilate(ref, rect)
+# def test_pylena():
+#     rect = pln.se.rect2d(rect_width, rect_height)
+#     pln.morpho.dilate(ref, rect)
 
 def test_pylena_decomp():
     rect = pln.se.rect2d_decomposable(rect_width, rect_height)
     pln.morpho.dilate(ref, rect)
 
+
 def test_sckimage():
     rect = skimorph.rectangle(rect_width, rect_height, dtype="uint8")
     skimorph.binary_dilation(ref, rect)
+
 
 def test_cv2():
     rect = cv2.getStructuringElement(
@@ -51,12 +52,13 @@ def test_cv2():
 
 
 sizes_list = [sizes]
-rect_sizes_list = [{"width": 1+2**x, "height": 1+2**x} for x in range(1, 9)]
+rect_sizes_list = [{"width": 1+2**x, "height": 1+2**x} for x in range(1, 8)]
 
 
 def get_sizes():
     for s in sizes_list:
         yield s
+
 
 def get_rect_sizes():
     for x in rect_sizes_list:
@@ -65,53 +67,58 @@ def get_rect_sizes():
 
 def print_results(times):
     for t in times:
-        print("Img {}x{} SE rect={}x{} (nb={}): Pln={}, PlnD={}, Skimg={}, CV={}".format(
+        print("Img {}x{} SE rect={}x{} (nb={}): PlnD={}, Skimg={}, CV={}".format(
             t["sizes"]["width"], t["sizes"]["height"], t["rect"]["width"], t["rect"]["height"],
-            t["number"], t["Pylena"], t["PylenaD"], t["Skimage"], t["OpenCV"]
+            t["number"],
+            # t["Pylena"],
+            t["PylenaD"], t["Skimage"], t["OpenCV"]
         ))
+
 
 def plot_results_by_SE(times, size):
     x = [x["width"] for x in rect_sizes_list]
-    y_pln = []
+    # y_pln = []
     y_plnD = []
     y_skimage = []
     y_cv = []
     for t in times:
         if t["sizes"]["width"] == size["width"] and t["sizes"]["height"] == size["height"]:
-            y_pln.append(t["Pylena"])
+            # y_pln.append(t["Pylena"])
             y_plnD.append(t["PylenaD"])
             y_skimage.append(t["Skimage"])
             y_cv.append(t["OpenCV"])
 
     plt.figure("Benchmarks: Time vs. SE size")
 
-    plt.plot(x, y_pln, label="Pylena")
+    # plt.plot(x, y_pln, label="Pylena")
     plt.plot(x, y_plnD, label="Pylena Decomp")
     plt.plot(x, y_skimage, label="Skimage")
     plt.plot(x, y_cv, label="OpenCV")
 
     plt.xlabel("SE square edge size")
     plt.ylabel("Time (s)")
+    plt.yscale("log")
     plt.title(
         "dilation ({}x{}) image, {} iterations".format(size["width"], size["height"], number))
     plt.legend()
 
+
 def plot_results_by_pixels(times, rect_width, rect_height):
     x = [x["width"] * x["height"] for x in sizes_list]
-    y_pln = []
+    # y_pln = []
     y_plnD = []
     y_skimage = []
     y_cv = []
     for t in times:
         if t["rect"]["width"] == rect_width and t["rect"]["height"] == rect_height:
-            y_pln.append(t["Pylena"])
+            # y_pln.append(t["Pylena"])
             y_plnD.append(t["PylenaD"])
             y_skimage.append(t["Skimage"])
             y_cv.append(t["OpenCV"])
 
     plt.figure("Benchmarks: Time vs. Image size")
 
-    plt.plot(x, y_pln, label="Pylena")
+    # plt.plot(x, y_pln, label="Pylena")
     plt.plot(x, y_plnD, label="Pylena Decomp")
     plt.plot(x, y_skimage, label="Skimage")
     plt.plot(x, y_cv, label="OpenCV")
@@ -138,32 +145,47 @@ def main_bench():
                 number, s["width"], s["height"], rect["width"], rect["height"]
             ))
 
+            # print("Starting Pylena...")
+            # time_pln = timeit.timeit(
+            #     "test_pylena()",
+            #     setup="from __main__ import test_pylena",
+            #     number=number
+            # )
+            # print("End Pylena")
+
+            print("Starting Pylena decomposable...")
+            time_pln_decomp = timeit.timeit(
+                "test_pylena_decomp()",
+                setup="from __main__ import test_pylena_decomp",
+                number=number
+            )
+            print("End Pylena decomposable")
+
+            print("Starting scikit-image...")
+            time_skimage = timeit.timeit(
+                "test_sckimage()",
+                setup="from __main__ import test_sckimage",
+                number=number
+            )
+            print("End scikit-image")
+
+            print("Starting OpenCV...")
+            time_cv2 = timeit.timeit(
+                "test_cv2()",
+                setup="from __main__ import test_cv2",
+                number=number
+            )
+            print("End OpenCV")
+
             times.append(
                 {
                     "number": number,
                     "sizes": s,
                     "rect": rect,
-                    "Pylena": timeit.timeit(
-                        "test_pylena()",
-                        setup="from __main__ import test_pylena",
-                        number=number
-                    ),
-                    "PylenaD": timeit.timeit(
-                        "test_pylena_decomp()",
-                        setup="from __main__ import test_pylena_decomp",
-                        number=number
-                    ),
-                    "Skimage":
-                    timeit.timeit(
-                        "test_sckimage()",
-                        setup="from __main__ import test_sckimage",
-                        number=number
-                    ),
-                    "OpenCV": timeit.timeit(
-                        "test_cv2()",
-                        setup="from __main__ import test_cv2",
-                        number=number
-                    )
+                    # "Pylena": time_pln,
+                    "PylenaD": time_pln_decomp,
+                    "Skimage": time_skimage,
+                    "OpenCV": time_cv2
                 }
             )
 
@@ -177,7 +199,7 @@ if __name__ == "__main__":
     plt.figure("Original image")
     plt.imshow(ref)
 
-    plot_results_by_SE(times, {"width": 1000, "height": 1000})
-    plot_results_by_pixels(times, rect_width=15, rect_height=15)
+    plot_results_by_SE(times, {"width": 3138, "height": 3138})
+    # plot_results_by_pixels(times, rect_width=15, rect_height=15)
 
     plt.show()

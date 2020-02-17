403a626a71535117015ee60ed859c25cb10401b2
Edwin Carlinet <carlinet@lrde.epita.fr>
2020-02-17 17:19:36 +0100

    Migrate labeling

--- tests/labeling/blobs.cpp
+++ tests/labeling/blobs.cpp
@@ -1,8 +1,8 @@
-#include <mln/labeling/blobs.hpp>
+#include <mln/labeling/experimental/blobs.hpp>
 
 #include <mln/core/algorithm/iota.hpp>
 #include <mln/core/algorithm/transform.hpp>
-#include <mln/core/image/ndimage.hpp>
+#include <mln/core/image/experimental/ndimage.hpp>
 #include <mln/core/image/view/operators.hpp>
 #include <mln/core/neighborhood/c4.hpp>
 #include <mln/core/neighborhood/c8.hpp>
@@ -16,13 +16,13 @@ TEST(Labeling, blobs_U)
 {
   using namespace mln::view::ops;
 
-  mln::image2d<bool> ima = {{0, 1, 0, 0, 0, 1, 0}, //
+  mln::experimental::image2d<bool> ima = {{0, 1, 0, 0, 0, 1, 0}, //
                                           {0, 1, 0, 1, 0, 1, 0}, //
                                           {0, 1, 0, 0, 0, 1, 0}, //
                                           {0, 1, 1, 1, 1, 1, 0}, //
                                           {0, 1, 1, 1, 1, 1, 0}};
 
-  mln::image2d<uint8_t> ref = {{0, 1, 0, 0, 0, 1, 0}, //
+  mln::experimental::image2d<uint8_t> ref = {{0, 1, 0, 0, 0, 1, 0}, //
                                              {0, 1, 0, 2, 0, 1, 0}, //
                                              {0, 1, 0, 0, 0, 1, 0}, //
                                              {0, 1, 1, 1, 1, 1, 0}, //
@@ -30,7 +30,7 @@ TEST(Labeling, blobs_U)
 
 
   int nlabel;
-  auto out = mln::labeling::blobs<uint8_t>(ima, mln::c4, nlabel);
+  auto out = mln::labeling::experimental::blobs<uint8_t>(ima, mln::experimental::c4, nlabel);
   ASSERT_EQ(nlabel, 2);
   ASSERT_IMAGES_EQ_EXP(ref, out);
 }
@@ -41,13 +41,13 @@ TEST(Labeling, blobs_fast)
 {
   using namespace mln::view::ops;
 
-  mln::image2d<uint8_t> ima(5, 5);
+  mln::experimental::image2d<uint8_t> ima(5, 5);
   mln::iota(ima, 0);
 
   auto mask = mln::transform(ima, [](int x) { return x % 2 == 0; });
 
   int nlabel;
-  auto out = mln::labeling::blobs<uint8_t>(mask, mln::c4, nlabel);
+  auto out = mln::labeling::experimental::blobs<uint8_t>(mask, mln::experimental::c4, nlabel);
   ASSERT_EQ(nlabel, 13);
   ASSERT_IMAGES_EQ_EXP(mask, out > 0);
 }
@@ -56,18 +56,18 @@ TEST(Labeling, blobs_custom)
 {
   using namespace mln::view::ops;
 
-  mln::image2d<uint8_t> ima(5, 5);
+  mln::experimental::image2d<uint8_t> ima(5, 5);
   mln::iota(ima, 0);
 
   int nlabel;
   {
-    auto out = mln::labeling::blobs<uint8_t>(ima % 2 == 0, mln::c4, nlabel);
+    auto out = mln::labeling::experimental::blobs<uint8_t>(ima % 2 == 0, mln::experimental::c4, nlabel);
     ASSERT_IMAGES_EQ_EXP(ima % 2 == 0, out > 0);
     ASSERT_EQ(nlabel, 13);
   }
 
   {
-    auto out = mln::labeling::blobs<uint8_t>(ima % 2 == 0, mln::c8, nlabel);
+    auto out = mln::labeling::experimental::blobs<uint8_t>(ima % 2 == 0, mln::experimental::c8, nlabel);
     ASSERT_IMAGES_EQ_EXP(ima % 2 == 0, out > 0);
     ASSERT_EQ(nlabel, 1u);
   }

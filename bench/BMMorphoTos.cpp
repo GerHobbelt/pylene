88aa786fe88c16e578f41229b686c8bac2a21695
Edwin Carlinet <carlinet@lrde.epita.fr>
2020-02-18 09:11:42 +0100

    Clean Old API

--- bench/BMMorphoTos.cpp
+++ bench/BMMorphoTos.cpp
@@ -3,13 +3,8 @@
 #include <mln/core/image/experimental/ndimage.hpp>
 
 #include <mln/core/neighborhood/c4.hpp>
-#include <mln/core/neighb2d.hpp>
-
 #include <mln/io/experimental/imread.hpp>
-
 #include <mln/morpho/experimental/tos.hpp>
-#include <mln/morpho/tos/tos.hpp>
-
 
 #include <benchmark/benchmark.h>
 
@@ -19,7 +14,6 @@ class BMMorpho : public benchmark::Fixture
 {
 public:
   using image_t = mln::experimental::image2d<uint8_t>;
-  using image_ref_t = mln::image2d<uint8_t>;
 
   BMMorpho()
   {
@@ -37,10 +31,6 @@ public:
     int nc = m_input.height();
     mln::resize(m_output, m_input);
     m_size = nr * nc;
-
-    m_input.to(m_input_, false);
-    m_output.to(m_output_, false);
-
   }
 
 
@@ -51,23 +41,12 @@ public:
     st.SetBytesProcessed(int64_t(st.iterations()) * int64_t(m_size));
   }
 
-  void run2(benchmark::State& st, std::function<void(const image_ref_t& input)> callback)
-  {
-    for (auto _ : st)
-      callback(m_input_);
-    st.SetBytesProcessed(int64_t(st.iterations()) * int64_t(m_size));
-  }
-
-
 protected:
   static bool                                g_loaded;
   static mln::experimental::image2d<uint8_t> g_input;
   mln::experimental::image2d<uint8_t>        m_input;
   mln::experimental::image2d<uint8_t>        m_output;
   std::size_t                                m_size;
-
-  mln::image2d<uint8_t>               m_input_;
-  mln::image2d<uint8_t>               m_output_;
 };
 
 bool                                BMMorpho::g_loaded = false;
@@ -79,12 +58,6 @@ BENCHMARK_F(BMMorpho, ToSNew)(benchmark::State& st)
 {
   auto f = [](const image_t& input) { mln::morpho::experimental::tos(input, input.domain().tl()); };
   this->run(st, f);
-}
-
-BENCHMARK_F(BMMorpho, ToSRef)(benchmark::State& st)
-{
-  auto f = [](const image_ref_t& input) { mln::morpho::tos(input); };
-  this->run2(st, f);
 }
 
 BENCHMARK_MAIN();

d50da11d367fb64be53d3eb6e1cd3566fd8a2f12
Edwin Carlinet <edwin.carlinet@lrde.epita.fr>
2020-03-05 23:31:37 +0100

    Fix conan test package.

--- test_package/main.cpp
+++ test_package/main.cpp
@@ -1,15 +1,21 @@
 #include <mln/core/algorithm/iota.hpp>
-#include <mln/core/image/image2d.hpp>
-#include <mln/io/imprint.hpp>
-#include <mln/io/imsave.hpp>
+#include <mln/core/image/experimental/ndimage.hpp>
+#include <mln/io/experimental/imprint.hpp>
+#include <mln/io/experimental/imread.hpp>
+#include <mln/io/experimental/imsave.hpp>
 
 
 int main()
 {
-  mln::image2d<mln::uint8> f(5, 5);
+  mln::experimental::image2d<uint8_t> f(5, 5);
 
   mln::iota(f, 0);
 
-  mln::io::imprint(f);
-  mln::io::imsave(f, "test.pgm");
+  mln::io::experimental::imprint(f);
+  mln::io::experimental::imsave(f, "test.pgm");
+
+  mln::experimental::box2d b;
+  b.x();
+
+  mln::io::experimental::imread("test.pgm", f);
 }

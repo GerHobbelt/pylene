1cfc36c1639e7ff5f9e883e812c9414ae316dce2
Celian GOSSEC <celian.gossec@epita.fr>
2020-03-24 20:55:51 +0100

    Add comment explaining wrapper + move wrapper inside parallel_execute

--- pylene/include/mln/core/algorithm/parallel_pointwise_canvas.hpp
+++ pylene/include/mln/core/algorithm/parallel_pointwise_canvas.hpp
@@ -78,27 +78,28 @@ namespace mln
   };
 */
 
-  struct ParallelForWrapper
-  {
-    ParallelCanvas2d* canvas;
-    ParallelForWrapper(ParallelCanvas2d* ptr)
-      : canvas{ptr}
-    {}
-
-    void operator()(const tbb::blocked_range2d<int>& tile) const
-    {
-      std::invoke(*canvas, tile);
-    }
-  };
-
   /*
   ** Caller for the TBB parallel_for
   ** Rationale being that every pointwise algorithm applies a function to every pixel,
   ** and each algorithm can take input image(s) as well as an output image, hence the variadInputImage
+  ** We create a wrapper class to circumvent TBB not allowing abstract classes as parallel_for body
   */
   void parallel_execute2d(ParallelCanvas2d& canvas)
   {
-    mln::ParallelForWrapper wrapper(&canvas);
+    struct ParallelForWrapper
+    {
+      ParallelCanvas2d* canvas;
+      ParallelForWrapper(ParallelCanvas2d* ptr)
+        : canvas{ptr}
+      {}
+
+      void operator()(const tbb::blocked_range2d<int>& tile) const
+      {
+        std::invoke(*canvas, tile);
+      }
+    };
+
+    ParallelForWrapper wrapper(&canvas);
     mln::experimental::box2d domain = wrapper.canvas->GetDomain();
     tbb::parallel_for(tbb::blocked_range2d<int>(domain.y(), domain.y() + domain.height(), wrapper.canvas->TILE_HEIGHT, //
                                                 domain.x(), domain.x() + domain.width(), wrapper.canvas->TILE_WIDTH),

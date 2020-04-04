8bd7340a6f37e0112ef223fca3394abd2522c30a
Edwin Carlinet <edwin.carlinet@lrde.epita.fr>
2020-04-04 19:26:20 +0200

    Update first start in doc with new API.

--- doc/source/snippets/first_start_1.cpp
+++ doc/source/snippets/first_start_1.cpp
@@ -1,32 +1,39 @@
-#include <mln/core/image/image2d.hpp>
+#include <mln/core/image/experimental/ndimage.hpp>
+
+#include <mln/core/rangev3/view/reverse.hpp>
+#include <mln/core/rangev3/foreach.hpp>
+
+#include <fmt/core.h>
+#include <iostream>
 
 int main()
 {
-  mln::image2d<int> f = {{1, 2, 3}, {4, 5, 6}};
+  mln::experimental::image2d<int> f = {{1, 2, 3}, {4, 5, 6}};
 
   std::cout << "== Traversing forward ==\n";
   std::cout << "Traversing through points.\n";
-  mln_foreach (mln::point2d p, f.domain())
-    std::cout << p << ":" << f(p) << "\n";
+  mln_foreach_new (auto p, f.domain())
+    fmt::print("f({},{}) = {}\n", p.x(), p.y(), f(p));
 
   std::cout << "Traversing on values.\n";
-  mln_foreach (int x, f.values())
+  mln_foreach_new (int x, f.new_values())
     std::cout << x << "\n";
 
   std::cout << "Traversing with pixels.\n";
-  mln_foreach (auto x, f.pixels())
-    std::cout << x.point() << ":" << x.val() << "\n";
+  mln_foreach_new (auto px, f.new_pixels())
+    fmt::print("f({},{}) = {}\n", px.point().x(), px.point().y(), px.val());
+
 
   std::cout << "\n== Traversing backward ==\n";
   std::cout << "Traversing through points.\n";
-  mln_reverse_foreach (mln::point2d p, f.domain())
-    std::cout << p << ":" << f(p) << "\n";
+  mln_foreach_new (auto p, mln::ranges::view::reverse(f.domain()))
+    fmt::print("f({},{}) = {}\n", p.x(), p.y(), f(p));
 
   std::cout << "Traversing on values.\n";
-  mln_reverse_foreach (int x, f.values())
+  mln_foreach_new (auto x, mln::ranges::view::reverse(f.new_values()))
     std::cout << x << "\n";
 
   std::cout << "Traversing with pixels.\n";
-  mln_reverse_foreach (auto x, f.pixels())
-    std::cout << x.point() << ":" << x.val() << "\n";
+  mln_foreach_new (auto px, mln::ranges::view::reverse(f.new_pixels()))
+    fmt::print("f({},{}) = {}\n", px.point().x(), px.point().y(), px.val());
 }

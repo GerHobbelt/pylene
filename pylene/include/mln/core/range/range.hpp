11c9df19a3b35cbe44509ccb26f8311254dd0310
Edwin Carlinet <edwin.carlinet@lrde.epita.fr>
2020-04-07 17:56:58 +0200

    Remove old range API.

--- pylene/include/mln/core/range/range.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <mln/core/config.hpp>
-#include <mln/core/range/has.hpp>
-#include <mln/core/range/iter.hpp>
-#include <mln/core/range/range_traits.hpp>
-#include <mln/core/range/size.hpp>

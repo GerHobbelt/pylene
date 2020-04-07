2189fcf8056ec2772b0e4a73689d563731d68082
Edwin Carlinet <edwin.carlinet@lrde.epita.fr>
2020-04-07 19:17:29 +0200

    Remove old pixel/image/neigborhood/concepts.

--- pylene/include/mln/core/algorithm/iota.hpp
+++ pylene/include/mln/core/algorithm/iota.hpp
@@ -28,7 +28,7 @@ namespace mln
   template <typename OutputImage, typename Value>
   void iota(OutputImage output, Value val)
   {
-    static_assert(mln::is_a<OutputImage, mln::Image>::value || mln::is_a<OutputImage, mln::experimental::Image>::value);
+    static_assert(mln::is_a<OutputImage, mln::experimental::Image>::value);
     static_assert(std::is_convertible_v<Value, image_value_t<OutputImage>>);
 
     auto&& vals = output.new_values();
--- pylene/include/mln/core/neighborhood/neighborhood.hpp
+++ pylene/include/mln/core/neighborhood/neighborhood.hpp
@@ -1,5 +1,4 @@
 #pragma once
 
-#include <mln/core/concept/neighborhood.hpp>
 #include <mln/core/config.hpp>
 #include <mln/core/neighborhood/neighborhood_traits.hpp>

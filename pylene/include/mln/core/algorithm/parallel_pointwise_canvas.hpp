6b5ef8ce8320987d5d87e13f5bd4db71f1828727
Celian GOSSEC <celian.gossec@epita.fr>
2020-04-12 14:03:50 +0200

    Split parallel pointwise in source/header files

--- pylene/include/mln/core/algorithm/parallel_pointwise_canvas.hpp
+++ pylene/include/mln/core/algorithm/parallel_pointwise_canvas.hpp
@@ -1,37 +1,30 @@
 #pragma once
 
-#include "tbb/parallel_for.h"
-#include "tbb/blocked_range2d.h"
-#include "mln/core/box.hpp"
-#include "mln/core/concept/object.hpp"
-#include <mln/core/image/image.hpp>
-#include <range/v3/functional/concepts.hpp>
+#include <tbb/blocked_range2d.h>
+#include <mln/core/box.hpp>
+
 #include <mln/core/algorithm/for_each.hpp>
 
 namespace mln
 {
-
-
   struct ParallelCanvas2d
   {
     static constexpr int TILE_WIDTH  = 128;
     static constexpr int TILE_HEIGHT = 128;
 
-
-    void operator()(const tbb::blocked_range2d<int>& tile) const
-    {
-      int x = tile.cols().begin();
-      int w = tile.cols().end() - tile.cols().begin();
-      int y = tile.rows().begin();
-      int h = tile.rows().end() - tile.rows().begin();
-
-      mln::experimental::box2d domain(x, y, w, h);
-      this->ExecuteTile(domain);
-    }
+    void operator()(const tbb::blocked_range2d<int>&) const;
 
     virtual mln::experimental::box2d GetDomain() const = 0;
     virtual void ExecuteTile(mln::experimental::box2d b) const = 0;
   };
+
+  /*
+  ** Caller for the TBB parallel_for
+  ** Rationale being that every pointwise algorithm applies a function to every pixel,
+  ** and each algorithm can take input image(s) as well as an output image, hence the variadInputImage
+  ** We create a wrapper class to circumvent TBB not allowing abstract classes as parallel_for body
+  */
+  void parallel_execute2d(ParallelCanvas2d&);
 
 
   template <class Function, class InputImage>
@@ -77,32 +70,4 @@ namespace mln
     }
   };
 */
-
-  /*
-  ** Caller for the TBB parallel_for
-  ** Rationale being that every pointwise algorithm applies a function to every pixel,
-  ** and each algorithm can take input image(s) as well as an output image, hence the variadInputImage
-  ** We create a wrapper class to circumvent TBB not allowing abstract classes as parallel_for body
-  */
-  void parallel_execute2d(ParallelCanvas2d& canvas)
-  {
-    struct ParallelForWrapper
-    {
-      ParallelCanvas2d* canvas;
-      ParallelForWrapper(ParallelCanvas2d* ptr)
-        : canvas{ptr}
-      {}
-
-      void operator()(const tbb::blocked_range2d<int>& tile) const
-      {
-        std::invoke(*canvas, tile);
-      }
-    };
-
-    ParallelForWrapper wrapper(&canvas);
-    mln::experimental::box2d domain = wrapper.canvas->GetDomain();
-    tbb::parallel_for(tbb::blocked_range2d<int>(domain.y(), domain.y() + domain.height(), wrapper.canvas->TILE_HEIGHT, //
-                                                domain.x(), domain.x() + domain.width(), wrapper.canvas->TILE_WIDTH),
-                      wrapper);
-  }
 } // namespace mln
---
+++ pylene/src/core/parallel_pointwise.cpp
@@ -0,0 +1,47 @@
+#include <mln/core/algorithm/parallel_pointwise_canvas.hpp>
+
+#include <tbb/parallel_for.h>
+#include <tbb/blocked_range2d.h>
+
+namespace mln
+{
+  void ParallelCanvas2d::operator()(const tbb::blocked_range2d<int>& tile) const
+  {
+    int x = tile.cols().begin();
+    int w = tile.cols().end() - tile.cols().begin();
+    int y = tile.rows().begin();
+    int h = tile.rows().end() - tile.rows().begin();
+
+    mln::experimental::box2d domain(x, y, w, h);
+    this->ExecuteTile(domain);
+  }
+
+
+  /*
+  ** Caller for the TBB parallel_for
+  ** Rationale being that every pointwise algorithm applies a function to every pixel,
+  ** and each algorithm can take input image(s) as well as an output image, hence the variadInputImage
+  ** We create a wrapper class to circumvent TBB not allowing abstract classes as parallel_for body
+  */
+  void parallel_execute2d(ParallelCanvas2d& canvas)
+  {
+    struct ParallelForWrapper
+    {
+      ParallelCanvas2d* canvas;
+      ParallelForWrapper(ParallelCanvas2d* ptr)
+        : canvas{ptr}
+      {}
+
+      void operator()(const tbb::blocked_range2d<int>& tile) const
+      {
+        std::invoke(*canvas, tile);
+      }
+    };
+
+    ParallelForWrapper wrapper(&canvas);
+    mln::experimental::box2d domain = wrapper.canvas->GetDomain();
+    tbb::parallel_for(tbb::blocked_range2d<int>(domain.y(), domain.y() + domain.height(), wrapper.canvas->TILE_HEIGHT, //
+                                                domain.x(), domain.x() + domain.width(), wrapper.canvas->TILE_WIDTH),
+                      wrapper);
+  }
+} // namespace mln

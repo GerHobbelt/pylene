d69b7f31d7de259a4ea01060a75148ee54538438
Michaël Roynard <michaelroynard@gmail.com>
2020-05-15 14:52:24 +0200

    Fix

--- pybind/tests/plot_bench_from_gbenchjson.py
+++ pybind/tests/plot_bench_from_gbenchjson.py
@@ -2,7 +2,8 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
-import json, re
+import json
+import re
 
 sns.set()
 sns.set_style("whitegrid")
@@ -11,7 +12,8 @@ sns.set_context("paper")
 
 bench_result = json.load(open("BMPlnVsOpenCV_Dilation_result.json"))
 
-benchmarks = {'Pln_DiscDecompPeriodic': [], 'Pln_RectDecomp': [], 'CV_Disc': [], 'CV_Rect': []}
+benchmarks = {'Pln_DiscDecompPeriodic': [],
+              'Pln_RectDecomp': [], 'CV_Disc': [], 'CV_Rect': []}
 size_se_p = re.compile(".*/([0-9]+)")
 indexes = []
 for bench in bench_result['benchmarks']:
@@ -27,7 +29,8 @@ for bench in bench_result['benchmarks']:
                 'throughput_bps': bench['bytes_per_second']
             })
 
-data = {'Pln_DiscDecompPeriodic': [], 'Pln_RectDecomp': [], 'CV_Disc': [], 'CV_Rect': []}
+data = {'Pln_DiscDecompPeriodic': [],
+        'Pln_RectDecomp': [], 'CV_Disc': [], 'CV_Rect': []}
 indexes = []
 
 for k in benchmarks.keys():
@@ -35,9 +38,11 @@ for k in benchmarks.keys():
         data[k].append(ret['time'])
 
 _k = list(benchmarks.keys())[0]
-for ret in benchmarks[_k]:fig.savefig("PlnVsOpenCV.png")
+for ret in benchmarks[_k]:
+    indexes.append(ret['size_se'])
 
-df = pd.DataFrame(data=data, index=indexes, columns=['Pln_DiscDecompPeriodic', 'Pln_RectDecomp', 'CV_Disc', 'CV_Rect'])
+df = pd.DataFrame(data=data, index=indexes, columns=[
+                  'Pln_DiscDecompPeriodic', 'Pln_RectDecomp', 'CV_Disc', 'CV_Rect'])
 
 fig = sns.lineplot(data=df, legend="full")
 fig.set(yscale="log", xlabel="SE size", ylabel="Time (ns)")

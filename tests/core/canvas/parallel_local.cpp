4ddb8ae47f83b9cabd2320e067b3bdf99afcec93
Celian GOSSEC <celian.gossec@epita.fr>
2020-05-27 20:52:56 +0200

    Add basically empty UT file for parallel local canvas

---
+++ tests/core/canvas/parallel_local.cpp
@@ -0,0 +1,15 @@
+#include <mln/core/canvas/parallel_local.hpp>
+
+#include <mln/core/image/ndimage.hpp>
+#include <mln/core/algorithm/equal.hpp>
+
+#include <gtest/gtest.h>
+
+
+TEST(Core, copy_buffer_into)
+{
+  mln::image2d<uint8_t> ref = {{0, 0, 0}, {0, 0, 0},{0, 0, 0}};
+  int* buffer = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+  mln::TileExecutorBase::copy_buffer_into(buffer, ref);
+  ASSERT_TRUE(mln::equal(buffer, ref));
+}

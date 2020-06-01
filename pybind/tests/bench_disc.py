83b6d4beaf6f37bb29cf15d0291444a85a341cd3
Michaël Roynard <michaelroynard@gmail.com>
2020-06-01 15:44:31 +0200

    Update bench for grayscale instead of binary

--- pybind/tests/bench_disc.py
+++ pybind/tests/bench_disc.py
@@ -20,12 +20,9 @@ def setup_test_img():
     global sizes
     ref = np.zeros((sizes["width"], sizes["height"]), dtype="uint8")
     rnd.seed(42)
-    nb_pixels = sizes["width"]*sizes["height"]
-    nb_pixels_white = int(nb_pixels * percent / 100)
-    for i in range(0, nb_pixels_white):
-        x = rnd.randint(0, sizes["width"]-1)
-        y = rnd.randint(0, sizes["height"]-1)
-        ref[x, y] = 255
+    for x in range(0, sizes["width"]):
+        for y in range(0, sizes["height"]):
+            ref[x, y] = rnd.randint(0, 255)
     return ref
 
 
@@ -40,7 +37,7 @@ def test_pylena_decomp():
 
 def test_sckimage():
     disc = skimorph.disk(radius, dtype="uint8")
-    skimorph.binary_dilation(ref, disc)
+    skimorph.dilation(ref, disc)
 
 
 def test_cv2():

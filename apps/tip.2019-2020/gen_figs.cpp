0a91da10d8982ec92ea800061e3978c486e84d50
Michaël Roynard <michaelroynard@gmail.com>
2020-06-17 18:38:55 +0200

    WIP functional pipeline

--- apps/tip.2019-2020/gen_figs.cpp
+++ apps/tip.2019-2020/gen_figs.cpp
@@ -5,14 +5,24 @@
 #include <mln/core/colors.hpp>
 #include <mln/core/image/experimental/ndimage.hpp>
 #include <mln/core/image/image.hpp>
+#include <mln/core/image/view/operators.hpp>
 #include <mln/core/image/views.hpp>
+#include <mln/core/se/disc.hpp>
 #include <mln/io/experimental/imread.hpp>
 #include <mln/io/experimental/imsave.hpp>
+#include <mln/morpho/experimental/dilation.hpp>
+#include <mln/morpho/experimental/erosion.hpp>
+#include <mln/morpho/experimental/gaussian_directional_2d.hpp>
+
 
 #include <cmath>
 #include <iostream>
+#include <string>
+#include <string_view>
+#include <utility>
 #include <vector>
 
+using namespace std::literals;
 
 constexpr double pi()
 {
@@ -124,8 +134,133 @@ mln::experimental::image2d<uint8_t> gaussian_blur(Ima input, std::size_t sigma)
   return output;
 }
 
+namespace bg_sub
+{
+  namespace detail
+  {
+    template <size_t N>
+    constexpr std::string_view sv(const char (&literal)[N])
+    {
+      return std::string_view(literal, N - 1);
+    }
+
+    template <size_t... N>
+    constexpr std::array<std::string_view, sizeof...(N)> sva(const char (&... literals)[N])
+    {
+      return {{sv(literals)...}};
+    }
+
+    template <size_t... Idx>
+    constexpr std::array<std::pair<std::string_view, std::string_view>, sizeof...(Idx)>
+    swap_impl(std::array<std::string_view, sizeof...(Idx)> la, std::array<std::string_view, sizeof...(Idx)> ra,
+              std::index_sequence<Idx...>)
+    {
+      return {std::make_pair(la[Idx], ra[Idx])...};
+    }
+
+    template <size_t N, size_t M>
+    constexpr std::array<std::pair<std::string_view, std::string_view>, N> svap(std::array<std::string_view, N> la,
+                                                                                std::array<std::string_view, M> ra)
+    {
+      static_assert(N == M, "Wrong size");
+      using Indexes = std::make_index_sequence<N>;
+      return swap_impl(la, ra, Indexes{});
+    }
+  } // namespace detail
+
+  constexpr auto filenames_base = detail::sva("a.jpg", "b.jpg", "c.jpg", "d.jpg", "e.jpg", "f.jpg", "g.jpg", "h.jpg",
+                                              "i.jpg", "g.jpg", "k.jpg", "l.jpg", "m.jpg");
+
+  constexpr auto filenames_bg =
+      detail::sva("a_bg.jpg", "b_bg.jpg", "c_bg.jpg", "d_bg.jpg", "e_bg.jpg", "f_bg.jpg", "g_bg.jpg", "h_bg.jpg",
+                  "i_bg.jpg", "g_bg.jpg", "k_bg.jpg", "l_bg.jpg", "m_bg.jpg");
+
+  constexpr std::string_view file_path     = "images/bg_sub_samples";
+  constexpr std::string_view file_path_tmp = "images/bg_sub_tmp";
+
+  constexpr auto filenames = detail::svap(filenames_base, filenames_bg);
+
+} // namespace bg_sub
+
+void test_deriche_gaussian2d(std::string_view filename, std::string_view filename_bg)
+{
+  mln::experimental::image2d<mln::rgb8> img_color, bg_color;
+  std::cout << "deriche gaussian2d" << std::endl;
+  std::cout << (std::string{bg_sub::file_path} + '/' + std::string{filename}) << std::endl;
+  std::cout << (std::string{bg_sub::file_path} + '/' + std::string{filename_bg}) << std::endl;
+  mln::io::experimental::imread(std::string{bg_sub::file_path} + '/' + std::string{filename}, img_color);
+  mln::io::experimental::imread(std::string{bg_sub::file_path} + '/' + std::string{filename_bg}, bg_color);
+
+  auto grayscale = [](auto v) -> uint8_t { return 0.2126 * v[0] + 0.7152 * v[1] + 0.0722 * v[2]; };
+
+  auto img_grey = mln::view::transform(img_color, grayscale);
+  auto bg_grey  = mln::view::transform(bg_color, grayscale);
+
+  mln::io::experimental::imsave(img_grey, std::string{bg_sub::file_path_tmp} + '/' + std::string{filename});
+  mln::io::experimental::imsave(bg_grey, std::string{bg_sub::file_path_tmp} + '/' + std::string{filename_bg});
+
+  const float kLineHeight = 10;
+  const float kWordWidth  = 10;
+
+  const float kLineVerticalSigma   = (kLineHeight * 0.5f) * 0.1f;
+  const float kLineHorizontalSigma = (kWordWidth * 0.5f) * 1.f;
+
+  auto bg_blurred = mln::morpho::experimental::gaussian2d(bg_grey, kLineVerticalSigma, kLineHorizontalSigma, 255);
+
+  mln::io::experimental::imsave(bg_blurred,
+                                std::string{bg_sub::file_path_tmp} + "/blurred_"s + std::string{filename_bg});
+}
+
+
+void test_bg_sub_pipeline(std::string_view filename, std::string_view filename_bg)
+{
+  // Input Color
+  mln::experimental::image2d<mln::rgb8> img_color, bg_color;
+  mln::io::experimental::imread(std::string{bg_sub::file_path} + '/' + std::string{filename}, img_color);
+  mln::io::experimental::imread(std::string{bg_sub::file_path} + '/' + std::string{filename_bg}, bg_color);
+
+  // GrayScale (view)
+  auto grayscale = [](auto v) -> uint8_t { return 0.2126 * v[0] + 0.7152 * v[1] + 0.0722 * v[2]; };
+
+  auto img_grey = mln::view::transform(img_color, grayscale);
+  auto bg_grey  = mln::view::transform(bg_color, grayscale);
+
+  // Gaussian on BG (algo)
+  const float kLineHeight          = 5;
+  const float kWordWidth           = 5;
+  const float kLineVerticalSigma   = (kLineHeight * 0.5f) * 0.1f;
+  const float kLineHorizontalSigma = (kWordWidth * 0.5f) * 1.f;
+  auto bg_blurred = mln::morpho::experimental::gaussian2d(bg_grey, kLineVerticalSigma, kLineHorizontalSigma, 255);
+
+  // Substract (view)
+  using namespace mln::view::ops;
+  auto tmp_grey = img_grey - bg_blurred;
+
+  // thresholding (view)
+  float threshold       = 150;
+  auto  thesholding_fun = [threshold](auto x) -> uint8_t { return (x < threshold) ? 0 : 255; };
+  auto  tmp_thresholded = mln::view::transform(tmp_grey, thesholding_fun);
+
+  // erosion (algo)
+  mln::experimental::se::disc win(3);
+  auto                        tmp_eroded = mln::morpho::experimental::erosion(tmp_thresholded, win);
+
+  // dilation (algo)
+  auto tmp_dilated = mln::morpho::experimental::dilation(tmp_eroded, win);
+
+  // output
+  mln::io::experimental::imsave(tmp_dilated,
+                                std::string{bg_sub::file_path_tmp} + "/result_"s + std::string{filename_bg});
+}
+
 int main()
 {
+  for (auto&& img : bg_sub::filenames)
+  {
+    test_deriche_gaussian2d(img.first, img.second);
+    test_bg_sub_pipeline(img.first, img.second);
+  }
+
   mln::experimental::image2d<mln::rgb8> lena_color;
   mln::io::experimental::imread("images/lena_color.png", lena_color);
 

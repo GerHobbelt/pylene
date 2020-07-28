331c4fdf218ffe63f58e834fcd0aa1d6d5d2d73d
Edwin Carlinet <edwin.carlinet@lrde.epita.fr>
2020-07-28 12:37:16 +0200

    Fix benchmark.

--- bench/opencv/BMPlnVsOpenCV_bgSubPipeline.cpp
+++ bench/opencv/BMPlnVsOpenCV_bgSubPipeline.cpp
@@ -13,16 +13,17 @@
 #include <opencv2/opencv.hpp>
 
 #include <array>
-#include <iostream>
 #include <string>
 #include <string_view>
 #include <utility>
-#include <variant>
 #include <vector>
 
 #include <malloc.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <fmt/core.h>
+
+bool ENABLE_MEMORY_LOG = false;
 
 
 using namespace std::literals;
@@ -54,22 +55,21 @@ namespace details
   void log_memory_usage(const std::string& label = std::string())
   {
     struct mallinfo m = mallinfo();
-    std::cout << "mallinfo:" << label << std::endl;
-    std::cout << "  non-mmapped space allocated from system =" << m.arena << std::endl;
-    std::cout << "  number of free chunks =" << m.ordblks << std::endl;
-    std::cout << "  number of fastbin blocks =" << m.smblks << std::endl;
-    std::cout << "  number of mmapped regions =" << m.hblks << std::endl;
-    std::cout << "  space in mmapped regions =" << m.hblkhd << std::endl;
-    std::cout << "  maximum total allocated space =" << m.usmblks << std::endl;
-    std::cout << "  space available in freed fastbin blocks =" << m.fsmblks << std::endl;
-    std::cout << "  total allocated space =" << m.uordblks << std::endl;
-    std::cout << "  total free space =" << m.fordblks << std::endl;
+    fmt::print("mallinfo: {}\n", label);
+    fmt::print("  non-mmapped space allocated from system = {}\n", m.arena);
+    fmt::print("  number of free chunks = {}\n", m.ordblks);
+    fmt::print("  number of fastbin blocks = {}\n", m.smblks);
+    fmt::print("  number of mmapped regions = {}\n", m.hblks);
+    fmt::print("  space in mmapped regions = {}\n", m.hblkhd);
+    fmt::print("  maximum total allocated space = {}\n", m.usmblks);
+    fmt::print("  space available in freed fastbin blocks = {}\n", m.fsmblks);
+    fmt::print("  total allocated space = {}\n", m.uordblks);
+    fmt::print("  total free space = {}\n", m.fordblks);
   }
 } // namespace details
 
 std::vector<std::pair<std::string, std::string>> filenames = {
     {"a.jpg", "a_bg.jpg"}, //
-    /*
     {"b.jpg", "b_bg.jpg"}, //
     {"c.jpg", "c_bg.jpg"}, //
     {"d.jpg", "d_bg.jpg"}, //
@@ -82,7 +82,6 @@ std::vector<std::pair<std::string, std::string>> filenames = {
     {"k.jpg", "k_bg.jpg"}, //
     {"l.jpg", "l_bg.jpg"}, //
     {"m.jpg", "m_bg.jpg"}  //
-    */
 };
 
 
@@ -95,97 +94,88 @@ public:
   using image_t      = mln::image2d<mln::rgb8>;
   using out_image_t  = mln::image2d<uint8_t>;
   using image_cv_t   = cv::Mat;
-  using callback_pln = std::function<void(const std::vector<image_t>& input_img, const std::vector<image_t>& input_bg,
-                                          std::vector<out_image_t>& output)>;
-  using callback_cv =
-      std::function<void(const std::vector<image_cv_t>& input_img_cv, const std::vector<image_cv_t>& input_bg_cv,
-                         std::vector<image_cv_t>& output_cv)>;
-  using callback_t = std::variant<callback_pln, callback_cv>;
+  using callback_pln = std::function<void(const image_t&, const image_t&, out_image_t&)>;
+  using callback_cv  = std::function<void(const image_cv_t& input_img_cv, const image_cv_t& input_bg_cv, image_cv_t& output_cv)>;
 
   const std::string filepath =
       std::string{fixtures::ImagePath::get_image_path()} + "../apps/tip.2019-2020/images/bg_sub_mosaic";
 
   BMPlnVsOpenCV_BgSubPipeline()
   {
-    // std::cout << "Loading start" << std::endl;
-
     if (!g_loaded)
     {
-      auto it_imgs    = g_input_imgs.begin();
-      auto it_bgs     = g_input_bgs.begin();
-      auto it_outs    = g_outputs.begin();
-      auto it_imgs_cv = g_input_imgs_cv.begin();
-      auto it_bgs_cv  = g_input_bgs_cv.begin();
-      auto it_outs_cv = g_outputs_cv.begin();
-      for (auto&& filename : filenames)
+      std::size_t n = filenames.size();
+
+      g_input_imgs.resize(n);
+      g_input_bgs.resize(n);
+      g_outputs.resize(n);
+      g_input_imgs_cv.resize(n);
+      g_input_bgs_cv.resize(n);
+      g_outputs_cv.resize(n);
+
+      for (std::size_t i = 0; i < n; ++i)
       {
-        mln::io::imread(filepath + "/mosaic_15x15_" + std::string{filename.first}, *it_imgs);
-        mln::io::imread(filepath + "/mosaic_15x15_" + std::string{filename.second}, *it_bgs);
-        mln::resize(*it_outs, *it_imgs);
+        mln::io::imread(fmt::format("{}/mosaic_{}_{}", filepath, "6x6", filenames[i].first), g_input_imgs[i]);
+        mln::io::imread(fmt::format("{}/mosaic_{}_{}", filepath, "6x6", filenames[i].second), g_input_bgs[i]);
+        mln::resize(g_outputs[i], g_input_imgs[i]);
 
-        *it_imgs_cv = cv::imread(filepath + "/mosaic_15x15_" + std::string{filename.first}, cv::IMREAD_COLOR);
-        *it_bgs_cv  = cv::imread(filepath + "/mosaic_15x15_" + std::string{filename.second}, cv::IMREAD_COLOR);
-        it_outs_cv->create(it_imgs_cv->size().width, it_imgs_cv->size().height, CV_8UC1);
+        int w = g_input_imgs[i].width();
+        int h = g_input_imgs[i].height();
 
-        g_size += it_imgs->width() * it_imgs->height();
-        ++it_imgs;
-        ++it_bgs;
-        ++it_outs;
-        ++it_imgs_cv;
-        ++it_bgs_cv;
-        ++it_outs_cv;
+        g_input_imgs_cv[i] = cv::imread(fmt::format("{}/mosaic_{}_{}", filepath, "6x6", filenames[i].first), cv::IMREAD_COLOR);
+        g_input_bgs_cv[i]  = cv::imread(fmt::format("{}/mosaic_{}_{}", filepath, "6x6", filenames[i].second), cv::IMREAD_COLOR);
+        g_outputs_cv[i].create(w, h, CV_8UC1);
+
+        g_size += w * h;
       }
       g_loaded = true;
     }
-
-    m_input_imgs    = g_input_imgs;
-    m_input_bgs     = g_input_bgs;
-    m_outputs       = g_outputs;
-    m_input_imgs_cv = g_input_imgs_cv;
-    m_input_bgs_cv  = g_input_bgs_cv;
-    m_outputs_cv    = g_outputs_cv;
-    m_size          = g_size;
-
-    // std::cerr << "After loading" << std::endl;
-    // details::log_memory_usage();
-    // std::cerr << "------" << std::endl;
-    // std::cout << "Loading end" << std::endl;
   };
 
   static void run_hook_before()
   {
-    std::size_t ms, rss;
-    details::GetMemorySize(ms, rss);
-    std::cout << "Amount of memory in use:" << ms << ", RSS=" << rss << std::endl;
-    details::log_memory_usage();
-    std::cout << "run start" << std::endl;
+    if (ENABLE_MEMORY_LOG)
+    {
+      std::size_t ms, rss;
+      details::GetMemorySize(ms, rss);
+      details::log_memory_usage();
+      fmt::print("Start -- Amount of memory in use: {} RSS={}\n", ms, rss);
+    }
   }
 
   static void run_hook_after()
   {
-    std::size_t ms, rss;
-    std::cout << "run end" << std::endl;
-    details::GetMemorySize(ms, rss);
-    std::cout << "Amount of memory in use:" << ms << ", RSS=" << rss << std::endl;
-    details::log_memory_usage();
+    if (ENABLE_MEMORY_LOG)
+    {
+      std::size_t ms, rss;
+      details::GetMemorySize(ms, rss);
+      fmt::print("End -- Amount of memory in use: {} RSS={}\n", ms, rss);
+      details::log_memory_usage();
+    }
   }
 
   void run(benchmark::State& st, callback_pln callback)
   {
-    //run_hook_before();
+    int img_id = st.range(0);
+    run_hook_before();
     for (auto _ : st)
-      callback(m_input_imgs, m_input_bgs, m_outputs);
-    st.SetBytesProcessed(int64_t(st.iterations()) * int64_t(m_size));
-    //run_hook_after();
+      callback(g_input_imgs[img_id], g_input_bgs[img_id], g_outputs[img_id]);
+
+
+    st.SetBytesProcessed(int64_t(st.iterations()) * int64_t(g_size));
+    run_hook_after();
   }
 
   void run(benchmark::State& st, callback_cv callback)
   {
-    //run_hook_before();
+    int img_id = st.range(0);
+
+    run_hook_before();
     for (auto _ : st)
-      callback(m_input_imgs_cv, m_input_bgs_cv, m_outputs_cv);
-    st.SetBytesProcessed(int64_t(st.iterations()) * int64_t(m_size));
-    //run_hook_after();
+      callback(g_input_imgs_cv[img_id], g_input_bgs_cv[img_id], g_outputs_cv[img_id]);
+
+    st.SetBytesProcessed(int64_t(st.iterations()) * int64_t(g_size));
+    run_hook_after();
   }
 
 
@@ -200,101 +190,53 @@ protected:
   static std::vector<image_cv_t>  g_input_bgs_cv;
   static std::vector<image_cv_t>  g_outputs_cv;
   static std::size_t              g_size;
-  std::vector<image_t>            m_input_imgs;
-  std::vector<image_t>            m_input_bgs;
-  std::vector<out_image_t>        m_outputs;
-  std::vector<image_cv_t>         m_input_imgs_cv;
-  std::vector<image_cv_t>         m_input_bgs_cv;
-  std::vector<image_cv_t>         m_outputs_cv;
-  std::size_t                     m_size;
 };
 
 bool                                                  BMPlnVsOpenCV_BgSubPipeline::g_loaded = false;
 std::size_t                                           BMPlnVsOpenCV_BgSubPipeline::g_size   = 0;
-std::vector<BMPlnVsOpenCV_BgSubPipeline::image_t>     BMPlnVsOpenCV_BgSubPipeline::g_input_imgs{filenames.size()};
-std::vector<BMPlnVsOpenCV_BgSubPipeline::image_t>     BMPlnVsOpenCV_BgSubPipeline::g_input_bgs{filenames.size()};
-std::vector<BMPlnVsOpenCV_BgSubPipeline::out_image_t> BMPlnVsOpenCV_BgSubPipeline::g_outputs{filenames.size()};
-std::vector<BMPlnVsOpenCV_BgSubPipeline::image_cv_t>  BMPlnVsOpenCV_BgSubPipeline::g_input_imgs_cv{filenames.size()};
-std::vector<BMPlnVsOpenCV_BgSubPipeline::image_cv_t>  BMPlnVsOpenCV_BgSubPipeline::g_input_bgs_cv{filenames.size()};
-std::vector<BMPlnVsOpenCV_BgSubPipeline::image_cv_t>  BMPlnVsOpenCV_BgSubPipeline::g_outputs_cv{filenames.size()};
+std::vector<BMPlnVsOpenCV_BgSubPipeline::image_t>     BMPlnVsOpenCV_BgSubPipeline::g_input_imgs;
+std::vector<BMPlnVsOpenCV_BgSubPipeline::image_t>     BMPlnVsOpenCV_BgSubPipeline::g_input_bgs;
+std::vector<BMPlnVsOpenCV_BgSubPipeline::out_image_t> BMPlnVsOpenCV_BgSubPipeline::g_outputs;
+std::vector<BMPlnVsOpenCV_BgSubPipeline::image_cv_t>  BMPlnVsOpenCV_BgSubPipeline::g_input_imgs_cv;
+std::vector<BMPlnVsOpenCV_BgSubPipeline::image_cv_t>  BMPlnVsOpenCV_BgSubPipeline::g_input_bgs_cv;
+std::vector<BMPlnVsOpenCV_BgSubPipeline::image_cv_t>  BMPlnVsOpenCV_BgSubPipeline::g_outputs_cv;
 
 // PLN
 
 BENCHMARK_DEFINE_F(BMPlnVsOpenCV_BgSubPipeline, Pln_PipeViewsDisc)(benchmark::State& st)
 {
-  const int radius = st.range(0);
+  const int radius = st.range(1);
+  auto      se     = mln::se::disc(radius);
 
-  auto f = [radius](const std::vector<image_t>& input_imgs, const std::vector<image_t>& input_bgs,
-                    std::vector<out_image_t>& outputs) {
-    auto it_imgs = input_imgs.begin();
-    auto it_bgs  = input_bgs.begin();
-    auto it_outs = outputs.begin();
-    auto se      = mln::se::disc(radius);
-    for (; it_imgs != input_imgs.end(); ++it_imgs, ++it_bgs, ++it_outs)
-    {
-      pln_bg_sub_pipe_views(*it_imgs, *it_bgs, *it_outs, se);
-    }
-  };
-
+  callback_pln f = [se](const image_t& in1, const image_t& in2, out_image_t& out) { pln_bg_sub_pipe_views(in1, in2, out, se); };
   this->run(st, f);
 }
 
 BENCHMARK_DEFINE_F(BMPlnVsOpenCV_BgSubPipeline, Pln_PipeViewsRect)(benchmark::State& st)
 {
-  const int radius = st.range(0);
+  const int radius = st.range(1);
+  auto      edge   = 2 * radius + 1;
+  auto      se     = mln::se::rect2d(edge, edge);
 
-  auto f = [radius](const std::vector<image_t>& input_imgs, const std::vector<image_t>& input_bgs,
-                    std::vector<out_image_t>& outputs) {
-    auto it_imgs = input_imgs.begin();
-    auto it_bgs  = input_bgs.begin();
-    auto it_outs = outputs.begin();
-    auto edge    = 2 * radius + 1;
-    auto se      = mln::se::rect2d(edge, edge);
-    for (; it_imgs != input_imgs.end(); ++it_imgs, ++it_bgs, ++it_outs)
-    {
-      pln_bg_sub_pipe_views(*it_imgs, *it_bgs, *it_outs, se);
-    }
-  };
-
+  auto f = [se](const image_t& in1, const image_t& in2, out_image_t& out) { pln_bg_sub_pipe_views(in1, in2, out, se); };
   this->run(st, f);
 }
 
 BENCHMARK_DEFINE_F(BMPlnVsOpenCV_BgSubPipeline, Pln_PipeAlgosDisc)(benchmark::State& st)
 {
-  const int radius = st.range(0);
-
-  auto f = [radius](const std::vector<image_t>& input_imgs, const std::vector<image_t>& input_bgs,
-                    std::vector<out_image_t>& outputs) {
-    auto it_imgs = input_imgs.begin();
-    auto it_bgs  = input_bgs.begin();
-    auto it_outs = outputs.begin();
-    auto se      = mln::se::disc(radius);
-    for (; it_imgs != input_imgs.end(); ++it_imgs, ++it_bgs, ++it_outs)
-    {
-      pln_bg_sub_pipe_algos(*it_imgs, *it_bgs, *it_outs, se);
-    }
-  };
-
+  const int radius = st.range(1);
+  auto      se     = mln::se::disc(radius);
+  auto f = [se](const image_t& in1, const image_t& in2, out_image_t& out) { pln_bg_sub_pipe_algos(in1, in2, out, se); };
   this->run(st, f);
 }
 
 BENCHMARK_DEFINE_F(BMPlnVsOpenCV_BgSubPipeline, Pln_PipeAlgosRect)(benchmark::State& st)
 {
-  const int radius = st.range(0);
+  const int radius = st.range(1);
+  auto      edge   = 2 * radius + 1;
+  auto      se     = mln::se::rect2d(edge, edge);
 
-  auto f = [radius](const std::vector<image_t>& input_imgs, const std::vector<image_t>& input_bgs,
-                    std::vector<out_image_t>& outputs) {
-    auto it_imgs = input_imgs.begin();
-    auto it_bgs  = input_bgs.begin();
-    auto it_outs = outputs.begin();
-    auto edge    = 2 * radius + 1;
-    auto se      = mln::se::rect2d(edge, edge);
-    for (; it_imgs != input_imgs.end(); ++it_imgs, ++it_bgs, ++it_outs)
-    {
-      pln_bg_sub_pipe_algos(*it_imgs, *it_bgs, *it_outs, se);
-    }
-  };
-
+  auto f = [se](const image_t& in1, const image_t& in2, out_image_t& out) { pln_bg_sub_pipe_algos(in1, in2, out, se); };
   this->run(st, f);
 }
 
@@ -302,58 +244,41 @@ BENCHMARK_DEFINE_F(BMPlnVsOpenCV_BgSubPipeline, Pln_PipeAlgosRect)(benchmark::St
 
 BENCHMARK_DEFINE_F(BMPlnVsOpenCV_BgSubPipeline, CV_PipeAlgoDisc)(benchmark::State& st)
 {
-  const int radius = st.range(0);
-
-  auto f = [radius](const std::vector<image_cv_t>& input_imgs_cv, const std::vector<image_cv_t>& input_bgs_cv,
-                    std::vector<image_cv_t>& outputs_cv) {
-    auto it_imgs_cv = input_imgs_cv.begin();
-    auto it_bgs_cv  = input_bgs_cv.begin();
-    auto it_outs_cv = outputs_cv.begin();
-    auto se         = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(2 * radius + 1, 2 * radius + 1));
-    for (; it_imgs_cv != input_imgs_cv.end(); ++it_imgs_cv, ++it_bgs_cv, ++it_outs_cv)
-    {
-      cv_bg_sub_pipe(*it_imgs_cv, *it_bgs_cv, *it_outs_cv, se);
-    }
-  };
+  const int radius = st.range(1);
+  auto se = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(2 * radius + 1, 2 * radius + 1));
+  auto f = [se](const image_cv_t& in1, const image_cv_t& in2, image_cv_t& out) { cv_bg_sub_pipe(in1, in2, out, se); };
 
   this->run(st, f);
 }
 
 BENCHMARK_DEFINE_F(BMPlnVsOpenCV_BgSubPipeline, CV_PipeAlgoRect)(benchmark::State& st)
 {
-  const int radius = st.range(0);
+  const int radius = st.range(1);
 
-  auto f = [radius](const std::vector<image_cv_t>& input_imgs_cv, const std::vector<image_cv_t>& input_bgs_cv,
-                    std::vector<image_cv_t>& outputs_cv) {
-    auto it_imgs_cv = input_imgs_cv.begin();
-    auto it_bgs_cv  = input_bgs_cv.begin();
-    auto it_outs_cv = outputs_cv.begin();
-    auto se         = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(2 * radius + 1, 2 * radius + 1));
-    for (; it_imgs_cv != input_imgs_cv.end(); ++it_imgs_cv, ++it_bgs_cv, ++it_outs_cv)
-    {
-      cv_bg_sub_pipe(*it_imgs_cv, *it_bgs_cv, *it_outs_cv, se);
-    }
-  };
-
+  auto se = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(2 * radius + 1, 2 * radius + 1));
+  auto f  = [se](const image_cv_t& in1, const image_cv_t& in2, image_cv_t& out) { cv_bg_sub_pipe(in1, in2, out, se); };
   this->run(st, f);
 }
 
 namespace
 {
   void CustomArguments(benchmark::internal::Benchmark* b) {
-    for (int i = 1; i < 7; ++i)
+    for (int img_id = 0; img_id < (int)(filenames.size()); ++img_id)
     {
-      int r = 1 << i;
+      for (int i = 1; i < 7; ++i)
+      {
+        int r = 1 << i;
 
-      b->Arg(r);
+        b->Args({img_id, r});
 
-      if (i >= 3)
-        b->Arg(r + (1 << (i - 2)));
+        if (i >= 3)
+          b->Args({img_id, r + (1 << (i - 2))});
 
-      b->Arg(r + (1 << (i - 1)));
+                 b->Args({img_id, r + (1 << (i - 1))});
 
-      if (i >= 3)
-        b->Arg(r + (1 << (i - 1)) + (1 << (i - 2)));
+        if (i >= 3)
+          b->Args({img_id, r + (1 << (i - 1)) + (1 << (i - 2))});
+      }
     }
   }
 }

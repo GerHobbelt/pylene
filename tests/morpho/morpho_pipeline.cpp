80d43e68f938338930835ecdb957de9ec557266b
Edwin Carlinet <edwin.carlinet@lrde.epita.fr>
2020-11-24 13:11:28 +0100

    Fix compilation.

--- tests/morpho/morpho_pipeline.cpp
+++ tests/morpho/morpho_pipeline.cpp
@@ -48,9 +48,9 @@ TEST(Morpho, gradient_int_endtoend)
   mln::se::rect2d win(3, 3);
 
   tbb::task_scheduler_init init;
-  
+
   auto grad1 = mln::morpho::internal_gradient(ima, win);
   auto grad2 = mln::morpho::MorphoPipeline(mln::morpho::e_MorphoPipelineOperation::Grad_int, ima, win).execute(); //TODO mln::morpho::parallel::internal_gradient
 
-  ASSERT_IMAGES_EQ_EXP(grad2, grad1);
+  ASSERT_IMAGES_EQ_EXP((grad2.__cast<uint8_t, 2>()), grad1);
 }

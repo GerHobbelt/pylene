3a854597945f719e676d76132637c0a4e52d3b2b
Baptiste Esteban <besteban@lrde.epita.fr>
2021-02-17 18:22:26 +0100

    Create Conan components

--- conanfile.py
+++ conanfile.py
@@ -11,27 +11,22 @@ class Pylene(ConanFile):
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
-        "python": [True, False]
+        "pylena_numpy": [True, False]
     }
     default_options = {
         "shared": False,
         "fPIC": False,
-        "python": False,
+        "pylena_numpy": False,
         "gtest:shared": False,
         "boost:shared": True
     }
 
     generators = [ "cmake", "cmake_paths", "cmake_find_package" ]
-    exports_sources = ["pylene/*", "cmake/*", "CMakeLists.txt", "LICENSE"]
+    exports_sources = ["pylene/*", "pylene-python/*", "cmake/*", "CMakeLists.txt", "LICENSE"]
 
     build_requires = [
         "gtest/[>=1.10]",
         "benchmark/[>=1.5.0]",
-        # Temporary remove pybind11 from conan because of the issue
-        # https://github.com/conan-io/conan-center-index/issues/4407
-        # on conan 1.33 and waiting for the merging of the PR
-        # https://github.com/conan-io/conan-center-index/pull/4445
-        "pybind11/2.6.2", 
 
         # For now boost is too heavy and is not based on components
         # Such a dependancy brings linktime overhead because too many libraries are linked
@@ -42,7 +37,7 @@ class Pylene(ConanFile):
         "range-v3/0.10.0",
         "fmt/6.0.0",
         "tbb/2020.0",
-        "xsimd/7.4.6"
+        "xsimd/7.4.6",
     ]
 
 
@@ -50,11 +45,15 @@ class Pylene(ConanFile):
         tools.check_min_cppstd(self, "20")
         if self.options.shared:
             del self.options.fPIC
+        if self.options.pylena_numpy and self.options.fPIC:
+            self.requires.add("pybind11/2.6.2")
+        elif self.options.pylena_numpy:
+            self.output.warn("fPIC option set to False. Skipping pylena_numpy component. To build pylena_numpy component, please set fPIC option to True.")
 
     def build(self):
         cmake = CMake(self)
         cmake.definitions["PYLENE_BUILD_LIBS_ONLY"] = "YES"
-        if self.options.python:
+        if self.options.fPIC and self.options.pylena_numpy:
             cmake.definitions["PYLENE_BUILD_PYTHON"] = "YES"
         cmake.configure()
         cmake.build()
@@ -65,18 +64,28 @@ class Pylene(ConanFile):
         tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
-        self.cpp_info.system_libs.append("freeimage")
         self.cpp_info.names["cmake_find_package"] = "Pylene"
         self.cpp_info.names["cmake_find_package_multi"] = "Pylene"
-        self.cpp_info.libs = [ "Pylene" ]
 
+        self.cpp_info.components["Pylene"].system_libs.append("freeimage")
+        self.cpp_info.components["Pylene"].names["cmake_find_package"] = "Pylene"
+        self.cpp_info.components["Pylene"].names["cmake_find_package_multi"] = "Pylene"
+        self.cpp_info.components["Pylene"].libs = ["Pylene"]
+        self.cpp_info.components["Pylene"].includedirs = [os.path.join(self.package_folder, "include")]
+        self.cpp_info.components["Pylene"].requires = ["range-v3::range-v3", "fmt::fmt", "tbb::tbb", "xsimd::xsimd"]
 
-        self.cpp_info.cxxflags.append(tools.cppstd_flag(self.settings))
-
+        if self.options.fPIC and self.options.pylena_numpy:
+            self.cpp_info.components["pylena_numpy"].names["cmake_find_pakage"] = "pylena_numpy"
+            self.cpp_info.components["pylena_numpy"].names["cmake_find_pakage_multi"] = "pylena_numpy"
+            self.cpp_info.components["pylena_numpy"].libs = ["pylena_numpy"]
+            self.cpp_info.components["pylena_numpy"].requires = ["Pylene", "pybind11::pybind11"]
+            self.cpp_info.components["pylena_numpy"].includedirs = [os.path.join(self.package_folder, "include")]
 
         v = tools.Version(self.settings.compiler.version)
-        if self.settings.compiler == "gcc" and v.major == "9":
-            self.cpp_info.cxxflags.append("-fconcepts")
+        for comp in self.cpp_info.components:
+            self.cpp_info.components[comp].cxxflags.append(tools.cppstd_flag(self.settings))
+            if self.settings.compiler == "gcc" and v.major == "9":
+                self.cpp_info.components[comp].cxxflags.append("-fconcepts")
 
     def package_id(self):
         del self.info.settings.compiler.cppstd
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,13 +1,17 @@
 from conans import ConanFile, CMake
 import os
+import sys
 
 class PyleneTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = [ "cmake_find_package" ]
+    generators = [ "cmake_find_package", "cmake" ]
 
 
     def build(self):
+        os.remove(os.path.join(self.build_folder, "Findpybind11.cmake"))
         cmake = CMake(self)
+        if self.options["pylene"].pylena_numpy and self.options["pylene"].fPIC:
+            cmake.definitions["WITH_PYLENA_NUMPY"] = "YES"
         cmake.configure()
         cmake.build()
 
@@ -17,3 +21,4 @@ class PyleneTestConan(ConanFile):
 
     def test(self):
         self.run(".{}main".format(os.sep))
+        self.run("{} main.py".format(sys.executable))

d6cde417b1937b38f9111bc61049bbd93b98cba7
Baptiste Esteban <baptiste.esteban.27@gmail.com>
2021-03-16 10:52:57 +0100

    Make pybind11 a requirement for the user

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -6,9 +6,10 @@ class PyleneTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = [ "cmake_find_package", "cmake" ]
     
-    # Disable python packaging until cmake_find_package generator for pybind11 does not work
+    requires = ["pybind11/2.6.2"]
+
     def _build_python(self):
-        return (self.options["pylene"].shared or self.options["pylene"].fPIC or tools.os_info.is_windows) and False
+        return self.options["pylene"].shared or self.options["pylene"].fPIC or tools.os_info.is_windows
 
     def build(self):
         cmake = CMake(self)

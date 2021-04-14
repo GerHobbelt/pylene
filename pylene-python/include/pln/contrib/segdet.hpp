ee03b829c4955792d0b7d3fcf6f4a2e6f67c6100
Alexandre Bourquelot <alexandre.bourquelot@epita.fr>
2021-04-15 00:34:52 +0200

    Add array like output for segments

--- pylene-python/include/pln/contrib/segdet.hpp
+++ pylene-python/include/pln/contrib/segdet.hpp
@@ -1,12 +1,31 @@
 #pragma once
 #include <mln/contrib/segdet/segdet.hpp>
 
+
+using sorry_edwin =
+    std::vector<std::pair<bool, std::pair<std::vector<std::vector<uint32_t>>, std::vector<std::vector<uint32_t>>>>>;
 namespace pln::contrib::segdet
 {
-  int detect_line_hat(mln::ndbuffer_image img, int min_len, int disc)
+  sorry_edwin detect_line_hat(mln::ndbuffer_image img, int min_len, int disc)
   {
     auto p = mln::contrib::segdet::detect_line(img.__cast<uint8_t, 2>(), min_len, disc);
 
-    return p.size();
+    sorry_edwin out;
+
+    for (auto& seg : p)
+    {
+      std::vector<std::vector<uint32_t>> points;
+      for (auto& point : seg.points)
+        points.push_back(std::vector<uint32_t>({point.x, point.y, point.thickness}));
+      std::vector<std::vector<uint32_t>> under;
+      for (auto& under_o : seg.under_other)
+        points.push_back(std::vector<uint32_t>({under_o.x, under_o.y, under_o.thickness}));
+
+      auto inner_pair = std::make_pair(points, under);
+      auto outer_pair = std::make_pair(seg.is_horizontal, inner_pair);
+      out.push_back(outer_pair);
+    }
+
+    return out;
   }
-} // namespace pln::contri::segdet
+} // namespace pln::contrib::segdet
--- pylene-python/src/module.cpp
+++ pylene-python/src/module.cpp
@@ -4,6 +4,7 @@
 #include <pln/core/image_cast.hpp>
 
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 #include <memory>
 

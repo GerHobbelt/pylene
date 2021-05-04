103fab1f207b1104bec10faaf12612d0f790fe28
Victor Simonin <victor.simonin@epita.fr>
2021-05-04 23:05:54 +0200

    Saliency integration with Component Tree structure

---
+++ pylene/src/hierarchies/component_saliency.cpp
@@ -0,0 +1,103 @@
+#include "mln/hierarchies/component_saliency.hpp"
+#include "mln/core/algorithm/fill.hpp"
+
+#include <mln/accu/accumulators/count.hpp>
+
+#include <algorithm>
+
+namespace mln
+{
+  static std::vector<int> lca_preprocess(mln::morpho::component_tree<uint8_t> tree, const auto node_map)
+  {
+    return tree.compute_attribute_on_points(node_map, mln::accu::features::count<>());
+  }
+
+  // TODO Use sparse table to optimize LCA
+  static int lca(const std::vector<int>& depth, mln::morpho::component_tree<uint8_t> tree, int u, int v)
+  {
+    // Put u and v at the same level
+    while (depth[u] != depth[v])
+    {
+      if (depth[u] > depth[v])
+        u = tree.parent[u];
+      else
+        v = tree.parent[v];
+    }
+
+    // Go up until the ancestor is common
+    while (u != v)
+    {
+      u = tree.parent[u];
+      v = tree.parent[v];
+    }
+
+    return u;
+  }
+
+  std::vector<Edge> saliency_map(mln::morpho::component_tree<uint8_t> tree, const auto node_map, const Graph& leaf_graph)
+  {
+    std::vector<Edge> res;
+
+    std::vector<int> depth = lca_preprocess(tree, node_map);
+
+    for (Edge edge : leaf_graph.get_edges())
+    {
+      int merge_parent_node = lca(depth, tree, std::get<0>(edge), std::get<1>(edge));
+      std::get<2>(edge)     = leaf_graph.weight_node(merge_parent_node);
+      res.emplace_back(edge);
+    }
+
+    return res;
+  }
+
+  image2d<double> saliency_khalimsky_grid(mln::morpho::component_tree<uint8_t> tree, const auto node_map, const Graph& leaf_graph)
+  {
+    int height = leaf_graph.get_height();
+    int width  = leaf_graph.get_width();
+
+    int res_height = 2 * height + 1;
+    int res_width  = 2 * width + 1;
+
+    image2d<double> res(res_width, res_height);
+    fill(res, 0);
+
+    const std::vector<Edge>& s_map = saliency_map(tree, node_map, leaf_graph);
+
+    for (const auto& edge : s_map)
+    {
+      int    u = std::get<0>(edge);
+      int    v = std::get<1>(edge);
+      double w = std::get<2>(edge);
+
+      int u_pos[2] = {u % width, u / width};
+      int v_pos[2] = {v % width, v / width};
+
+      int res_offset[2]   = {u_pos[0] - v_pos[0], u_pos[1] - v_pos[1]};
+      int res_edge_pos[2] = {2 * v_pos[0] + 1 + res_offset[0], 2 * v_pos[1] + 1 + res_offset[1]};
+
+      res({res_edge_pos[0], res_edge_pos[1]}) = w;
+    }
+
+    for (int y = 0; y < res_height; y += 2)
+    {
+      for (int x = 0; x < res_width; x += 2)
+      {
+        double max = std::numeric_limits<double>::min();
+
+        if (y + 1 < height)
+          max = std::max(max, res({x, y + 1}));
+        if (x + 1 < width)
+          max = std::max(max, res({x + 1, y}));
+        if (y - 1 >= 0)
+          max = std::max(max, res({x, y - 1}));
+        if (x - 1 >= 0)
+          max = std::max(max, res({x - 1, y}));
+
+        res({x, y}) = max;
+      }
+    }
+
+    return res;
+  }
+
+} // namespace mln

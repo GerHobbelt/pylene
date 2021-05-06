47c365f23be45ce45e1f2168cfb001a219f25a91
Alexandre Bourquelot <alexandre.bourquelot@epita.fr>
2021-05-06 11:53:26 +0200

    execute preprocess before processing on call

--- pylene-python/include/pln/contrib/segdet.hpp
+++ pylene-python/include/pln/contrib/segdet.hpp
@@ -9,26 +9,6 @@ using sorry_edwin =
     std::vector<std::pair<bool, std::pair<std::vector<std::vector<uint32_t>>, std::vector<std::vector<uint32_t>>>>>;
 namespace pln::contrib::segdet
 {
-  sorry_edwin detect_line_hat(mln::ndbuffer_image img, int min_len, int disc)
-  {
-    auto p = mln::contrib::segdet::detect_line(img.__cast<uint8_t, 2>(), min_len, disc);
-
-    sorry_edwin out;
-
-    for (auto& seg : p)
-    {
-      std::vector<std::vector<uint32_t>> points;
-      for (auto& point : seg.points)
-        points.push_back(std::vector<uint32_t>({point.x, point.y, point.thickness}));
-      std::vector<std::vector<uint32_t>> under;
-
-      auto inner_pair = std::make_pair(points, under);
-      auto outer_pair = std::make_pair(seg.is_horizontal, inner_pair);
-      out.push_back(outer_pair);
-    }
-
-    return out;
-  }
 
   mln::ndbuffer_image preprocess(mln::ndbuffer_image img)
   {
@@ -48,5 +28,29 @@ namespace pln::contrib::segdet
 
     return std::move(out);
   }
+
+    sorry_edwin detect_line_hat(mln::ndbuffer_image img, int min_len, int disc)
+    {
+      // Preprocessing
+      mln::ndbuffer_image preprocessed_img = preprocess(std::move(img));
+      // Line detection
+      auto p = mln::contrib::segdet::detect_line(preprocessed_img.__cast<uint8_t, 2>(), min_len, disc);
+
+      sorry_edwin out;
+
+      for (auto& seg : p)
+      {
+        std::vector<std::vector<uint32_t>> points;
+        for (auto& point : seg.points)
+          points.push_back(std::vector<uint32_t>({point.x, point.y, point.thickness}));
+        std::vector<std::vector<uint32_t>> under;
+
+        auto inner_pair = std::make_pair(points, under);
+        auto outer_pair = std::make_pair(seg.is_horizontal, inner_pair);
+        out.push_back(outer_pair);
+      }
+
+      return out;
+    }
 
 } // namespace pln::contrib::segdet

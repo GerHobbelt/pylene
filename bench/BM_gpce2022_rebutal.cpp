309796e812ae52727ee965fce16158f5cb4afd87
Michaël Roynard <michael.roynard@lrde.epita.fr>
2022-09-30 10:00:54 +0200

    WIP bench GPCE2022

--- bench/BM_gpce2022_rebutal.cpp
+++ bench/BM_gpce2022_rebutal.cpp
@@ -13,7 +13,7 @@
 #include <mln/io/imread.hpp>
 #include <mln/io/imsave.hpp>
 #include <mln/labeling/blobs.hpp>
-#include <mln/morpho/erosion.hpp>
+#include <mln/morpho/dilation.hpp>
 #include <mln/morpho/watershed.hpp>
 
 #include <benchmark/benchmark.h>
@@ -27,20 +27,30 @@
 
 using namespace std::literals;
 
-static const std::string g_path = "./apps/icip.2022/images/";
+static const std::string g_path = "./build/bench/";
 
 class BMgpce2022_rebutal : public benchmark::Fixture
 {
   virtual void SetUp(const benchmark::State&) override
   {
-    mln::io::imread(g_path + "connected_component_labels.png", m_input_rgb8);
+    mln::io::imread(g_path + "Space1_20MB.jpg", m_input_rgb8);
     m_pixel_count = m_input_rgb8.domain().size();
     m_disc        = mln::se::disc{3};
-  }
+    /*
+    auto grayscale = [](auto v) -> uint8_t { return 0.2126 * v[0] + 0.7152 * v[1] + 0.0722 * v[2]; };
 
+    auto ima_grayscale = mln::transform(m_input_rgb8, grayscale);
+
+    uint8_t threshold    = 125;
+    auto    thresholding = [threshold](auto v) -> uint8_t { return v > threshold ? 255 : 0; };
+
+    m_input_thresholded = mln::transform(ima_grayscale, thresholding);
+    */
+  }
 protected:
   std::size_t             m_pixel_count;
   mln::image2d<mln::rgb8> m_input_rgb8;
+  // mln::image2d<uint8_t>   m_input_thresholded;
   mln::se::disc           m_disc;
 };
 
@@ -58,9 +68,9 @@ BENCHMARK_F(BMgpce2022_rebutal, labelize_view)(benchmark::State& st)
     auto input_thresholded = mln::view::transform(ima_grayscale, thresholding);
 
     // perform connected components labeling
-    int nlabel;
-    // auto ima_ret = mln::morpho::erosion(input_thresholded, m_disc);
-    auto ima_ret = mln::labeling::blobs<uint8_t>(input_thresholded, mln::c8, nlabel);
+    // int nlabel;
+    auto ima_ret = mln::morpho::dilation(input_thresholded, mln::c8);
+    // auto ima_ret = mln::labeling::blobs<uint8_t>(input_thresholded, mln::c8, nlabel);
     // auto ima_ret = mln::morpho::watershed<int16_t>(input_thresholded, mln::c8, nlabel);
 
     benchmark::DoNotOptimize(ima_ret);
@@ -73,6 +83,7 @@ BENCHMARK_F(BMgpce2022_rebutal, labelize_serial)(benchmark::State& st)
 {
   while (st.KeepRunning())
   {
+
     auto grayscale = [](auto v) -> uint8_t { return 0.2126 * v[0] + 0.7152 * v[1] + 0.0722 * v[2]; };
 
     auto ima_grayscale = mln::transform(m_input_rgb8, grayscale);
@@ -83,9 +94,9 @@ BENCHMARK_F(BMgpce2022_rebutal, labelize_serial)(benchmark::State& st)
     auto input_thresholded = mln::transform(ima_grayscale, thresholding);
 
     // perform connected components labeling
-    int nlabel;
-    // auto ima_ret = mln::morpho::erosion(input_thresholded, m_disc);
-    auto ima_ret = mln::labeling::blobs<uint8_t>(input_thresholded, mln::c8, nlabel);
+    //int nlabel;
+    auto ima_ret = mln::morpho::dilation(input_thresholded, mln::c8);
+    // auto ima_ret = mln::labeling::blobs<uint8_t>(input_thresholded, mln::c8, nlabel);
     //  auto ima_ret = mln::morpho::watershed<int16_t>(input_thresholded, mln::c8, nlabel);
     benchmark::DoNotOptimize(ima_ret);
   }

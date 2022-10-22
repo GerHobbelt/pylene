4574d7bd8816a03ccf371ad5f6babb9ff846c854
Baptiste Esteban <besteban@lrde.epita.fr>
2022-10-22 18:31:50 +0200

    Merge branch 'development/fix_horizontal_cut' into 'next'
    Fix horizontal cut issue
    
    See merge request olena/pylene!148

--- tests/morpho/component_tree.cpp
+++ tests/morpho/component_tree.cpp
@@ -1,3 +1,4 @@
+#include <mln/accu/accumulators/count.hpp>
 #include <mln/core/image/ndimage.hpp>
 #include <mln/morpho/alphatree.hpp>
 #include <mln/morpho/maxtree.hpp>
@@ -134,4 +135,100 @@ TEST(Morpho, Filtering_subtractive)
     auto rec = tree.reconstruct(nodemap);
     ASSERT_IMAGES_EQ_EXP(rec, expected);
   }
+}
+
+TEST(Morpho, HorizontalCut)
+{
+  mln::morpho::component_tree<int> t;
+  t.parent = std::vector<int>{0, 0, 0, 1, 1, 2, 2, 2};
+  t.values = std::vector<int>{3, 2, 1, 0, 0, 0, 0, 0};
+  
+  const mln::image2d<int> nodemap{
+    {5, 5, 6, 3, 3},
+    {5, 5, 6, 3, 4},
+    {6, 6, 6, 7, 4}
+  };
+
+  const mln::image2d<int> ref_cut_1{
+    {2, 2, 2, 3, 3},
+    {2, 2, 2, 3, 4},
+    {2, 2, 2, 2, 4}
+  };
+
+  const mln::image2d<int> ref_cut_2{
+    {2, 2, 2, 1, 1},
+    {2, 2, 2, 1, 1},
+    {2, 2, 2, 2, 1}
+  };
+
+  const mln::image2d<int> ref_cut_3{
+    {0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0}
+  };
+
+  const auto cut_0 = t.horizontal_cut(0, nodemap);
+  const auto cut_1 = t.horizontal_cut(1, nodemap);
+  const auto cut_2 = t.horizontal_cut(2, nodemap);
+  const auto cut_3 = t.horizontal_cut(3, nodemap);
+  const auto cut_4 = t.horizontal_cut(4, nodemap);
+
+  ASSERT_IMAGES_EQ_EXP(cut_0, nodemap);
+  ASSERT_IMAGES_EQ_EXP(cut_1, ref_cut_1);
+  ASSERT_IMAGES_EQ_EXP(cut_2, ref_cut_2);
+  ASSERT_IMAGES_EQ_EXP(cut_3, ref_cut_3);
+  ASSERT_IMAGES_EQ_EXP(cut_4, ref_cut_3);
+}
+
+TEST(Morpho, HorizontalCut_AfterFiltering)
+{
+  mln::morpho::component_tree<int> t;
+  t.parent = std::vector<int>{0, 0, 0, 1, 1, 2, 2, 2};
+  t.values = std::vector<int>{3, 2, 1, 0, 0, 0, 0, 0};
+  
+  mln::image2d<int> nodemap{
+    {5, 5, 6, 3, 3},
+    {5, 5, 6, 3, 4},
+    {6, 6, 6, 7, 4}
+  };
+
+  const mln::image2d<int> ref_cut_0{
+    {5, 5, 6, 3, 3},
+    {5, 5, 6, 3, 4},
+    {6, 6, 6, 2, 4}
+  };
+
+  const mln::image2d<int> ref_cut_1{
+    {2, 2, 2, 3, 3},
+    {2, 2, 2, 3, 4},
+    {2, 2, 2, 2, 4}
+  };
+
+  const mln::image2d<int> ref_cut_2{
+    {2, 2, 2, 1, 1},
+    {2, 2, 2, 1, 1},
+    {2, 2, 2, 2, 1}
+  };
+
+  const mln::image2d<int> ref_cut_3{
+    {0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0}
+  };
+
+  const auto area = t.compute_attribute_on_points(nodemap, mln::accu::accumulators::count{});
+  t.filter(mln::morpho::ct_filtering::CT_FILTER_DIRECT, nodemap, [&area](int n) { return area[n] > 1; });
+
+  const auto cut_0 = t.horizontal_cut(0, nodemap);
+  const auto cut_1 = t.horizontal_cut(1, nodemap);
+  const auto cut_2 = t.horizontal_cut(2, nodemap);
+  const auto cut_3 = t.horizontal_cut(3, nodemap);
+  const auto cut_4 = t.horizontal_cut(4, nodemap);
+
+  ASSERT_IMAGES_EQ_EXP(nodemap, ref_cut_0);
+  ASSERT_IMAGES_EQ_EXP(cut_0, ref_cut_0);
+  ASSERT_IMAGES_EQ_EXP(cut_1, ref_cut_1);
+  ASSERT_IMAGES_EQ_EXP(cut_2, ref_cut_2);
+  ASSERT_IMAGES_EQ_EXP(cut_3, ref_cut_3);
+  ASSERT_IMAGES_EQ_EXP(cut_4, ref_cut_3);
 }
